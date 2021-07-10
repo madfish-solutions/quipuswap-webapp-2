d1aa7204f415720c92e25b50a038ea4f6cc5dedf
olehkhalin <olehkhalin@gmail.com>
2021-07-10 15:36:26 +0300

    Added some functions

--- src/utils/default-seo.config.ts
+++ src/utils/default-seo.config.ts
@@ -1,16 +1,16 @@
 export const DEFAULT_SEO = {
-  TITLE: 'WEBSITE NAME',
-  DESCRIPTION: 'WEBSITE DESCRIPTION',
-  SITE_NAME: 'WEBSITE NAME',
-  WEBSITE_URL: 'https://website-url/', // Slash in the end is necessary
+  TITLE: 'QuipuSwap - decentralized  DEX and AMM protocol for Tezos',
+  DESCRIPTION: 'Add and Trade any Tezos based tokens, add liquidity, earn trading fees and baking rewards, participate in governance and use liquidity tokens in farming projects.',
+  SITE_NAME: 'QuipuSwap',
+  WEBSITE_URL: 'https://quipuswap-landing.vercel.app/', // Slash in the end is necessary
   IMAGE: 'og_image.jpg',
   OG: {
     TYPE: 'website',
-    SITE_NAME: 'WEBSITE NAME',
+    SITE_NAME: 'QuipuSwap - decentralized  DEX and AMM protocol for Tezos',
   },
   TWITTER: {
-    HANDLE: '@AUTHOR',
-    SITE: '@WEBSITE',
+    HANDLE: '@madfishofficial',
+    SITE: '@QuipuSwap',
     CARD_TYPE: 'summary_large_image',
   },
 };
--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,4 +1,17 @@
 export const COLOR_MODE_STORAGE_KEY = 'theme';
+
+export const DEFAULT_DECIMALS = 6;
+
 export const QUIPUSWAP = 'https://quipuswap.com/';
 export const QUIPUSWAP_TRADE = 'https://quipuswap.com/swap';
 export const QUIPUSWAP_INVEST = 'https://quipuswap.com/invest/add-liquidity';
+
+export const APOLLO_CLIENT_ENDPOINT = process.env.NEXT_PUBLIC_APOLLO_CLIENT_ENDPOINT;
+
+export const QUIPUSWAP_ANALYTICS_TOKENS = 'https://analytics.quipuswap.com/tokens';
+export const QUIPUSWAP_ANALYTICS_PAIRS = 'https://analytics.quipuswap.com/pairs';
+
+export const TEMPLEWALLET_IMG = 'https://img.templewallet.com/insecure/fill/50/50/ce/0/plain';
+export const CLOUDFLARE_IPFS = 'https://cloudflare-ipfs.com/ipfs';
+export const IPFS = 'ipfs';
+export const IPFS_IO = 'https://ipfs.io/ipfs/';
