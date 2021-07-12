9ea1d67ca798a483384304d719dd892115c24cf8
olehkhalin <olehkhalin@gmail.com>
2021-07-12 19:07:05 +0300

    Fixed errors

--- src/pages/ui-kit.tsx
+++ src/pages/ui-kit.tsx
@@ -4,7 +4,6 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import cx from 'classnames';
 
 import { BaseLayout } from '@layouts/BaseLayout';
-import { Container } from '@components/ui/Container';
 import { Button } from '@components/ui/Button';
 import { ColorModeSwitcher } from '@components/ui/ColorModeSwitcher';
 import { Logo } from '@components/svg/Logo';
@@ -22,204 +21,194 @@ const UiKit: React.FC = () => {
       description={t('ui-kit:Home page description. Couple sentences...')}
     >
       <section className={s.section}>
-        <Container>
-          <h1 className={s.header}>Colors</h1>
-          <h2 className={s.sectionHeader}>Brand colors</h2>
-          <div className={s.colorsBlock}>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.brand01)} />
-              <h3 className={s.blockHeader}>$brand-01</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.brand02)} />
-              <h3 className={s.blockHeader}>$brand-02</h3>
-            </div>
+        <h1 className={s.header}>Colors</h1>
+        <h2 className={s.sectionHeader}>Brand colors</h2>
+        <div className={s.colorsBlock}>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.brand01)} />
+            <h3 className={s.blockHeader}>$brand-01</h3>
           </div>
-          <h2 className={s.sectionHeader}>Typography</h2>
-          <div className={s.colorsBlock}>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.text01)} />
-              <h3 className={s.blockHeader}>$text-01</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.text02)} />
-              <h3 className={s.blockHeader}>$text-02</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.text03)} />
-              <h3 className={s.blockHeader}>$text-03</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.textInverse)} />
-              <h3 className={s.blockHeader}>$text-inverse</h3>
-            </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.brand02)} />
+            <h3 className={s.blockHeader}>$brand-02</h3>
           </div>
-          <h2 className={s.sectionHeader}>Surface / Background</h2>
-          <div className={s.colorsBlock}>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui01)} />
-              <h3 className={s.blockHeader}>$ui-01</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui02)} />
-              <h3 className={s.blockHeader}>$ui-02</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui03)} />
-              <h3 className={s.blockHeader}>$ui-03</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui04)} />
-              <h3 className={s.blockHeader}>$ui-04</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui05)} />
-              <h3 className={s.blockHeader}>$ui-05</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui06)} />
-              <h3 className={s.blockHeader}>$ui-06</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.ui07)} />
-              <h3 className={s.blockHeader}>$ui-07</h3>
-            </div>
+        </div>
+        <h2 className={s.sectionHeader}>Typography</h2>
+        <div className={s.colorsBlock}>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.text01)} />
+            <h3 className={s.blockHeader}>$text-01</h3>
           </div>
-          <h2 className={s.sectionHeader}>Hover state</h2>
-          <div className={s.colorsBlock}>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.hoverPrimary01)} />
-              <h3 className={s.blockHeader}>$hover-primary-01</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.hoverPrimary02)} />
-              <h3 className={s.blockHeader}>$hover-primary-02</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.hoverLightRow)} />
-              <h3 className={s.blockHeader}>$hover-light-row</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.hoverDarkRow)} />
-              <h3 className={s.blockHeader}>$hover-dark-row</h3>
-            </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.text02)} />
+            <h3 className={s.blockHeader}>$text-02</h3>
           </div>
-          <h2 className={s.sectionHeader}>Support</h2>
-          <div className={s.colorsBlock}>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.supportSuccess)} />
-              <h3 className={s.blockHeader}>$support-success</h3>
-            </div>
-            <div className={s.color}>
-              <span className={cx(s.colorInner, s.supportError)} />
-              <h3 className={s.blockHeader}>$support-error</h3>
-            </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.text03)} />
+            <h3 className={s.blockHeader}>$text-03</h3>
           </div>
-        </Container>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.textInverse)} />
+            <h3 className={s.blockHeader}>$text-inverse</h3>
+          </div>
+        </div>
+        <h2 className={s.sectionHeader}>Surface / Background</h2>
+        <div className={s.colorsBlock}>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui01)} />
+            <h3 className={s.blockHeader}>$ui-01</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui02)} />
+            <h3 className={s.blockHeader}>$ui-02</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui03)} />
+            <h3 className={s.blockHeader}>$ui-03</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui04)} />
+            <h3 className={s.blockHeader}>$ui-04</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui05)} />
+            <h3 className={s.blockHeader}>$ui-05</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui06)} />
+            <h3 className={s.blockHeader}>$ui-06</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.ui07)} />
+            <h3 className={s.blockHeader}>$ui-07</h3>
+          </div>
+        </div>
+        <h2 className={s.sectionHeader}>Hover state</h2>
+        <div className={s.colorsBlock}>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.hoverPrimary01)} />
+            <h3 className={s.blockHeader}>$hover-primary-01</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.hoverPrimary02)} />
+            <h3 className={s.blockHeader}>$hover-primary-02</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.hoverLightRow)} />
+            <h3 className={s.blockHeader}>$hover-light-row</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.hoverDarkRow)} />
+            <h3 className={s.blockHeader}>$hover-dark-row</h3>
+          </div>
+        </div>
+        <h2 className={s.sectionHeader}>Support</h2>
+        <div className={s.colorsBlock}>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.supportSuccess)} />
+            <h3 className={s.blockHeader}>$support-success</h3>
+          </div>
+          <div className={s.color}>
+            <span className={cx(s.colorInner, s.supportError)} />
+            <h3 className={s.blockHeader}>$support-error</h3>
+          </div>
+        </div>
       </section>
       <section className={s.section}>
-        <Container>
-          <h1 className={s.header}>Elevation</h1>
-          <div className={s.elevationBlock}>
-            <div className={s.elevation}>
-              $elevation-01
-            </div>
-            <div className={cx(s.elevation, s.elevation02)}>
-              $elevation-02
-            </div>
-            <div className={cx(s.elevation, s.elevation03)}>
-              $elevation-03
-            </div>
-            <div className={cx(s.elevation, s.elevation04)}>
-              $elevation-04
-            </div>
-            <div className={cx(s.elevation, s.elevation05)}>
-              $elevation-05
-            </div>
-            <div className={cx(s.elevation, s.elevation06)}>
-              $elevation-06
-            </div>
+        <h1 className={s.header}>Elevation</h1>
+        <div className={s.elevationBlock}>
+          <div className={s.elevation}>
+            $elevation-01
           </div>
-        </Container>
+          <div className={cx(s.elevation, s.elevation02)}>
+            $elevation-02
+          </div>
+          <div className={cx(s.elevation, s.elevation03)}>
+            $elevation-03
+          </div>
+          <div className={cx(s.elevation, s.elevation04)}>
+            $elevation-04
+          </div>
+          <div className={cx(s.elevation, s.elevation05)}>
+            $elevation-05
+          </div>
+          <div className={cx(s.elevation, s.elevation06)}>
+            $elevation-06
+          </div>
+        </div>
       </section>
       <section className={s.section}>
-        <Container>
-          <h1 className={s.header}>Buttons Light</h1>
-          <div className={s.buttonsBlock}>
-            <Button
-              className={s.button}
-            >
-              Primary
-            </Button>
-            <Button
-              className={s.button}
-              disabled
-            >
-              Primary
-            </Button>
-          </div>
-          <div className={s.buttonsBlock}>
-            <Button
-              className={s.button}
-              theme="secondary"
-            >
-              Secondary
-            </Button>
-            <Button
-              className={s.button}
-              disabled
-              theme="secondary"
-            >
-              Secondary
-            </Button>
-          </div>
-          <div className={s.buttonsBlock}>
-            <Button
-              className={s.button}
-              theme="tertiary"
-            >
-              Tertiary
-            </Button>
-            <Button
-              className={s.button}
-              disabled
-              theme="tertiary"
-            >
-              Tertiary
-            </Button>
-          </div>
-          <div className={s.buttonsBlock}>
-            <Button
-              className={s.button}
-              theme="quaternary"
-            >
-              Quaternary
-            </Button>
-            <Button
-              className={s.button}
-              disabled
-              theme="quaternary"
-            >
-              Quaternary
-            </Button>
-          </div>
-        </Container>
+        <h1 className={s.header}>Buttons Light</h1>
+        <div className={s.buttonsBlock}>
+          <Button
+            className={s.button}
+          >
+            Primary
+          </Button>
+          <Button
+            className={s.button}
+            disabled
+          >
+            Primary
+          </Button>
+        </div>
+        <div className={s.buttonsBlock}>
+          <Button
+            className={s.button}
+            theme="secondary"
+          >
+            Secondary
+          </Button>
+          <Button
+            className={s.button}
+            disabled
+            theme="secondary"
+          >
+            Secondary
+          </Button>
+        </div>
+        <div className={s.buttonsBlock}>
+          <Button
+            className={s.button}
+            theme="tertiary"
+          >
+            Tertiary
+          </Button>
+          <Button
+            className={s.button}
+            disabled
+            theme="tertiary"
+          >
+            Tertiary
+          </Button>
+        </div>
+        <div className={s.buttonsBlock}>
+          <Button
+            className={s.button}
+            theme="quaternary"
+          >
+            Quaternary
+          </Button>
+          <Button
+            className={s.button}
+            disabled
+            theme="quaternary"
+          >
+            Quaternary
+          </Button>
+        </div>
       </section>
       <section className={s.section}>
-        <Container>
-          <h1 className={s.header}>Toggle Color theme</h1>
-          <ColorModeSwitcher />
-        </Container>
+        <h1 className={s.header}>Toggle Color theme</h1>
+        <ColorModeSwitcher />
       </section>
       <section className={s.section}>
-        <Container>
-          <h1 className={s.header}>Icons</h1>
-          <div className={s.iconsBlock}>
-            <Logo className={s.icon} />
-            <MenuClosed className={s.icon} />
-            <MenuOpened className={s.icon} />
-          </div>
-        </Container>
+        <h1 className={s.header}>Icons</h1>
+        <div className={s.iconsBlock}>
+          <Logo className={s.icon} />
+          <MenuClosed className={s.icon} />
+          <MenuOpened className={s.icon} />
+        </div>
       </section>
     </BaseLayout>
   );
