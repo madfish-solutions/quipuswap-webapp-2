9134b953466011e97c8ca22a2d0207296171dd7c
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-07-15 20:09:15 +0300

    added complex recipient component

--- src/components/ui/ComplexInput/ComplexRecipient.tsx
+++ src/components/ui/ComplexInput/ComplexRecipient.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames';
 import { useTranslation } from 'next-i18next';
 import { ColorModes, ColorThemeContext } from '@providers/ColorThemeContext';
 
+import { isTouchDevice } from '@utils/helpers';
 import { Button } from '../Button';
 
 import s from './ComplexInput.module.sass';
@@ -10,7 +11,9 @@ import s from './ComplexInput.module.sass';
 type ComplexRecipientProps = {
   className?: string,
   label?:string,
-} & React.HTMLProps<HTMLInputElement>;
+} & (
+  | React.HTMLProps<HTMLTextAreaElement>
+  | React.HTMLProps<HTMLInputElement>);
 
 const modeClass = {
   [ColorModes.Light]: s.light,
@@ -31,6 +34,7 @@ export const ComplexRecipient: React.FC<ComplexRecipientProps> = ({
   const compoundClassName = cx(
     { [s.focused]: focused },
     modeClass[colorThemeMode],
+    s.recipientBase,
     className,
   );
 
@@ -49,16 +53,23 @@ export const ComplexRecipient: React.FC<ComplexRecipientProps> = ({
       <div className={s.background}>
 
         <div className={s.shape}>
-          <div className={cx(s.item1, s.label2)} />
-          <div className={s.item2} />
-
-          <input
-            className={cx(s.item3, s.input)}
-            {...props}
-            value={value}
-            onChange={(e:React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
-          />
-          <Button onMouseDown={(e:React.MouseEvent<HTMLButtonElement>) => { e.preventDefault(); e.stopPropagation(); }} theme="quaternary" className={cx(s.item4)}>
+          {isTouchDevice()
+            ? (
+              <textarea
+                className={cx(s.item1, s.input, s.recipient)}
+                {...props as React.HTMLProps<HTMLTextAreaElement>}
+                value={value}
+                onChange={(e:React.ChangeEvent<HTMLTextAreaElement>) => onChange(e.target.value)}
+              />
+            ) : (
+              <input
+                className={cx(s.item1, s.input, s.recipient)}
+                {...props as React.HTMLProps<HTMLInputElement>}
+                value={value}
+                onChange={(e:React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
+              />
+            )}
+          <Button onMouseDown={(e:React.MouseEvent<HTMLButtonElement>) => { e.preventDefault(); e.stopPropagation(); }} theme="quaternary" className={s.pasteWrap}>
             <div className={cx(s.paste)}>
               {t('swap:Paste')}
             </div>
