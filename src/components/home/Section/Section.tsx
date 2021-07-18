04fa669b2834cb6113e69c4420286b67e40285d0
olehkhalin <olehkhalin@gmail.com>
2021-07-18 13:59:45 +0300

    Edited section

--- src/components/home/Section/Section.tsx
+++ src/components/home/Section/Section.tsx
@@ -4,17 +4,20 @@ import cx from 'classnames';
 import s from './Section.module.sass';
 
 type SectionProps = {
-  header?: string
+  header: string
+  description: string
   className?: string
 };
 
 export const Section: React.FC<SectionProps> = ({
   header,
+  description,
   className,
   children,
 }) => (
   <section className={cx(s.root, className)}>
-    {header && (<h2 className={s.header}>{header}</h2>)}
+    <h2 className={s.header}>{header}</h2>
+    <p className={s.description}>{description}</p>
     {children}
   </section>
 );
