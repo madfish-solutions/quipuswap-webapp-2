a473fc4e42d172541c9fc9f0cdb25294459b5429
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-08-05 15:32:05 +0300

    fix lint

--- src/utils/dapp/tokensMetadata.ts
+++ src/utils/dapp/tokensMetadata.ts
@@ -1,9 +1,8 @@
 import {
   METADATA_API,
 } from '@utils/defaults';
-import { QSNetwork } from '@utils/types';
 
-export const getTokenMetadata = async (network:QSNetwork, address:string, tokenId?:number) => {
+export const getTokenMetadata = async (address:string, tokenId?:number) => {
   const data = await fetch(`${METADATA_API}/${address}/${tokenId || 0}`)
     .then((res) => res.json())
     .catch(() => (null));
