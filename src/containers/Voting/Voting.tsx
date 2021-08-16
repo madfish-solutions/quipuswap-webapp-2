fe94d324bfdc3060231b9de175918e26b5740c2c
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-08-16 11:13:13 +0300

    refactored

--- src/containers/Voting/Voting.tsx
+++ src/containers/Voting/Voting.tsx
@@ -5,15 +5,11 @@ import BigNumber from 'bignumber.js';
 import { withTypes, Field, FormSpy } from 'react-final-form';
 import {
   batchify,
-  estimateReward,
-  findDex,
   FoundDex,
-  getLiquidityShare,
   TransferParams,
   vetoCurrentBaker,
   voteForBaker,
 } from '@quipuswap/sdk';
-import { TezosToolkit } from '@taquito/taquito';
 
 import { useConnectModalsState } from '@hooks/useConnectModalsState';
 import {
@@ -25,7 +21,7 @@ import { parseDecimals } from '@utils/helpers';
 import {
   VoterType, WhitelistedToken, WhitelistedTokenPair,
 } from '@utils/types';
-import { FACTORIES, TEZOS_TOKEN } from '@utils/defaults';
+import { TEZOS_TOKEN } from '@utils/defaults';
 import { VotingStats } from '@components/voting/VotingStats';
 import { Card } from '@components/ui/Card';
 import { Tabs } from '@components/ui/Tabs';
@@ -38,6 +34,7 @@ import { ArrowDown } from '@components/svg/ArrowDown';
 
 import s from '@styles/CommonContainer.module.sass';
 import { VotingDetails } from './VotingDetails';
+import { hanldeTokenPairSelect } from './votingHelpers';
 
 const TabsContent = [
   {
@@ -108,72 +105,6 @@ type HeaderProps = {
 };
 
 type QSMainNet = 'mainnet' | 'florencenet';
-
-const hanldeTokenPairSelect = (
-  pair: WhitelistedTokenPair,
-  setTokenPair: (pair: WhitelistedTokenPair) => void,
-  setDex: (dex: FoundDex) => void,
-  setRewards: (reward: string) => void,
-  setVoter: (voter: VoterType) => any,
-  tezos: TezosToolkit | null,
-  accountPkh: string | null,
-  networkId?: QSMainNet,
-) => {
-  const asyncFunc = async () => {
-    if (!tezos || !accountPkh || !networkId) {
-      setTokenPair(pair);
-      return;
-    }
-    try {
-      const secondAsset = {
-        contract: pair.token2.contractAddress,
-        id: pair.token2.fa2TokenId,
-      };
-      const foundDex = await findDex(tezos, FACTORIES[networkId], secondAsset);
-      setDex(foundDex);
-      const asyncRewards = async () => {
-        const res = await estimateReward(tezos, foundDex, accountPkh);
-        const rewards = res.div(new BigNumber(10).pow(new BigNumber(6))).toString();
-        setRewards(rewards);
-      };
-      asyncRewards();
-      const asyncVoter = async () => {
-        const voter = await foundDex.storage.storage.voters.get(accountPkh);
-        if (voter) {
-          setVoter({
-            veto: voter.veto.div(new BigNumber(10).pow(new BigNumber(6))).toString(),
-            candidate: voter.candidate,
-            vote: voter.vote.div(new BigNumber(10).pow(new BigNumber(6))).toString(),
-          });
-        } else setVoter({} as VoterType);
-      };
-      asyncVoter();
-      const share = await getLiquidityShare(tezos, foundDex, accountPkh!!);
-
-      const frozenBalance = share.frozen.div(
-        new BigNumber(10)
-          .pow(
-            // TODO: allow token->token
-            new BigNumber(6),
-          ),
-      ).toString();
-      const totalBalance = share.total.div(
-        new BigNumber(10)
-          .pow(
-            // new BigNumber(pair.token2.metadata.decimals),
-            new BigNumber(6),
-          ),
-      ).toString();
-      const res = {
-        ...pair, frozenBalance, balance: totalBalance, dex: foundDex,
-      };
-      setTokenPair(res);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  asyncFunc();
-};
 
 const Header: React.FC<HeaderProps> = ({
   handleSubmit,
