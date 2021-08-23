1618ab571538cb18d148e7346039462dddaa4d6c
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-08-23 11:32:58 +0300

    wip

---
+++ src/pages/liquidity/[from-to].tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { BaseLayout } from '@layouts/BaseLayout';
+import { Liquidity } from '@containers/Liquidity';
+import { LineChartSampleData } from '@components/ui/LineChart/content';
+
+import s from '@styles/SwapLiquidity.module.sass';
+
+const LineChart = dynamic(() => import('@components/ui/LineChart'), {
+  ssr: false,
+});
+
+const LiquidityPage: React.FC = () => {
+  const { t } = useTranslation(['common', 'swap']);
+
+  return (
+    <BaseLayout
+      title={t('swap:Liquidity page')}
+      description={t('swap:Liquidity page description. Couple sentences...')}
+      className={s.wrapper}
+    >
+      <Liquidity />
+      <LineChart className={s.chart} data={LineChartSampleData} />
+    </BaseLayout>
+  );
+};
+
+export const getStaticPaths = async () => ({
+  paths: [],
+  fallback: true,
+});
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...await serverSideTranslations(locale, ['common', 'swap']),
+  },
+});
+
+export default LiquidityPage;
