5e7e3cc502010fe42bf5bc4b80121e4efca59427
olehkhalin <olehkhalin@gmail.com>
2021-08-31 13:25:13 +0300

    Redirects

--- next.config.js
+++ next.config.js
@@ -25,6 +25,21 @@ module.exports = withReactSvg({
         permanent: true,
       },
       {
+        source: '/liquidity/:slug',
+        destination: `/liquidity/add/:slug`,
+        permanent: true,
+      },
+      {
+        source: '/invest/add-liquidity/:slug',
+        destination: `/liquidity/add/TEZ-:slug`,
+        permanent: true,
+      },
+      {
+        source: '/invest/add-token/:slug',
+        destination: `/liquidity/add/TEZ-:slug`,
+        permanent: true,
+      },
+      {
         source: '/liquidity/add',
         destination: `/liquidity/add/${process.env.DEFAULT_SWAP_URI}`,
         permanent: true,
@@ -32,6 +47,11 @@ module.exports = withReactSvg({
       {
         source: '/liquidity/remove',
         destination: `/liquidity/remove/${process.env.DEFAULT_SWAP_URI}`,
+        permanent: true,
+      },
+      {
+        source: '/invest/remove-liquidity/:slug',
+        destination: `/liquidity/remove/TEZ-:slug`,
         permanent: true,
       },
     ]
