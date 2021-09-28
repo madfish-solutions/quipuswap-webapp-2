6cc836de366f120ebfb275c7252f652ba0b2eb0c
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-09-28 17:13:00 +0300

    review fixes

--- src/components/home/DexDashboard/DexDashboard.tsx
+++ src/components/home/DexDashboard/DexDashboard.tsx
@@ -1,5 +1,5 @@
 import React, {
-  useContext, useState, useEffect, useMemo,
+  useState, useEffect,
 } from 'react';
 import cx from 'classnames';
 import { useTranslation } from 'next-i18next';
@@ -7,32 +7,23 @@ import BigNumber from 'bignumber.js';
 import { TezosToolkit } from '@taquito/taquito';
 
 import { useGetHomeOverviewQuery } from '@graphql';
-import { ColorModes, ColorThemeContext } from '@providers/ColorThemeContext';
-import { fromDecimals } from '@utils/helpers';
 import { getStorageInfo } from '@utils/dapp';
 import { MAINNET_NETWORK, STABLE_TOKEN } from '@utils/defaults';
 import { Section } from '@components/home/Section';
 import { Card } from '@components/ui/Card';
 import { SliderUI } from '@components/ui/Slider';
 
-import { DashboardCard } from './DashboardCard';
 import s from './DexDashboard.module.sass';
+import { DexDashboardInner } from './DexDashboardInner';
 
 type DexDashboardProps = {
   className?: string
-};
-
-const modeClass = {
-  [ColorModes.Light]: s.light,
-  [ColorModes.Dark]: s.dark,
 };
 
 export const DexDashboard: React.FC<DexDashboardProps> = ({
   className,
 }) => {
   const { t } = useTranslation(['home']);
-
-  const { colorThemeMode } = useContext(ColorThemeContext);
   const { loading, data, error } = useGetHomeOverviewQuery();
   const [totalSupply, setTotalSupply] = useState<BigNumber>();
 
@@ -47,19 +38,6 @@ export const DexDashboard: React.FC<DexDashboardProps> = ({
     asyncLoad();
   }, []);
 
-  const tvl = useMemo(() => fromDecimals(
-    new BigNumber(data?.overview?.totalLiquidity ?? '0'), 6,
-  )
-    .multipliedBy(new BigNumber(data?.overview?.xtzUsdQuote ?? '0'))
-    .toFixed(0), [data]);
-
-  const volume24h = useMemo(() => fromDecimals(
-    new BigNumber(data?.overview?.volume24h ?? '0'), 6,
-  )
-    .multipliedBy(new BigNumber(data?.overview?.xtzUsdQuote ?? '0'))
-    .toFixed(0), [data]);
-  const transactions24h = useMemo(() => new BigNumber(data?.overview?.trasactionsCount24h ?? '0').toString(), [data]);
-
   return (
     <Section
       header={t('home|DEX Dashboard')}
@@ -70,79 +48,25 @@ export const DexDashboard: React.FC<DexDashboardProps> = ({
         className={(s.mobile)}
         contentClassName={s.mobContent}
       >
-        <SliderUI className={s.mobSlider}>
-          <DashboardCard
-            className={cx(s.card, modeClass[colorThemeMode])}
-            size="extraLarge"
-            volume={tvl}
-            tooltip={t('home|TVL (Total Value Locked) represents the total amount of assets currently locked on a DeFi platform. In the case of a DEX it also represents the overall volume of liquidity.')}
-            label={t('home|TVL')}
-            currency="$"
-            loading={loading || !!error}
-          />
-          <DashboardCard
-            className={cx(s.card, modeClass[colorThemeMode])}
-            size="extraLarge"
-            volume={volume24h}
-            tooltip={t('home|The accumulated cost of all assets traded via QuipuSwap today.')}
-            label={t('home|Daily Volume')}
-            currency="$"
-            loading={loading || !!error}
-          />
-          <DashboardCard
-            className={cx(s.card, modeClass[colorThemeMode])}
-            size="extraLarge"
-            volume={transactions24h}
-            tooltip={t('home|The overall number of transactions conducted on QuipuSwap today.')}
-            label={t('home|Daily Transaction')}
-            loading={loading || !!error}
-          />
-          <DashboardCard
-            className={cx(s.card, modeClass[colorThemeMode])}
-            size="extraLarge"
-            volume={new BigNumber(totalSupply ?? '0').toString()}
-            tooltip={t('home|The current number of available QUIPU tokens.')}
-            label={t('home|Total supply')}
-            currency="QUIPU"
-            loading={totalSupply === undefined}
-          />
-        </SliderUI>
+        <>
+          <SliderUI className={s.mobSlider}>
+            <DexDashboardInner
+              totalLiquidity={data?.overview?.totalLiquidity}
+              xtzUsdQuote={data?.overview?.xtzUsdQuote}
+              trasactionsCount24h={data?.overview?.trasactionsCount24h}
+              totalSupply={totalSupply}
+              loading={loading || !!error}
+            />
+          </SliderUI>
+        </>
       </Card>
       <Card className={s.desktop} contentClassName={s.content}>
-        <DashboardCard
-          className={cx(s.card, modeClass[colorThemeMode])}
-          size="extraLarge"
-          volume={tvl}
-          tooltip={t('home|TVL (Total Value Locked) represents the total amount of assets currently locked on a DeFi platform. In the case of a DEX it also represents the overall volume of liquidity.')}
-          label={t('home|TVL')}
-          currency="$"
+        <DexDashboardInner
+          totalLiquidity={data?.overview?.totalLiquidity}
+          xtzUsdQuote={data?.overview?.xtzUsdQuote}
+          trasactionsCount24h={data?.overview?.trasactionsCount24h}
+          totalSupply={totalSupply}
           loading={loading || !!error}
-        />
-        <DashboardCard
-          className={cx(s.card, modeClass[colorThemeMode])}
-          size="extraLarge"
-          volume={volume24h}
-          tooltip={t('home|The accumulated cost of all assets traded via QuipuSwap today.')}
-          label={t('home|Daily Volume')}
-          currency="$"
-          loading={loading || !!error}
-        />
-        <DashboardCard
-          className={cx(s.card, modeClass[colorThemeMode])}
-          size="extraLarge"
-          volume={transactions24h}
-          tooltip={t('home|The overall number of transactions conducted on QuipuSwap today.')}
-          label={t('home|Daily Transaction')}
-          loading={loading || !!error}
-        />
-        <DashboardCard
-          className={cx(s.card, modeClass[colorThemeMode])}
-          size="extraLarge"
-          volume={new BigNumber(totalSupply ?? '0').toString()}
-          tooltip={t('home|The current number of available QUIPU tokens.')}
-          label={t('home|Total supply')}
-          currency="QUIPU"
-          loading={totalSupply === undefined}
         />
       </Card>
     </Section>
