3fa57df83bfb2ed5bf4655e174c813bbc692c955
Dmytro <herkodmytro@gmail.com>
2021-10-01 16:37:59 +0300

    Removed florencenet from QSMainNet

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,7 +2,7 @@ import { FoundDex } from '@quipuswap/sdk';
 import BigNumber from 'bignumber.js';
 import { BigMapAbstraction } from '@taquito/taquito';
 
-export type QSMainNet = 'mainnet' | 'florencenet' | 'granadanet';
+export type QSMainNet = 'mainnet' | 'granadanet';
 
 type QSNetworkType =
   | 'mainnet'
