8062c70d63cb3b00772ea29da4b4570659b4e292
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-10-27 10:05:31 +0300

    deleted oldversion button

--- src/components/common/Header/Sidebar/Sidebar.tsx
+++ src/components/common/Header/Sidebar/Sidebar.tsx
@@ -1,10 +1,8 @@
 import React, { useContext } from 'react';
 import cx from 'classnames';
-import { useTranslation } from 'next-i18next';
 
 import { ColorModes, ColorThemeContext } from '@providers/ColorThemeContext';
 import { ConnectWalletButton } from '@components/common/ConnectWalletButton';
-import { NetworkSelect } from '@components/common/NetworkSelect';
 import { Button } from '@components/ui/Button';
 import { Madfish } from '@components/svg/Madfish';
 
@@ -14,7 +12,7 @@ import { QPToken } from '../QPToken';
 import s from './Sidebar.module.sass';
 
 type SidebarProps = {
-  className?: string
+  className?: string;
 };
 
 const modeClass = {
@@ -22,31 +20,19 @@ const modeClass = {
   [ColorModes.Dark]: s.dark,
 };
 
-export const Sidebar: React.FC<SidebarProps> = ({
-  className,
-}) => {
-  const { t } = useTranslation(['common']);
+export const Sidebar: React.FC<SidebarProps> = ({ className }) => {
   const { colorThemeMode } = useContext(ColorThemeContext);
 
   return (
     <div className={cx(s.root, modeClass[colorThemeMode], className)}>
       <div className={s.wallet}>
         <ConnectWalletButton className={s.button} />
-        <NetworkSelect className={s.button} />
-        <Button external href="https://quipuswap.com/" theme="secondary" className={s.button}>
-          {t('common|Old version')}
-        </Button>
       </div>
       <Navigation className={s.navigation} iconId="desktop" />
       <footer className={s.footer}>
         <QPToken className={s.token} id="desktop" />
         <Socials className={s.socials} id="desktop" />
-        <Button
-          href="https://www.madfish.solutions/"
-          external
-          theme="clean"
-          className={s.madfish}
-        >
+        <Button href="https://www.madfish.solutions/" external theme="clean" className={s.madfish}>
           <Madfish />
         </Button>
       </footer>
