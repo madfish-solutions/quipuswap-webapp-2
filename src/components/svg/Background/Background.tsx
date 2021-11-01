0d55b96f35dc6dcbc0e5489ca2fc31a054637801
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-01 15:47:09 +0200

    refactor: use advice from review

--- src/components/svg/Background/Background.tsx
+++ src/components/svg/Background/Background.tsx
@@ -1,19 +1,21 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 
-import {ColorModes, ColorThemeContext} from '@providers/ColorThemeContext';
-import {isClient} from '@utils/helpers';
+import { ColorModes as ColorMode, ColorThemeContext } from '@providers/ColorThemeContext';
+import { isClient } from '@utils/helpers';
 
-export const Background: React.FC<IconProps> = ({className}) => {
-  const {colorThemeMode} = useContext(ColorThemeContext);
+function getBackgroundPath(isDesktop: boolean, themeMode: ColorMode = ColorMode.Light) {
+  return `/svg/${isDesktop ? 'Desktop' : 'Mobile'}${themeMode}.svg`;
+}
+
+export const Background: React.FC<IconProps> = ({ className }) => {
+  const { colorThemeMode } = useContext(ColorThemeContext);
   if (!isClient) return null;
-  if (window.innerWidth > 1024) {
-    if (colorThemeMode === ColorModes.Dark) {
-      return <img src="/svg/DesktopDark.svg" alt="background" className={className} />;
-    }
-    return <img src="/svg/DesktopLight.svg" alt="background" className={className} />;
-  }
-  if (colorThemeMode === ColorModes.Dark) {
-    return <img src="/svg/MobileDark.svg" alt="background" className={className} />;
-  }
-  return <img src="/svg/MobileLight.svg" alt="background" className={className} />;
+  const isDesktop = window.innerWidth > 1024;
+  return (
+    <img
+      src={getBackgroundPath(isDesktop, colorThemeMode)}
+      alt="background"
+      className={className}
+    />
+  );
 };
