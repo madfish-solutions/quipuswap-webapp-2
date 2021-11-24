f6c0256ea84e967c3e0ed2d9fdebbbefacc639ab
Dmytro <herkodmytro@gmail.com>
2021-11-24 11:22:45 +0200

    Trying to get token/token from contract

--- src/containers/Liquidity/LiquidityForms/LiquidityForm.tsx
+++ src/containers/Liquidity/LiquidityForms/LiquidityForm.tsx
@@ -12,7 +12,7 @@ import {
   Token,
   findDex,
   FoundDex,
-  // getLiquidityShare,
+  getLiquidityShare,
 } from '@quipuswap/sdk';
 import { FormSpy } from 'react-final-form';
 import router from 'next/router';
@@ -20,14 +20,14 @@ import router from 'next/router';
 import {
   useTezos,
   useNetwork,
-  // useAccountPkh,
-  // getUserBalance,
+  useAccountPkh,
+  getUserBalance,
   getContract,
   getStorageInfo,
 } from '@utils/dapp';
 import {
   QSMainNet,
-  // PoolShare,
+  PoolShare,
   TokenDataMap,
   WhitelistedTokenPair,
 } from '@utils/types';
@@ -36,14 +36,16 @@ import {
   TEZOS_TOKEN,
   QUIPU_TOKEN,
 } from '@utils/defaults';
-// import { fromDecimals, getWhitelistedTokenSymbol } from '@utils/helpers';
-import { getWhitelistedTokenSymbol } from '@utils/helpers';
+import { fromDecimals, getWhitelistedTokenSymbol } from '@utils/helpers';
 import { Transactions } from '@components/svg/Transactions';
 
-// import { LiquidityFormAdd } from './LiquidityFormAdd';
-// import { LiquidityDetails } from '../LiquidityDetails';
-// import { LiquidityFormRemove } from './LiquidityFormRemove';
+import { LiquidityFormAdd } from './LiquidityFormAdd';
+import { LiquidityDetails } from '../LiquidityDetails';
+import { LiquidityFormRemove } from './LiquidityFormRemove';
 import s from '../Liquidity.module.sass';
+
+const taquitoUtils = require('@taquito/utils');
+const MichelCodec = require('@taquito/michel-codec');
 
 const fallbackTokenPair = {
   token1: TEZOS_TOKEN,
@@ -65,27 +67,23 @@ type LiquidityFormProps = {
   tokensData: TokenDataMap;
 };
 
-// const RealForm:React.FC<LiquidityFormProps> = ({ tokensData }) => {
-const RealForm:React.FC<LiquidityFormProps> = () => {
+const RealForm:React.FC<LiquidityFormProps> = ({ tokensData }) => {
   const tezos = useTezos();
   const networkId = useNetwork().id as QSMainNet;
-  // const accountPkh = useAccountPkh();
+  const accountPkh = useAccountPkh();
 
-  // const [dex, setDex] = useState<FoundDex | null>(null);
-  const [, setDex] = useState<FoundDex | null>(null);
-  // const [poolShare, setPoolShare] = useState<PoolShare>();
+  const [dex, setDex] = useState<FoundDex | null>(null);
+  const [isTezToTokenDex, setIsTezToTokenDex] = useState(true);
+  const [poolShare, setPoolShare] = useState<PoolShare>();
   const [tabState, setTabState] = useState(TabsContent[0]);
-  // const [tokenA, setTokenA] = useState(TEZOS_TOKEN);
-  const [tokenA] = useState(TEZOS_TOKEN);
-  // const [tokenB, setTokenB] = useState(QUIPU_TOKEN);
-  const [tokenB] = useState(QUIPU_TOKEN);
-  // const [tokenABalance, setTokenABalance] = useState<string>('0');
-  // const [tokenBBalance, setTokenBBalance] = useState<string>('0');
-  // const [lpTokenBalance, setLpTokenBalance] = useState<string>('0');
+  const [tokenA, setTokenA] = useState(TEZOS_TOKEN);
+  const [tokenB, setTokenB] = useState(QUIPU_TOKEN);
+  const [tokenABalance, setTokenABalance] = useState<string>('0');
+  const [tokenBBalance, setTokenBBalance] = useState<string>('0');
+  const [lpTokenBalance, setLpTokenBalance] = useState<string>('0');
 
   useEffect(() => {
     let isLoadDex = true;
-
     const loadDex = async () => {
       if (!tezos) return;
 
@@ -105,13 +103,13 @@ const RealForm:React.FC<LiquidityFormProps> = () => {
         let foundDex:FoundDex;
         if (isTezInPair) {
           foundDex = await findDex(tezos, FACTORIES[networkId], token);
+          setIsTezToTokenDex(true);
         } else {
           const contract = await getContract(tezos, 'KT1SumF3C6ZRKHpDsctzbeqw9rMHQk1ATR4H');
           const storage = await getStorageInfo(tezos, 'KT1SumF3C6ZRKHpDsctzbeqw9rMHQk1ATR4H');
           foundDex = new FoundDex(contract, storage);
+          setIsTezToTokenDex(false);
         }
-
-        console.log({ foundDex });
 
         if (isLoadDex) setDex(foundDex);
       } catch (error) {
@@ -123,56 +121,134 @@ const RealForm:React.FC<LiquidityFormProps> = () => {
     return () => { isLoadDex = false; };
   }, [tezos, networkId, tokenB, tokenA]);
 
-  // useEffect(() => {
-  //   let isLoadBalances = true;
-  //   const getTokensBalances = async () => {
-  //     if (!tezos || !accountPkh) return;
+  useEffect(() => {
+    let isLoadBalance = true;
+    const getTokenABalance = async () => {
+      if (!tezos || !accountPkh) return;
 
-  //     const userTokenABalanance = await getUserBalance(
-  //       tezos,
-  //       accountPkh,
-  //       tokenA.contractAddress,
-  //       tokenA.type,
-  //       tokenA.fa2TokenId,
-  //     );
-  //     const userTokenBBalance = await getUserBalance(
-  //       tezos,
-  //       accountPkh,
-  //       tokenB.contractAddress,
-  //       tokenB.type,
-  //       tokenB.fa2TokenId,
-  //     );
+      const userTokenABalanance = await getUserBalance(
+        tezos,
+        accountPkh,
+        tokenA.contractAddress,
+        tokenA.type,
+        tokenA.fa2TokenId,
+      );
 
-  //     const userLpTokenBalance = dex && (await getUserBalance(
-  //       tezos,
-  //       accountPkh,
-  //       dex.contract.address,
-  //       tokenB.type,
-  //       tokenB.fa2TokenId,
-  //     ));
+      if (userTokenABalanance && isLoadBalance) {
+        setTokenABalance(fromDecimals(userTokenABalanance, tokenA.metadata.decimals).toFixed());
+      } else if (!userTokenABalanance && isLoadBalance) {
+        setTokenABalance('0');
+      }
+    };
+    getTokenABalance();
 
-  //     if (userTokenABalanance && isLoadBalances) {
-  //       setTokenABalance(fromDecimals(userTokenABalanance, tokenA.metadata.decimals).toFixed());
-  //     } else if (!userTokenABalanance && isLoadBalances) {
-  //       setTokenABalance('0');
-  //     }
+    return () => { isLoadBalance = false; };
+  }, [tezos, accountPkh, dex, tokenA]);
 
-  //     if (userTokenBBalance && isLoadBalances) {
-  //       setTokenBBalance(fromDecimals(userTokenBBalance, tokenB.metadata.decimals).toFixed());
-  //     } else if (!userTokenBBalance && isLoadBalances) {
-  //       setTokenBBalance('0');
-  //     }
+  useEffect(() => {
+    let isLoadBalance = true;
+    const getTokenBBalance = async () => {
+      if (!tezos || !accountPkh) return;
 
-  //     if (userLpTokenBalance && isLoadBalances) {
-  //       setLpTokenBalance(fromDecimals(userLpTokenBalance, 6).toFixed());
-  //     } else if (!userLpTokenBalance && isLoadBalances) {
-  //       setLpTokenBalance('0');
-  //     }
-  //   };
-  //   getTokensBalances();
+      const userTokenBBalance = await getUserBalance(
+        tezos,
+        accountPkh,
+        tokenB.contractAddress,
+        tokenB.type,
+        tokenB.fa2TokenId,
+      );
 
-  //   return () => { isLoadBalances = false; };
-  // }, [tezos, accountPkh, dex]);
+      if (userTokenBBalance && isLoadBalance) {
+        setTokenBBalance(fromDecimals(userTokenBBalance, tokenB.metadata.decimals).toFixed());
+      } else if (!userTokenBBalance && isLoadBalance) {
+        setTokenBBalance('0');
+      }
+    };
+    getTokenBBalance();
+
+    return () => { isLoadBalance = false; };
+  }, [tezos, accountPkh, dex, tokenB]);
+
+  useEffect(() => {
+    let isLoadBalance = true;
+    const getLpTokenBalance = async () => {
+      if (!tezos || !accountPkh) return;
+
+      const userLpTokenBalance = dex && (await getUserBalance(
+        tezos,
+        accountPkh,
+        dex.contract.address,
+        tokenB.type,
+        tokenB.fa2TokenId,
+      ));
+
+      if (userLpTokenBalance && isLoadBalance) {
+        setLpTokenBalance(fromDecimals(userLpTokenBalance, 6).toFixed());
+      } else if (!userLpTokenBalance && isLoadBalance) {
+        setLpTokenBalance('0');
+      }
+    };
+    getLpTokenBalance();
+
+    return () => { isLoadBalance = false; };
+  }, [tezos, accountPkh, dex, tokenB]);
+
+  useEffect(() => {
+    const tmp = async () => {
+      if (isTezToTokenDex || !dex) return;
+
+      const token1AddressBytes = taquitoUtils.b58decode('KT18pGT8JoJQGXpQQorF6nAMfCgTCC4fabK3');
+      const token2AddressBytes = taquitoUtils.b58decode('KT1GPuEi25L8n4QfCfgy18BLBXJsTxBeCGaL');
+      const michelData = {
+        prim: 'Pair',
+        args: [
+          {
+            prim: 'Pair',
+            args: [
+              {
+                prim: 'Pair',
+                args: [
+                  {
+                    bytes: token1AddressBytes,
+                  },
+                  {
+                    int: '0',
+                  },
+                ],
+              },
+              {
+                prim: 'Pair',
+                args: [
+                  {
+                    prim: 'Right',
+                    args: [{ prim: 'Unit' }],
+                  },
+                  {
+                    bytes: token2AddressBytes,
+                  },
+                ],
+              }],
+          },
+          {
+            prim: 'Pair',
+            args: [
+              {
+                int: '0',
+              },
+              {
+                prim: 'Right',
+                args: [{ prim: 'Unit' }],
+              },
+            ],
+          }],
+      };
+      const key = Buffer.from(MichelCodec.packData(michelData)).toString('hex');
+
+      const info = await dex.storage.storage.token_to_id.get(key);
+      console.log({ info });
+    };
+    tmp();
+  }, [dex, isTezToTokenDex]);
 
   // useEffect(() => {
   //   let isLoadShares = true;
@@ -224,7 +300,7 @@ const RealForm:React.FC<LiquidityFormProps> = () => {
         }}
         contentClassName={s.content}
       >
-        {/* {tabState.id === 'add' && (
+        {tabState.id === 'add' && (
           <LiquidityFormAdd
             dex={dex}
             tokenA={tokenA}
@@ -246,8 +322,7 @@ const RealForm:React.FC<LiquidityFormProps> = () => {
             tokenBBalance={tokenBBalance}
             lpTokenBalance={lpTokenBalance}
           />
-        )} */}
-        qwe
+        )}
       </Card>
       {/* <LiquidityDetails
         currentTab={tabState.label}
