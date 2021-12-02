726682f3bc7743ea6f36c7b685f179493ccdafab
Dmytro <herkodmytro@gmail.com>
2021-12-02 16:39:26 +0200

    Fixed a Background component

--- src/components/svg/Background.tsx
+++ src/components/svg/Background.tsx
@@ -1,22 +1,16 @@
 import React, { useContext } from 'react';
 
-import { ColorModes as ColorMode, ColorThemeContext } from '@quipuswap/ui-kit';
+import { ColorThemeContext } from '@quipuswap/ui-kit';
 import { isClient } from '@utils/helpers';
-
-function getBackgroundPath(isDesktop: boolean, themeMode: ColorMode = ColorMode.Light) {
-  return `/svg/${isDesktop ? 'Desktop' : 'Mobile'}${themeMode}.svg`;
-}
 
 export const Background: React.FC<IconProps> = ({ className }) => {
   const { colorThemeMode } = useContext(ColorThemeContext);
 
   if (!isClient) return null;
 
-  const isDesktop = window.innerWidth > 1024;
-
   return (
     <img
-      src={getBackgroundPath(isDesktop, colorThemeMode)}
+      src={`/svg/Desktop${colorThemeMode}.svg`}
       alt="background"
       className={className}
     />
