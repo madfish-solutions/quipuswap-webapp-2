6d18bedcbbf20bfb03d4a1e424a7e2e20f691e61
Dmytro <herkodmytro@gmail.com>
2021-12-02 14:45:42 +0200

    Fixed background styles

--- src/components/svg/Background.tsx
+++ src/components/svg/Background.tsx
@@ -4,16 +4,15 @@ import { ColorModes as ColorMode, ColorThemeContext } from '@quipuswap/ui-kit';
 import { isClient } from '@utils/helpers';
 
 function getBackgroundPath(isDesktop: boolean, themeMode: ColorMode = ColorMode.Light) {
-  console.log(`/svg/${isDesktop ? 'Desktop' : 'Mobile'}${themeMode}.svg`);
-
   return `/svg/${isDesktop ? 'Desktop' : 'Mobile'}${themeMode}.svg`;
 }
 
 export const Background: React.FC<IconProps> = ({ className }) => {
   const { colorThemeMode } = useContext(ColorThemeContext);
+
   if (!isClient) return null;
+
   const isDesktop = window.innerWidth > 1024;
-  console.log(colorThemeMode);
 
   return (
     <img
