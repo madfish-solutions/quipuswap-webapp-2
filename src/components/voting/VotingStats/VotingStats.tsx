2716e6407686a95c40327b90f5786230bd360f89
digberi <orkepuma@gmail.com>
2021-12-08 12:36:36 +0200

    * make "claim reward" disabled if reward value is 0

--- src/components/voting/VotingStats/VotingStats.tsx
+++ src/components/voting/VotingStats/VotingStats.tsx
@@ -13,6 +13,7 @@ import cx from 'classnames';
 
 import { useAccountPkh, useTezos } from '@utils/dapp';
 
+import BigNumber from 'bignumber.js';
 import s from './VotingStats.module.sass';
 
 const modeClass = {
@@ -21,11 +22,11 @@ const modeClass = {
 };
 
 type VotingStatsProps = {
-  className?: string
-  pendingReward?: string
-  amounts?: string[],
-  dex?:FoundDex
-  handleSubmit: (params: TransferParams[]) => void
+  className?: string;
+  pendingReward?: string;
+  amounts?: string[];
+  dex?: FoundDex;
+  handleSubmit: (params: TransferParams[]) => void;
 };
 
 export const VotingStats: React.FC<VotingStatsProps> = ({
@@ -40,34 +41,45 @@ export const VotingStats: React.FC<VotingStatsProps> = ({
   const tezos = useTezos();
   const accountPkh = useAccountPkh();
 
-  const content = useMemo(() => [
-    {
-      id: 0,
-      header: 'vote|Your LP',
-      amount: amounts[0] ?? '0',
-      tooltip: 'vote|Total number of LP tokens you own.',
-    },
-    {
-      id: 1,
-      header: 'vote|Your votes',
-      amount: amounts[1] ?? '0',
-      tooltip: 'vote|The amount of votes cast. You have to lock your LP tokens to cast a vote for a baker.',
-    },
-    {
-      id: 2,
-      header: 'vote|Your vetos',
-      amount: amounts[2] ?? '0',
-      tooltip: 'vote|The amount of shares cast to veto a baker. You have to lock your LP tokens to veto a baker.',
-    },
-  ], [amounts]);
+  const BNZero = new BigNumber(0);
+  const isRewardGreaterThenZero = () =>
+    new BigNumber(pendingReward).isGreaterThan(BNZero);
+
+  const content = useMemo(
+    () => [
+      {
+        id: 0,
+        header: 'vote|Your LP',
+        amount: amounts[0] ?? '0',
+        tooltip: 'vote|Total number of LP tokens you own.',
+      },
+      {
+        id: 1,
+        header: 'vote|Your votes',
+        amount: amounts[1] ?? '0',
+        tooltip:
+          'vote|The amount of votes cast. You have to lock your LP tokens to cast a vote for a baker.',
+      },
+      {
+        id: 2,
+        header: 'vote|Your vetos',
+        amount: amounts[2] ?? '0',
+        tooltip:
+          'vote|The amount of shares cast to veto a baker. You have to lock your LP tokens to veto a baker.',
+      },
+    ],
+    [amounts]
+  );
 
   return (
-    <Card className={className} contentClassName={cx(s.content, modeClass[colorThemeMode])}>
+    <Card
+      className={className}
+      contentClassName={cx(s.content, modeClass[colorThemeMode])}
+    >
       <div className={s.reward}>
         <div className={s.rewardContent}>
           <span className={s.rewardHeader}>
-            {t('vote|Your Pending Rewards')}
-            :
+            {t('vote|Your Pending Rewards')}:
           </span>
           <span className={s.rewardAmount}>
             {pendingReward}
@@ -76,9 +88,7 @@ export const VotingStats: React.FC<VotingStatsProps> = ({
         </div>
         <VotingReward />
       </div>
-      {content.map(({
-        id, header, amount, tooltip,
-      }) => (
+      {content.map(({ id, header, amount, tooltip }) => (
         <div key={id} className={s.item}>
           <span className={s.header}>
             {t(header)}
@@ -89,7 +99,7 @@ export const VotingStats: React.FC<VotingStatsProps> = ({
         </div>
       ))}
       <Button
-        disabled={!tezos || !accountPkh || !dex}
+        disabled={!tezos || !accountPkh || !dex || !isRewardGreaterThenZero()}
         onClick={() => {
           const asyncFunc = async () => {
             if (!tezos || !dex || !accountPkh) return;
