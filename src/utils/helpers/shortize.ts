c161c27f22135450fbdb9cf53ec3d3dee079455b
Artem Koziar <koziar.artem@gmail.com>
2021-12-09 12:19:59 +0200

    [voting] Fix getWhitelistedBakerName

--- src/utils/helpers/shortize.ts
+++ src/utils/helpers/shortize.ts
@@ -1,1 +1,1 @@
-export const shortize = (str: string, length?: number) => `${str.slice(0, length ?? 4)}...${str.slice(-4)}`;
+export const shortize = (str: string, length: number = 4) => `${str.slice(0, length)}...${str.slice(-length)}`;
