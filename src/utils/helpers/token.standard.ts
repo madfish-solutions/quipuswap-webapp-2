b5e90d063bb3a40209d1c33e52457869f3616cb8
Artem Koziar <koziar.artem@gmail.com>
2022-01-10 15:21:50 +0200

    Fix getStandardValue

--- src/utils/helpers/token.standard.ts
+++ src/utils/helpers/token.standard.ts
@@ -3,9 +3,9 @@ import { Standard } from '@graphql';
 export const getStandardValue = (standard: Standard) => {
   switch (standard) {
     case Standard.Fa2:
-      return 'fa2';
+      return 'FA2';
     case Standard.Fa12:
-      return 'fa1.2';
+      return 'FA12';
     default:
       return undefined;
   }
