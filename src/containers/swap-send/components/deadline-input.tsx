939ea9b0dca270f78c3d6e360211e0969206a6d9
Dmytro <herkodmytro@gmail.com>
2022-01-11 17:20:16 +0200

    Fixed bugs with input token's decimals and balance loading

--- src/containers/swap-send/components/deadline-input.tsx
+++ src/containers/swap-send/components/deadline-input.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'next-i18next';
 
 import { DEFAULT_DEADLINE_MINS } from '@app.config';
 import s from '@styles/CommonContainer.module.sass';
+import { Nullable } from '@utils/types';
 
 interface DeadlineInputProps {
   error?: string;
@@ -16,7 +17,7 @@ interface DeadlineInputProps {
 export const DeadlineInput: FC<DeadlineInputProps> = ({ error, onChange, value }) => {
   const { t } = useTranslation(['common']);
 
-  const handleChange = (newValue?: string) =>
+  const handleChange = (newValue: Nullable<string>) =>
     onChange(newValue ? new BigNumber(newValue) : new BigNumber(DEFAULT_DEADLINE_MINS));
 
   return (
