40608dccc105f98549c0ee195a44d751068f1b18
Artem Koziar <koziar.artem@gmail.com>
2022-01-12 12:36:13 +0200

    fix DeadlineInput onChange

--- src/containers/swap-send/components/deadline-input.tsx
+++ src/containers/swap-send/components/deadline-input.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'next-i18next';
 
 import { DEFAULT_DEADLINE_MINS } from '@app.config';
 import s from '@styles/CommonContainer.module.sass';
+import { Nullable } from '@utils/types';
 
 interface DeadlineInputProps {
   error?: string;
@@ -16,8 +17,8 @@ interface DeadlineInputProps {
 export const DeadlineInput: FC<DeadlineInputProps> = ({ error, onChange, value }) => {
   const { t } = useTranslation(['common']);
 
-  const handleChange = (newValue?: string) =>
-    onChange(newValue ? new BigNumber(newValue) : new BigNumber(DEFAULT_DEADLINE_MINS));
+  const handleChange = (newValue: Nullable<string>) =>
+    onChange(new BigNumber(newValue ? newValue : DEFAULT_DEADLINE_MINS));
 
   return (
     <>
