3d24c1b0f73f579b5791e64cc8fd6e89899680f3
Artem Koziar <koziar.artem@gmail.com>
2022-01-18 14:59:27 +0200

    empty commit

--- src/utils/helpers/type-checks.ts
+++ src/utils/helpers/type-checks.ts
@@ -4,4 +4,7 @@ export const isNull = <T>(value: Nullable<T>): value is null => value === null;
 
 export const isUndefined = <T>(value: Undefined<T>): value is undefined => value === undefined;
 
+/*
+  Check to empty value
+ */
 export const isExist = <T>(value: Undefined<Nullable<T>>): value is T => !isNull(value) && !isUndefined(value);
