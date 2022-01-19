7d48d35291dfa11a6f2dbedd0362fedddde39fa3
Inokentii Mazhara <keshan3262@gmail.com>
2022-01-19 18:27:09 +0200

    Implement wrapping very long routes

--- src/utils/helpers/arrays.ts
+++ src/utils/helpers/arrays.ts
@@ -16,3 +16,5 @@ export const getUniqArray = <T>(list: T[], getKey: (el: T) => string): T[] => {
 export const isFoundIndex = (index: number) => index !== -1;
 
 export const isEmptyArray = (array: unknown[]) => array.length === 0;
+
+export const isLastElementIndex = (index: number, array: unknown[]) => index === array.length - 1;
