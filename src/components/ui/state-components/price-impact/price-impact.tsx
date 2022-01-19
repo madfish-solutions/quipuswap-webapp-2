9cc0bec04c85c7913b841db9159cd760fd3734a9
Inokentii Mazhara <keshan3262@gmail.com>
2022-01-19 17:55:16 +0200

    Fix NaN in 'Price impact' field

--- src/components/ui/state-components/price-impact/price-impact.tsx
+++ src/components/ui/state-components/price-impact/price-impact.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 
 import BigNumber from 'bignumber.js';
 
-import { FormatNumber } from '@utils/formatNumber';
 import { Nullable } from '@utils/types';
 
 import { StateCurrencyAmount } from '../state-currency-amount';
@@ -12,9 +11,5 @@ interface StatePriceImpactProps {
 }
 
 export const StatePriceImpact: FC<StatePriceImpactProps> = ({ priceImpact }) => {
-  const wrapPriceImpact = new BigNumber(priceImpact ?? NaN);
-
-  const amount = wrapPriceImpact.lt(0.01) ? '<0.01' : FormatNumber(priceImpact ?? 0, { decimals: 2 });
-
-  return <StateCurrencyAmount isLoading={wrapPriceImpact.isNaN()} amount={amount} currency="%" />;
+  return <StateCurrencyAmount isLoading={priceImpact === null} amount={priceImpact} currency="%" />;
 };
