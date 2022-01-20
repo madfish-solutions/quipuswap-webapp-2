8f20e056b4319a01e1a7f4e4159e5a5bce73da16
Artem Koziar <koziar.artem@gmail.com>
2022-01-20 15:41:07 +0200

    Add translation to AlarmMessage

--- src/components/common/testnet-alert/testnet-alert.tsx
+++ src/components/common/testnet-alert/testnet-alert.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { useTranslation } from 'next-i18next';
+
 import { AlarmMessage } from '@components/common/alarm-message';
 import { useNetwork } from '@utils/dapp';
 import { QSNetworkType } from '@utils/types';
@@ -7,12 +9,13 @@ import { QSNetworkType } from '@utils/types';
 import s from './testnet-alert.module.sass';
 
 export const TestnetAlert: FC = () => {
+  const { t } = useTranslation(['common']);
   const network = useNetwork();
   const isTestNet = network && network.type === QSNetworkType.TEST;
 
   return isTestNet ? (
     <div className={s.testNet}>
-      <AlarmMessage message="You are on Test net now!" className={s.AlarmMessage} />
+      <AlarmMessage message={t('common|You are on Test net now!')} className={s.AlarmMessage} />
     </div>
   ) : null;
 };
