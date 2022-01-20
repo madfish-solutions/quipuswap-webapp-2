34203664015ca1a9becb687210cc9cf975b62748
Inokentii Mazhara <keshan3262@gmail.com>
2022-01-20 18:10:46 +0200

    'Liquidity' tab: Implement setting default tokens on network change

--- src/containers/liquidity/liquidity-cards/use-liquidity-form.service.ts
+++ src/containers/liquidity/liquidity-cards/use-liquidity-form.service.ts
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import BigNumber from 'bignumber.js';
 import { useRouter } from 'next/router';
 
-import { DEFAULT_DEADLINE_MINS } from '@app.config';
-import { useTokens } from '@utils/dapp';
+import { DEFAULT_DEADLINE_MINS, networksDefaultTokens, TEZOS_TOKEN } from '@app.config';
+import { useNetwork, useTokens } from '@utils/dapp';
 import { Nullable, WhitelistedToken, WhitelistedTokenPair } from '@utils/types';
 
 import { useDexContract } from '../hooks';
@@ -14,6 +14,8 @@ import { getTabById, LiquidityTabs } from './liquidity-tabs';
 export const useLiquidityFormService = () => {
   const router = useRouter();
   const { data: tokens, loading } = useTokens();
+  const { id: networkId } = useNetwork();
+  const prevNetworkIdRef = useRef(networkId);
 
   const { tabId } = parseUrl(router.asPath);
 
@@ -44,10 +46,13 @@ export const useLiquidityFormService = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading]);
 
-  const changeRoute = async (tabId: LiquidityTabs, _tokenA: WhitelistedToken, _tokenB: WhitelistedToken) => {
-    const liqUrl = getLiquidityUrl(tabId || tab.id, _tokenA, _tokenB);
-    await router.replace(liqUrl, undefined, { shallow: true });
-  };
+  const changeRoute = useCallback(
+    async (tabId: LiquidityTabs, _tokenA: WhitelistedToken, _tokenB: WhitelistedToken) => {
+      const liqUrl = getLiquidityUrl(tabId || tab.id, _tokenA, _tokenB);
+      await router.replace(liqUrl, undefined, { shallow: true });
+    },
+    [router, tab.id]
+  );
 
   const handleChangeTab = (tabId: LiquidityTabs) => {
     const newTab = getTabById(tabId);
@@ -76,12 +81,22 @@ export const useLiquidityFormService = () => {
     }
   };
 
-  const handleChangeTokensPair = ({ token1, token2 }: WhitelistedTokenPair) => {
-    setTokenA(token1);
-    setTokenB(token2);
-    clearDex();
-    void changeRoute(tab.id, token1, token2);
-  };
+  const handleChangeTokensPair = useCallback(
+    ({ token1, token2 }: WhitelistedTokenPair) => {
+      setTokenA(token1);
+      setTokenB(token2);
+      clearDex();
+      void changeRoute(tab.id, token1, token2);
+    },
+    [changeRoute, clearDex, tab.id]
+  );
+
+  useEffect(() => {
+    if (prevNetworkIdRef.current !== networkId) {
+      handleChangeTokensPair({ token1: TEZOS_TOKEN, token2: networksDefaultTokens[networkId] });
+    }
+    prevNetworkIdRef.current = networkId;
+  }, [networkId, handleChangeTokensPair]);
 
   return {
     tab,
