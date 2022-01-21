685d189bfea13de344431c9687d6d57b854f07ae
Digberi <orkepuma@gmail.com>
2022-01-21 21:50:14 +0200

    Revert "install temple"
    This reverts commit 128abd3b2a40450cc701ab52f0ec0e4139cabc93.

--- src/components/modals/WalletModal/content.ts
+++ src/components/modals/WalletModal/content.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Beacon as BeaconIcon, Temple as TempleIcon } from '@quipuswap/ui-kit';
+import { Beacon, Temple } from '@quipuswap/ui-kit';
 
 import { WalletType } from '@utils/types';
 
@@ -10,14 +10,15 @@ interface WalletProps {
   label: string;
 }
 
-export const Temple: WalletProps = {
-  id: WalletType.TEMPLE,
-  Icon: TempleIcon,
-  label: 'Temple Wallet'
-};
-
-export const Beacon: WalletProps = {
-  id: WalletType.BEACON,
-  Icon: BeaconIcon,
-  label: 'Beacon'
-};
+export const Wallets: WalletProps[] = [
+  {
+    id: WalletType.TEMPLE,
+    Icon: Temple,
+    label: 'Temple Wallet'
+  },
+  {
+    id: WalletType.BEACON,
+    Icon: Beacon,
+    label: 'Beacon'
+  }
+];
