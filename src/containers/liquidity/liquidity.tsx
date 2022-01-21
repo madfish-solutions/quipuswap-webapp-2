d9500fa15a914fdc82996a3f879e59f78f5b8060
Dmytro <herkodmytro@gmail.com>
2022-01-21 16:39:37 +0200

    Added DeadlineAndAsippage context

--- src/containers/liquidity/liquidity.tsx
+++ src/containers/liquidity/liquidity.tsx
@@ -3,13 +3,16 @@ import React, { FC } from 'react';
 import { StickyBlock } from '@quipuswap/ui-kit';
 
 import { LiquidityCards } from '@containers/liquidity/liquidity-cards';
+import { DeadlineAndSlippageProvider } from '@utils/dapp/slippage-deadline';
 
 interface LiquidityProps {
   className?: string;
 }
 
 export const Liquidity: FC<LiquidityProps> = ({ className }) => (
-  <StickyBlock className={className}>
-    <LiquidityCards />
-  </StickyBlock>
+  <DeadlineAndSlippageProvider>
+    <StickyBlock className={className}>
+      <LiquidityCards />
+    </StickyBlock>
+  </DeadlineAndSlippageProvider>
 );
