c9a9e497f5904d185fddeb7cc2e67ee69d1ec484
Dmytro <herkodmytro@gmail.com>
2022-01-24 13:46:09 +0200

    Added page title component

--- src/containers/liquidity/liquidity.tsx
+++ src/containers/liquidity/liquidity.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from 'react';
 
 import { StickyBlock } from '@quipuswap/ui-kit';
+import { useTranslation } from 'next-i18next';
 
-import { LiquidityCards } from '@containers/liquidity/liquidity-cards';
+import { PageTitle } from '@components/common/page-title';
+
+import { LiquidityCards } from './liquidity-cards';
 
 interface LiquidityProps {
   className?: string;
 }
 
-export const Liquidity: FC<LiquidityProps> = ({ className }) => (
-  <StickyBlock className={className}>
-    <LiquidityCards />
-  </StickyBlock>
-);
+export const Liquidity: FC<LiquidityProps> = ({ className }) => {
+  const { t } = useTranslation(['common']);
+
+  return (
+    <>
+      <PageTitle>{t('common|Liquidity')}</PageTitle>
+      <StickyBlock className={className}>
+        <LiquidityCards />
+      </StickyBlock>
+    </>
+  );
+};
