8b2bcbc18134aee7c2ff2378c755a974a9924e1c
Digberi <orkepuma@gmail.com>
2022-01-24 11:50:18 +0200

    fix layout in different browsers

--- src/components/common/Header/Sidebar/sidebar.helpels.ts
+++ src/components/common/Header/Sidebar/sidebar.helpels.ts
@@ -5,8 +5,8 @@ const PX = 'px';
 const NORMAL_WIDTH = 163;
 const WIDTH_WHEN_SIDEBAR_SCROLLABLE = 155;
 
-const handleNetworkSelectorDecrease = (networkSelector: HTMLDivElement) => {
-  networkSelector.style.setProperty(WIDTH, `${WIDTH_WHEN_SIDEBAR_SCROLLABLE}${PX}`);
+const handleNetworkSelectorDecrease = (networkSelector: HTMLDivElement, width: number) => {
+  networkSelector.style.setProperty(WIDTH, `${NORMAL_WIDTH - width}${PX}`);
 };
 
 const handleNetworkSelectorIncrease = (networkSelector: HTMLDivElement) => {
@@ -14,13 +14,15 @@ const handleNetworkSelectorIncrease = (networkSelector: HTMLDivElement) => {
 };
 
 const isElementScrolable = (element: HTMLElement) => element.scrollHeight > element.clientHeight;
+const changingWidth = (element: HTMLElement) => element.offsetWidth - element.clientWidth
 
 export const fixNetworkSelector = (sidebar: HTMLDivElement, networkSelector: HTMLDivElement) => {
   if (isElementScrolable(sidebar)) {
-    handleNetworkSelectorDecrease(networkSelector);
+    handleNetworkSelectorDecrease(networkSelector, changingWidth(sidebar));
   } else {
     handleNetworkSelectorIncrease(networkSelector);
   }
 };
+
 export const getSecondElement = (selector: string): HTMLDivElement =>
   document.querySelectorAll<HTMLDivElement>(selector)[INDEX_OF_SIDEBAR_NETWORK_SELECTOR_VALUE_CONTAINER];
