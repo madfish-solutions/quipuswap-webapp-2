6989a770d65955d1b1d3a0f23e81f8eb13a7042a
Dmytro <herkodmytro@gmail.com>
2022-01-25 16:56:52 +0200

    Fixed minimum dealine input validations

--- src/containers/liquidity/liquidity-cards/validators/validate-deadline.ts
+++ src/containers/liquidity/liquidity-cards/validators/validate-deadline.ts
@@ -1,10 +1,11 @@
 import BigNumber from 'bignumber.js';
 import { i18n } from 'next-i18next';
 
-const MAX_TRANSACTION_DURATION_MINUTES = 43200;
+const MIN_DEADLINE_MINUTES = 1;
+const MAX_DEADLINE_MINUTES = 43200;
 
 export const validateDeadline = (deadline: BigNumber) => {
-  if (deadline.gt(MAX_TRANSACTION_DURATION_MINUTES)) {
+  if (deadline.lt(MIN_DEADLINE_MINUTES) || deadline.gt(MAX_DEADLINE_MINUTES)) {
     return i18n?.t('common|deadlineOutOfRangeError') || 'Deadline has to be between 1m and 30 days (43200m)';
   }
 
