dd8dd815b949e24e38c9185f09326f5b7cd07ea7
Digberi <orkepuma@gmail.com>
2022-01-26 20:15:36 +0200

    creat loading state

--- src/components/common/TokensLogos/TokensLogos.tsx
+++ src/components/common/TokensLogos/TokensLogos.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import cx from 'classnames';
 
 import { TokenLogo } from '@components/common/TokenLogo';
-import { isNull } from '@utils/helpers';
+import { isExist, isNull } from '@utils/helpers';
 import { Nullable } from '@utils/types';
 
 import { FallbackLogo } from '../../svg/FallbackLogo';
@@ -16,6 +16,7 @@ interface TokensLogosPropsAbstraction {
   secondTokenSymbol?: Nullable<string>;
   className?: string;
   imageClassName?: string;
+  loading?: boolean;
 }
 
 interface FixedTokensLogosProps extends TokensLogosPropsAbstraction {
@@ -30,30 +31,32 @@ interface FillTokensLogosProps extends TokensLogosPropsAbstraction {
 export type TokensLogosProps = FixedTokensLogosProps | FillTokensLogosProps;
 
 export const TokensLogos: React.FC<TokensLogosProps> = props => {
-  const { firstTokenIcon, firstTokenSymbol, secondTokenIcon, secondTokenSymbol, className, imageClassName } = props;
+  const { firstTokenIcon, firstTokenSymbol, secondTokenIcon, secondTokenSymbol, className, imageClassName, loading } = props;
 
   const compoundClassName = cx(s.root, { [s.pairs]: secondTokenIcon }, className);
 
   const layoutBasedProps =
     props.layout === 'fill' ? { layout: 'fill' as const } : { layout: 'fixed' as const, size: props.width };
 
+  const wrapLoading = loading === true
+
   return (
     <div className={compoundClassName}>
       <div className={cx(s.imageWrapper, imageClassName)}>
-        {firstTokenIcon ? (
+        {isExist(firstTokenIcon) && !wrapLoading ? (
           <TokenLogo src={firstTokenIcon} tokenSymbol={firstTokenSymbol} {...layoutBasedProps} />
         ) : (
           <FallbackLogo className={cx(s.image)} />
         )}
       </div>
 
-      {secondTokenIcon && (
+      {isExist(secondTokenIcon) && !wrapLoading && (
         <div className={cx(s.imageWrapper, s.secondImage, imageClassName)}>
           <TokenLogo src={secondTokenIcon} tokenSymbol={secondTokenSymbol} {...layoutBasedProps} />
         </div>
       )}
 
-      {isNull(secondTokenIcon) && (
+      {isNull(secondTokenIcon) ||  (secondTokenIcon && wrapLoading) && (
         <div className={cx(s.secondImage, imageClassName)}>
           <FallbackLogo className={cx(s.image)} />
         </div>
