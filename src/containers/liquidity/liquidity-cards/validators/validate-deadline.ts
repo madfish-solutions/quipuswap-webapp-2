6a4e31b33c73984f230101e78d92866472a60725
Dmytro <herkodmytro@gmail.com>
2022-01-26 11:49:46 +0200

    Used variables from app.config

--- src/containers/liquidity/liquidity-cards/validators/validate-deadline.ts
+++ src/containers/liquidity/liquidity-cards/validators/validate-deadline.ts
@@ -1,11 +1,10 @@
 import BigNumber from 'bignumber.js';
 import { i18n } from 'next-i18next';
 
-const MIN_DEADLINE_MINUTES = 1;
-const MAX_DEADLINE_MINUTES = 43200;
+import { MIN_DEADLINE_MINS, MAX_DEADLINE_MINS } from '@app.config';
 
 export const validateDeadline = (deadline: BigNumber) => {
-  if (deadline.lt(MIN_DEADLINE_MINUTES) || deadline.gt(MAX_DEADLINE_MINUTES)) {
+  if (deadline.lt(MIN_DEADLINE_MINS) || deadline.gt(MAX_DEADLINE_MINS)) {
     return i18n?.t('common|deadlineOutOfRangeError') || 'Deadline has to be between 1m and 30 days (43200m)';
   }
 
