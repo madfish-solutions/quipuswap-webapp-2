3b6e2c2b37bd825573056b969d9a9a92e34b2137
Artem Koziar <koziar.artem@gmail.com>
2022-01-28 16:06:26 +0200

    Fix getTokensLabel

--- src/utils/helpers/get-tokens-label.ts
+++ src/utils/helpers/get-tokens-label.ts
@@ -1,9 +1,6 @@
-import { getWhitelistedTokenSymbol } from '@utils/helpers/index';
+import { getTokensPairName } from '@utils/helpers/index';
 import { Optional, WhitelistedToken } from '@utils/types';
 
 export const getTokensLabel = (inputToken: Optional<WhitelistedToken>, outputToken: Optional<WhitelistedToken>) => {
-  const inputTokenLabel = inputToken ? getWhitelistedTokenSymbol(inputToken) : '';
-  const outputTokenLabel = outputToken ? getWhitelistedTokenSymbol(outputToken) : '';
-
-  return inputTokenLabel && outputTokenLabel ? `${inputTokenLabel}-${outputTokenLabel}` : '';
+  return inputToken && outputToken ? getTokensPairName(inputToken, outputToken) : '';
 };
