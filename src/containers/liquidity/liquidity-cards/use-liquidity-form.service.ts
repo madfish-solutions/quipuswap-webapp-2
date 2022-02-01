d5eff5bb2cd65dfa95f917caaa188c5418980206
Inokentii Mazhara <keshan3262@gmail.com>
2022-02-01 16:20:22 +0200

    Some refactoring

--- src/containers/liquidity/liquidity-cards/use-liquidity-form.service.ts
+++ src/containers/liquidity/liquidity-cards/use-liquidity-form.service.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { Dispatch, MutableRefObject, SetStateAction, useEffect, useRef, useState } from 'react';
 
 import { useRouter } from 'next/router';
 
@@ -8,6 +8,22 @@ import { Nullable, WhitelistedToken, WhitelistedTokenPair } from '@utils/types';
 import { useDexContract } from '../hooks';
 import { findToken, getLiquidityUrl, parseUrl } from './helpers';
 import { getTabById, LiquidityTabs } from './liquidity-tabs';
+
+const handleSearchPromise = async (
+  searchPromise: Promise<WhitelistedToken | null>,
+  setToken: Dispatch<SetStateAction<Nullable<WhitelistedToken>>>,
+  tokenDirtyRef: MutableRefObject<boolean>
+) => {
+  try {
+    const token = await searchPromise;
+    if (token && !tokenDirtyRef.current) {
+      setToken(token);
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('Error while getting token metadata', e);
+  }
+};
 
 export const useLiquidityFormService = ({
   onTokensChange
@@ -45,32 +61,18 @@ export const useLiquidityFormService = ({
     if (validTokenA) {
       setTokenA(validTokenA);
     } else {
-      searchCustomTokens(contractTokenA, Number(idTokenA), true)
-        .then(result => {
-          if (result === null && !tokenADirtyRef.current) {
-            throw new Error();
-          }
-          setTokenA(result);
-        })
-        .catch(() => setTokenA(null));
+      handleSearchPromise(searchCustomTokens(contractTokenA, Number(idTokenA), true), setTokenA, tokenADirtyRef);
     }
 
     const validTokenB = findToken(contractTokenB, idTokenB, tokens);
     if (validTokenB) {
       setTokenB(validTokenB);
     } else {
-      searchCustomTokens(contractTokenB, Number(idTokenB), true)
-        .then(result => {
-          if (result === null && !tokenBDirtyRef.current) {
-            throw new Error();
-          }
-          setTokenB(result);
-        })
-        .catch(() => setTokenB(null));
+      handleSearchPromise(searchCustomTokens(contractTokenB, Number(idTokenB), true), setTokenB, tokenBDirtyRef);
     }
     handleUpdateTitle(validTokenA, validTokenB);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading, url, tokens]);
+  }, [loading, url, tokens, searchCustomTokens]);
 
   const changeRoute = async (tabId: LiquidityTabs, _tokenA: WhitelistedToken, _tokenB: WhitelistedToken) => {
     const liqUrl = getLiquidityUrl(tabId || tab.id, _tokenA, _tokenB);
