36f95a652967a0fbc48d730949f242cd14fa1ded
Artem Koziar <koziar.artem@gmail.com>
2022-02-04 11:52:51 +0200

    Add new news images

--- src/components/home/News/content.tsx
+++ src/components/home/News/content.tsx
@@ -17,23 +17,27 @@ export const isNewsWithLink = (news: News): news is NewsWithLink => 'url' in new
 export const NewsData: News[] = [
   {
     id: 0,
-    img: '/images/news1.png'
+    img: '/images/major-update.png'
   },
   {
     id: 1,
-    img: '/images/news2.png',
+    img: '/images/open-source-dex.png'
+  },
+  {
+    id: 2,
+    img: '/images/quipu-airdrop.png',
     url: 'https://story.madfish.solutions/quipuswap-governance-token-airdrop-announcement/',
     external: true
   },
   {
-    id: 2,
-    img: '/images/news3.png',
+    id: 3,
+    img: '/images/tokenomics-guide.png',
     url: 'https://story.madfish.solutions/quipuswap-tokenomics-guide-quipu-learn-everything-about-our-governance-token/',
     external: true
   },
   {
-    id: 3,
-    img: '/images/news4.png',
+    id: 4,
+    img: '/images/security-audit.png',
     url: 'https://story.madfish.solutions/least-authority-has-successfully-conducted-the-quipuswap-security-audit/',
     external: true
   }
