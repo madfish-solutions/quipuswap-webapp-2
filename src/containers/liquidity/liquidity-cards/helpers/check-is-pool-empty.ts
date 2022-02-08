5e25d89882bc58307f27dc6fd54ae8deb3a8a02a
Artem Koziar <koziar.artem@gmail.com>
2022-02-08 10:49:46 +0200

    Fix pairInfo

--- src/containers/liquidity/liquidity-cards/helpers/check-is-pool-empty.ts
+++ src/containers/liquidity/liquidity-cards/helpers/check-is-pool-empty.ts
@@ -1,12 +1,11 @@
 import { EMPTY_POOL_AMOUNT } from '@app.config';
-import { isNull } from '@utils/helpers';
 import { Optional } from '@utils/types';
 
 import { PairInfo } from '../add-liquidity-form';
 
 export const checkIsPoolEmpty = (pairInfo: Optional<PairInfo>) =>
   Boolean(
-    isNull(pairInfo) ||
+    !pairInfo ||
       pairInfo?.tokenAPool.eq(EMPTY_POOL_AMOUNT) ||
       pairInfo?.tokenBPool.eq(EMPTY_POOL_AMOUNT) ||
       pairInfo?.totalSupply.eq(EMPTY_POOL_AMOUNT)
