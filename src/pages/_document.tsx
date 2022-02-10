458d5e8c8c7ded2c488f701448be988f4aa86c13
Dmytro <herkodmytro@gmail.com>
2022-02-10 15:19:02 +0200

    Created custom _document page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body className="dark">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+// eslint-disable-next-line import/no-default-export
+export default MyDocument;
