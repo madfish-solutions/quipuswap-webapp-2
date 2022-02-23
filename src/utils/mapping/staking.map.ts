fea93abd4882dd9d12e8ff207ed7ff0e441d0bd2
Digberi <orkepuma@gmail.com>
2022-02-23 13:40:53 +0200

    add balance to staking list

--- src/utils/mapping/staking.map.ts
+++ src/utils/mapping/staking.map.ts
@@ -1,6 +1,14 @@
 import BigNumber from 'bignumber.js';
 
-import { RawStakingItem, RawStakeStats, StakingItem, StakeStats } from '@interfaces/staking.interfaces';
+import {
+  RawStakingItem,
+  RawStakeStats,
+  StakingItem,
+  StakeStats,
+  NoUserStakingItem,
+  UserStakingItem
+} from '@interfaces/staking.interfaces';
+import { isUndefined } from '@utils/helpers';
 import { Token } from '@utils/types';
 
 const mapToken = (raw: Token): Token => ({
@@ -8,21 +16,37 @@ const mapToken = (raw: Token): Token => ({
   fa2TokenId: raw.fa2TokenId === undefined ? undefined : Number(raw.fa2TokenId)
 });
 
-export const mapStakeItem = (raw: RawStakingItem): StakingItem => ({
-  ...raw,
-  id: new BigNumber(raw.id),
-  tokenA: mapToken(raw.tokenA),
-  tokenB: raw.tokenB ? mapToken(raw.tokenB) : undefined,
-  rewardToken: mapToken(raw.rewardToken),
-  tvl: new BigNumber(raw.tvl),
-  apr: raw.apr ? new BigNumber(raw.apr) : null,
-  apy: raw.apy ? new BigNumber(raw.apy) : null,
-  depositExchangeRate: new BigNumber(raw.depositExchangeRate),
-  earnExchangeRate: new BigNumber(raw.earnExchangeRate),
-  myBalance: null,
-  depositBalance: null,
-  earnBalance: null
-});
+export const mapStakeItem = (raw: RawStakingItem): StakingItem => {
+  let balances: NoUserStakingItem | UserStakingItem;
+
+  if (!isUndefined(raw.myBalance) && !isUndefined(raw.depositBalance) && !isUndefined(raw.earnBalance)) {
+    balances = {
+      myBalance: new BigNumber(raw.myBalance),
+      depositBalance: new BigNumber(raw.depositBalance),
+      earnBalance: new BigNumber(raw.earnBalance)
+    } as UserStakingItem;
+  } else {
+    balances = {
+      myBalance: null,
+      depositBalance: null,
+      earnBalance: null
+    } as NoUserStakingItem;
+  }
+
+  return {
+    ...raw,
+    ...balances,
+    id: new BigNumber(raw.id),
+    tokenA: mapToken(raw.tokenA),
+    tokenB: raw.tokenB ? mapToken(raw.tokenB) : undefined,
+    rewardToken: mapToken(raw.rewardToken),
+    tvl: new BigNumber(raw.tvl),
+    apr: raw.apr ? new BigNumber(raw.apr) : null,
+    apy: raw.apy ? new BigNumber(raw.apy) : null,
+    depositExchangeRate: new BigNumber(raw.depositExchangeRate),
+    earnExchangeRate: new BigNumber(raw.earnExchangeRate)
+  };
+};
 
 export const mapStakesItems = (rawList: RawStakingItem[]): StakingItem[] => rawList.map(mapStakeItem);
 
