42aff16c1f3614374f812283b091eaa7e802ddc8
Inokentii Mazhara <keshan3262@gmail.com>
2022-03-02 11:46:18 +0200

    Change URL for 'Learn more' link

--- src/components/modals/donation-modal/donation-modal.tsx
+++ src/components/modals/donation-modal/donation-modal.tsx
@@ -61,7 +61,7 @@ export const DonationModal: FC = () => {
         <img src="ukraine_outline.png" alt="Ukraine" className={s.image} />
         <div className={s.title}>Stand with Ukraine</div>
         <div className={s.description}>Donate TEZ - support Ukrainians in their fight with Putin occupants</div>
-        <Button external href="#" theme="underlined" className={s.learnMore}>
+        <Button external href="https://donate.tezos.org.ua/" theme="underlined" className={s.learnMore}>
           {t('common|Learn more')}
         </Button>
         <TokenSelect
