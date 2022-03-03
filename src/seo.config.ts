12813f836c8a2e7bd51469809a2523fa5677e22e
Inokentii Mazhara <keshan3262@gmail.com>
2022-03-03 12:00:39 +0200

    Change image for website metadata

--- src/seo.config.ts
+++ src/seo.config.ts
@@ -1,5 +1,5 @@
 export const SITE_NAME = 'QuipuSwap';
-const SITE_IMAGE = 'quipuswap.png';
+const SITE_IMAGE = 'quipuswap_support_ukraine.png';
 export const SITE_TITLE = `${SITE_NAME} - Tezos DEX with on-chain governance for baking rewards`;
 export const SITE_DESCRIPTION =
   'Decentralized exchange for the Tezos based tokens. Create liquidity pairs, swap tokens, participate in farming activities. The DEX is open source and audited.';
