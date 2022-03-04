c8f56860dbd659c63c22f9aa69a1e8d29b0aeea2
Inokentii Mazhara <keshan3262@gmail.com>
2022-03-04 13:29:28 +0200

    Add tooltips for stake details

--- src/containers/staking/item/components/staking-details/staking-details.tsx
+++ src/containers/staking/item/components/staking-details/staking-details.tsx
@@ -28,7 +28,7 @@ interface Props {
 }
 
 export const StakingDetails: FC<Props> = ({ item, isError }) => {
-  const { t } = useTranslation(['common', 'vote']);
+  const { t } = useTranslation(['common', 'vote', 'stake']);
   const { data: bakers } = useBakers();
 
   const CardCellClassName = cx(s.cellCenter, s.cell, styles.vertical);
@@ -49,7 +49,11 @@ export const StakingDetails: FC<Props> = ({ item, isError }) => {
       }}
       contentClassName={s.content}
     >
-      <DetailsCardCell cellName={t('stake|Value Locked')} className={CardCellClassName} tooltipContent={null}>
+      <DetailsCardCell
+        cellName={t('stake|Value Locked')}
+        className={CardCellClassName}
+        tooltipContent={t('stake|valueLockedTooltip')}
+      >
         <StateCurrencyAmount
           dollarEquivalent={tvlDollarEquivalent}
           currency={tokenBSymbol}
@@ -58,7 +62,11 @@ export const StakingDetails: FC<Props> = ({ item, isError }) => {
         />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|Daily Distribution')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|Daily Distribution')}
+        tooltipContent={t('stake|dailyDistributionTooltip')}
+        className={CardCellClassName}
+      >
         <StateCurrencyAmount
           dollarEquivalent={distributionDollarEquivalent}
           currency={rewardTokenSymbol}
@@ -67,35 +75,63 @@ export const StakingDetails: FC<Props> = ({ item, isError }) => {
         />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|APR')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell cellName={t('stake|APR')} tooltipContent={t('stake|aprTooltip')} className={CardCellClassName}>
         <StatePercentage value={item?.apr?.toFixed() ?? null} isLoading={isLoading} />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|dailyApr')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|dailyApr')}
+        tooltipContent={t('stake|dailyAprTooltip')}
+        className={CardCellClassName}
+      >
         <StatePercentage isLoading={!isError && !item} value={item?.apr?.dividedBy(365).toFixed() ?? null} />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|Current Delegate')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|Current Delegate')}
+        tooltipContent={t('stake|currentDelegateTooltip')}
+        className={CardCellClassName}
+      >
         <CandidateButton candidate={bakers[0]} />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|Next Delegate')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|Next Delegate')}
+        tooltipContent={t('stake|nextDelegateTooltip')}
+        className={CardCellClassName}
+      >
         <CandidateButton candidate={bakers[1]} />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|stakingEndsIn')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|stakingEndsIn')}
+        tooltipContent={t('stake|stakingEndsInTooltip')}
+        className={CardCellClassName}
+      >
         <Countdown endTimestamp={endTimestamp} />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|Lock Period')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|Lock Period')}
+        tooltipContent={t('stake|lockPeriodTooltip')}
+        className={CardCellClassName}
+      >
         <TimespanView value={86400 * MS_IN_SECOND} />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|Withdrawal Fee')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|Withdrawal Fee')}
+        tooltipContent={t('stake|withdrawalFeeTooltip')}
+        className={CardCellClassName}
+      >
         <StatePercentage isLoading={false} value="2" />
       </DetailsCardCell>
 
-      <DetailsCardCell cellName={t('stake|Interface Fee')} tooltipContent={null} className={CardCellClassName}>
+      <DetailsCardCell
+        cellName={t('stake|Interface Fee')}
+        tooltipContent={t('stake|interfaceFeeTooltip')}
+        className={CardCellClassName}
+      >
         <StatePercentage isLoading={false} value="2" />
       </DetailsCardCell>
 
