8f1e38f770f3becaaf4930ab70fa1f4ab435045d
Digberi <orkepuma@gmail.com>
2022-03-07 15:41:48 +0200

    add confirm operation to stake and unstake

--- src/containers/staking/hooks/use-do-stake.ts
+++ src/containers/staking/hooks/use-do-stake.ts
@@ -6,18 +6,19 @@ import { stakeTokenApi } from '@api/staking/stake-token.api';
 import { useToasts } from '@hooks/use-toasts';
 import { StakingItem } from '@interfaces/staking.interfaces';
 import { useRootStore } from '@providers/root-store-provider';
+import { useConfirmOperation } from '@utils/dapp/confirm-operation';
 import { defined } from '@utils/helpers';
 import { Token, WhitelistedBaker } from '@utils/types';
 
 export const useDoStake = () => {
   const rootStore = useRootStore();
-
-  const { showErrorToast, showSuccessToast } = useToasts();
+  const confirmOperation = useConfirmOperation();
+  const { showErrorToast } = useToasts();
 
   const doStake = useCallback(
     async (stakeItem: StakingItem, balance: BigNumber, tokenAddress: Token, selectedBaker: WhitelistedBaker) => {
       try {
-        await stakeTokenApi(
+        const operation = await stakeTokenApi(
           defined(rootStore.tezos),
           tokenAddress,
           defined(rootStore.authStore.accountPkh),
@@ -25,14 +26,15 @@ export const useDoStake = () => {
           balance,
           defined(selectedBaker).address
         );
-        showSuccessToast('Stake successful');
+
+        await confirmOperation(operation.opHash, { message: 'Stake successful' });
       } catch (error) {
         // eslint-disable-next-line no-console
         console.log('error', error);
         showErrorToast(error as Error);
       }
     },
-    [rootStore.authStore.accountPkh, rootStore.tezos, showErrorToast, showSuccessToast]
+    [rootStore.authStore.accountPkh, rootStore.tezos, showErrorToast, confirmOperation]
   );
 
   return { doStake };
