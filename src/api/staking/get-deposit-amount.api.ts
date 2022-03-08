44aa7dba70a5c5ae29cd942cab4bb517330c44f9
Dmytro <herkodmytro@gmail.com>
2022-03-08 11:48:48 +0200

    Created interfaces for storage

--- src/api/staking/get-deposit-amount.api.ts
+++ src/api/staking/get-deposit-amount.api.ts
@@ -2,8 +2,8 @@ import { TezosToolkit } from '@taquito/taquito';
 import BigNumber from 'bignumber.js';
 
 import { STAKING_CONTRACT_ADDRESS } from '@app.config';
+import { StakedAmount, StakingStorate } from '@interfaces/staking-storage.interfaces';
 import { getContract } from '@utils/dapp';
-import { StakedAmount, StakingStorate } from '@utils/types';
 
 export const getDepositAmount = async (tezos: TezosToolkit, stakingId: BigNumber, accountPkh: string) => {
   const contract = await getContract(tezos, STAKING_CONTRACT_ADDRESS);
