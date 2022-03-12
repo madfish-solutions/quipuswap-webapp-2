821bf57c0c29a2ebbf905cfefe36f5cf8c2ce90f
Digberi <orkepuma@gmail.com>
2022-03-12 12:24:08 +0200

    clean use-do-harvest

--- src/containers/staking/hooks/use-do-harvest.ts
+++ src/containers/staking/hooks/use-do-harvest.ts
@@ -6,17 +6,12 @@ import { StakingItem } from '@interfaces/staking.interfaces';
 import { useRootStore } from '@providers/root-store-provider';
 import { useConfirmOperation } from '@utils/dapp/confirm-operation';
 import { defined } from '@utils/helpers';
-import { sleep } from '@utils/helpers/sleep';
 import { Nullable } from '@utils/types';
-
-import { DELAY_BEFORE_DATA_UPDATE } from './constants';
-import { useGetStakingItem } from './use-get-staking-item';
 
 export const useDoHarvest = () => {
   const rootStore = useRootStore();
   const confirmOperation = useConfirmOperation();
   const { showErrorToast } = useToasts();
-  const { getStakingItem } = useGetStakingItem();
 
   const doHarvest = useCallback(
     async (stakeItem: Nullable<StakingItem>) => {
@@ -28,17 +23,13 @@ export const useDoHarvest = () => {
         );
 
         await confirmOperation(operation.opHash, { message: 'Harvest successful' });
-
-        await sleep(DELAY_BEFORE_DATA_UPDATE);
-
-        await getStakingItem(defined(stakeItem).id);
       } catch (error) {
         // eslint-disable-next-line no-console
         console.log('error', error);
         showErrorToast(error as Error);
       }
     },
-    [rootStore.authStore.accountPkh, rootStore.tezos, showErrorToast, confirmOperation, getStakingItem]
+    [rootStore.authStore.accountPkh, rootStore.tezos, showErrorToast, confirmOperation]
   );
 
   return { doHarvest };
