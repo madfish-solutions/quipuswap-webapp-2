9a048f0739c830366cd94e144ba3e8b2796aa312
Inokentii Mazhara <keshan3262@gmail.com>
2022-03-14 13:51:24 +0200

    Fix staking fractional amount of token to token LP tokens

--- src/utils/mapping/staking.map.ts
+++ src/utils/mapping/staking.map.ts
@@ -1,24 +1,29 @@
 import BigNumber from 'bignumber.js';
 
+import { DEFAULT_DECIMALS } from '@app.config';
 import { RawStakeStats, RawStakingItem, StakeStats, StakingItem } from '@interfaces/staking.interfaces';
 import { isExist, getTokensName, fromDecimals } from '@utils/helpers';
 import { Token, Undefined, Nullable, Optional } from '@utils/types';
 
 import { balanceMap } from './balance.map';
 
-const DEFAULT_DECIMALS = 0;
+const DEFAULT_MAP_BN_DECIMALS = 0;
 const FEES_PERCENTAGE_PRECISION = 16;
 
-const mapStakingToken = (raw: Token, newSymbol?: string): Token => ({
+const mapStakingToken = (raw: Token, isLp?: boolean, newSymbol?: string): Token => ({
   ...raw,
   fa2TokenId: raw.fa2TokenId === undefined ? undefined : Number(raw.fa2TokenId),
-  metadata: { ...raw.metadata, symbol: newSymbol ?? raw.metadata.symbol }
+  metadata: {
+    ...raw.metadata,
+    decimals: isLp ? DEFAULT_DECIMALS : raw.metadata.decimals,
+    symbol: newSymbol ?? raw.metadata.symbol
+  }
 });
 
 function mapRawBigNumber(raw: BigNumber.Value, decimals?: number): BigNumber;
 function mapRawBigNumber(raw: Undefined<BigNumber.Value>, decimals?: number): Undefined<BigNumber>;
 function mapRawBigNumber(raw: Nullable<BigNumber.Value>, decimals?: number): Nullable<BigNumber>;
-function mapRawBigNumber(raw: Optional<BigNumber.Value>, decimals = DEFAULT_DECIMALS): Optional<BigNumber> {
+function mapRawBigNumber(raw: Optional<BigNumber.Value>, decimals = DEFAULT_MAP_BN_DECIMALS): Optional<BigNumber> {
   return isExist(raw) ? fromDecimals(new BigNumber(raw), decimals) : raw;
 }
 
@@ -31,7 +36,7 @@ const nullableBalanceMap = (balanceAmount: Optional<string>, token: Token) => {
 };
 
 export const mapStakeItem = (raw: RawStakingItem): StakingItem => {
-  const stakedToken = mapStakingToken(raw.stakedToken, getTokensName(raw.tokenA, raw.tokenB));
+  const stakedToken = mapStakingToken(raw.stakedToken, Boolean(raw.tokenB), getTokensName(raw.tokenA, raw.tokenB));
   const rewardToken = mapStakingToken(raw.rewardToken);
 
   const myBalance = nullableBalanceMap(raw.myBalance, stakedToken);
