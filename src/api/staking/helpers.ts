e2401a6b4639780e5e0bda851acc2a7e2f7e982d
Innokentiy Mazhara <keshan3262@gmail.com>
2022-03-17 17:42:28 +0200

    Update helpers.ts

--- src/api/staking/helpers.ts
+++ src/api/staking/helpers.ts
@@ -23,7 +23,6 @@ const MILISECONDS_IN_SECONDS = 1000;
 export const REWARD_PRECISION = 1e18;
 
 export const fromRewardPrecision = (reward: BigNumber) => reward.dividedToIntegerBy(new BigNumber(REWARD_PRECISION));
-export const toRewardPrecision = (reward: BigNumber) => reward.multipliedBy(new BigNumber(REWARD_PRECISION));
 
 const isZeroString = (string: string) => string === '0';
 
