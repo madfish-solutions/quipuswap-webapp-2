609b7659599469aaeab5580b8f1bacf8aba86def
Artem Koziar <koziar.artem@gmail.com>
2022-03-21 17:02:35 +0200

    Add jest --watch & tests

--- src/utils/helpers/big-number-to-string/big-number-to-string.test.ts
+++ src/utils/helpers/big-number-to-string/big-number-to-string.test.ts
@@ -2,10 +2,14 @@ import { BigNumber } from 'bignumber.js';
 
 import { bigNumberToString } from './big-number-to-string';
 
-const valueForTesting = 123;
-
-test('Testing bigmunberToString function', () => {
-  const valueForTestingBN = new BigNumber(valueForTesting);
-
-  expect(bigNumberToString(valueForTestingBN)).toBe('123');
+describe('bigNumberToString', () => {
+  test('Should_Return0_When_BigNumberIsZero', () => {
+    expect(bigNumberToString(new BigNumber(0))).toBe('0');
+  });
+  test('Should_ReturnCorrectValue_When_BigNumberIsInt', () => {
+    expect(bigNumberToString(new BigNumber(123))).toBe('123');
+  });
+  test('Should_ReturnCorrectValue_When_BigNumberIsFloat', () => {
+    expect(bigNumberToString(new BigNumber(123.123))).toBe('123.123');
+  });
 });
