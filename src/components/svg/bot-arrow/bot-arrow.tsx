fafdeec85d66eac09fd3abde2961178f81023cd9
Digberi <orkepuma@gmail.com>
2022-03-22 16:11:46 +0200

    fix arrow color in NumberInput

---
+++ src/components/svg/bot-arrow/bot-arrow.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+export const BotArrow: FC<IconProps> = ({ className }) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path d="M3 6L8 11L13 6H3Z" fill="#A1A4B1" />
+    </svg>
+  );
+};
