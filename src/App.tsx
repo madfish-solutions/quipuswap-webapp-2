d9cfb397e593ea6978422a1b8cb855553bb9f563
Digberi <orkepuma@gmail.com>
2022-03-24 16:37:28 +0200

    fix test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import '@quipuswap/ui-kit/dist/ui-kit.cjs.development.css';
 import { ConnectWalletButton } from '@shared';
 
-export const sa dApp = () => {
+export const App = () => {
   return (
     <div className="App">
       <ConnectWalletButton />
