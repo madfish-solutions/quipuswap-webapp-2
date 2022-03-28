58123921cf9fdeec86a595abb4dad3930a8adf8b
Artem Koziar <koziar.artem@gmail.com>
2022-03-28 12:22:47 +0300

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import '@quipuswap/ui-kit/dist/ui-kit.cjs.development.css';
 import { ConnectWalletButton } from '@shared';
 
 export default function App() {
+
+
   return (
     <div className="App">
       <ConnectWalletButton />
