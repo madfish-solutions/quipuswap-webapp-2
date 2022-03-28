926d87aa6459e458ff5d6193e13c3ccb84af3b0d
Artem Koziar <koziar.artem@gmail.com>
2022-03-28 10:22:49 +0300

    Add ITHACANET_BAKERS

--- src/app.config.ts
+++ src/app.config.ts
@@ -252,6 +252,49 @@ export const HANGZHOUNET_BAKERS: WhitelistedBaker[] = [
   }
 ];
 
+export const ITHACANET_BAKERS: WhitelistedBaker[] = [
+  {
+    address: 'tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9'
+  },
+  {
+    address: 'tz1MeT8NACB8Q4uV9dPQ3YxXBmYgapbxQxQ5',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1MeT8NACB8Q4uV9dPQ3YxXBmYgapbxQxQ5'
+  },
+  {
+    address: 'tz1cg5EqC3WdZgRSvGJeW328S4KQNrT4jvyv',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1cg5EqC3WdZgRSvGJeW328S4KQNrT4jvyv'
+  },
+  {
+    address: 'tz1NiaviJwtMbpEcNqSP6neeoBYj8Brb3QPv',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1NiaviJwtMbpEcNqSP6neeoBYj8Brb3QPv'
+  },
+  {
+    address: 'tz1RJ74UepJA9tigjoEeUEFajowzVB3QQaVx',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1RJ74UepJA9tigjoEeUEFajowzVB3QQaVx'
+  },
+  {
+    address: 'tz1iZ9LkpAhN8X1L6RpBtfy3wxpEWzFrXz8j',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1iZ9LkpAhN8X1L6RpBtfy3wxpEWzFrXz8j'
+  },
+  {
+    address: 'tz1foXHgRzdYdaLgX6XhpZGxbBv42LZ6ubvE',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1foXHgRzdYdaLgX6XhpZGxbBv42LZ6ubvE'
+  },
+  {
+    address: 'tz1VoSM93UoY5gjuvb1bHdwdJZzU4P5eEAs4',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1VoSM93UoY5gjuvb1bHdwdJZzU4P5eEAs4'
+  },
+  {
+    address: 'tz1dqPQn5HXNJ7yjcqBx2w6sozjPXTV1kpfh',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1dqPQn5HXNJ7yjcqBx2w6sozjPXTV1kpfh'
+  },
+  {
+    address: 'tz1KkJtLB9pMdLKNpVRNZw9zmysrxKmYcRGU',
+    logo: 'https://services.tzkt.io/v1/avatars/tz1KkJtLB9pMdLKNpVRNZw9zmysrxKmYcRGU'
+  }
+];
+
 const dummyBakers: Record<QSNets, string> = {
   [QSNets.mainnet]: 'tz1aRoaRhSpRYvFdyvgWLL6TGyRoGF51wDjM',
   [QSNets.hangzhounet]: 'tz1aWXP237BLwNHJcCD4b3DutCevhqq2T1Z9',
