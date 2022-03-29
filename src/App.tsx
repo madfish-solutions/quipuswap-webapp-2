e32e294c32fdb37555284115dea3c90a60f808ce
Dmytro <herkodmytro@gmail.com>
2022-03-29 18:04:23 +0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable import/no-default-export */
 
+import { Routes, Route } from 'react-router-dom';
+
 import '@quipuswap/ui-kit/dist/ui-kit.cjs.development.css';
-import { ConnectWalletButton } from '@shared';
+import { ConnectWalletButton } from '@shared/components';
 
 export default function App() {
   return (
     <div className="App">
-      <ConnectWalletButton />
+      <Routes>
+        <Route path="/" element={<ConnectWalletButton />} />
+        <Route path="/terms" element={<h1>Terms of Usage</h1>} />
+        <Route path="/privacy" element={<h1>Privacy Policy</h1>} />
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { DAppProvider } from './providers/use-dapp';
@@ -9,10 +10,12 @@ import { WalletWrapper } from './providers/wallet-wrapper';
 require('dotenv').config();
 
 render(
-  <DAppProvider>
-    <WalletWrapper>
-      <App />
-    </WalletWrapper>
-  </DAppProvider>,
+  <BrowserRouter>
+    <DAppProvider>
+      <WalletWrapper>
+        <App />
+      </WalletWrapper>
+    </DAppProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
