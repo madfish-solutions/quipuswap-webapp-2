d29b564fe666ba9ef12db75fa405c6ec06b0a92e
Dmytro <herkodmytro@gmail.com>
2022-03-30 18:22:28 +0300

    Added globals.scss styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 /* eslint-disable import/no-default-export */
 
-import { Link } from 'react-router-dom';
-
 import { Layout } from './layout';
-import { Router, routes } from './router';
+import { Router } from './router';
+
+import './styles/globals.scss';
 
 export default function App() {
   return (
     <Layout>
-      <Link to={routes.home}>Home</Link>
-      <Link to={routes.privacyPolicy}>Privacy Policy</Link>
-      <Link to={routes.termsOfService}>Terms of Service</Link>
       <Router />
     </Layout>
   );
