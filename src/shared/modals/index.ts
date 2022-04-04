a2e77bbe03550936656adf78cf477bfab105b58c
Dmytro <herkodmytro@gmail.com>
2022-04-04 16:51:30 +0300

    Added support Ukraine button

--- src/shared/modals/index.ts
+++ src/shared/modals/index.ts
@@ -1,5 +1,6 @@
 export * from './account-modal';
 export * from './wallet-modal';
+export * from './donation-modal';
 export * from './modal';
 export * from './PositionsModal';
 export * from './BakersModal';
