8d4f6aaf49395d90384a335a7e7aca92c2443f5c
Artem <violence044@gmail.com>
2022-04-05 14:52:42 +0300

    Remove WalletWrapper

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,4 +2,3 @@ export * from './color-theme-context';
 export * from './root-store-provider';
 export * from './use-global-modals-state';
 export * from './use-dapp';
-export * from './wallet-wrapper';
