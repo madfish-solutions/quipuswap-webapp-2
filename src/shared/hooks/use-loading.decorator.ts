4fcea1fdeb56e08ee43879be5b30b001a478e44f
Dmytro <herkodmytro@gmail.com>
2022-04-11 15:04:09 +0300

    Fixed use-loading decorator parameters

--- src/shared/hooks/use-loading.decorator.ts
+++ src/shared/hooks/use-loading.decorator.ts
@@ -1,18 +1,16 @@
 import { useCallback, useState } from 'react';
 
-import { Undefined } from '@shared/types';
-
 import { useToasts } from './use-toasts';
 
-export const useLoadingDecorator = <T>(func: () => Promise<T>): [boolean, () => Promise<Undefined<T>>] => {
+export const useLoadingDecorator = <T>(func: () => Promise<T>): [boolean, () => Promise<void>] => {
   const { showErrorToast } = useToasts();
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const decoratorFunction = useCallback<() => Promise<Undefined<T>>>(async () => {
+  const decoratorFunction = useCallback<() => Promise<void>>(async () => {
     setIsLoading(true);
     try {
-      return await func();
+      await func();
     } catch (error) {
       showErrorToast(error as Error);
     } finally {
