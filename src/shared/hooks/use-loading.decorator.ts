0a1317300a29a10adf25be8b864e8f43109a95d9
Dmytro <herkodmytro@gmail.com>
2022-04-11 15:22:06 +0300

    Fixed type of decorator function

--- src/shared/hooks/use-loading.decorator.ts
+++ src/shared/hooks/use-loading.decorator.ts
@@ -1,16 +1,18 @@
 import { useCallback, useState } from 'react';
 
+import { Undefined } from '@shared/types';
+
 import { useToasts } from './use-toasts';
 
-export const useLoadingDecorator = <T>(func: () => Promise<T>): [boolean, () => Promise<void>] => {
+export const useLoadingDecorator = <T>(func: () => Promise<T>): [boolean, () => Promise<Undefined<T>>] => {
   const { showErrorToast } = useToasts();
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const decoratorFunction = useCallback<() => Promise<void>>(async () => {
+  const decoratorFunction = useCallback<() => Promise<Undefined<T>>>(async () => {
     setIsLoading(true);
     try {
-      await func();
+      return await func();
     } catch (error) {
       showErrorToast(error as Error);
     } finally {
