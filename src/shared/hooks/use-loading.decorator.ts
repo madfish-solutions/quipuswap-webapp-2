80ab82756da4c097f0f1b301a83ae606eaad82a2
Dmytro <herkodmytro@gmail.com>
2022-04-11 15:02:40 +0300

    use-loading is a generic

--- src/shared/hooks/use-loading.decorator.ts
+++ src/shared/hooks/use-loading.decorator.ts
@@ -1,13 +1,15 @@
 import { useCallback, useState } from 'react';
 
+import { Undefined } from '@shared/types';
+
 import { useToasts } from './use-toasts';
 
-export const useLoadingDecorator = (func: <T>() => Promise<T>) => {
+export const useLoadingDecorator = <T>(func: () => Promise<T>): [boolean, () => Promise<Undefined<T>>] => {
   const { showErrorToast } = useToasts();
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const decoratorFunction = useCallback<() => Promise<void>>(async () => {
+  const decoratorFunction = useCallback<() => Promise<Undefined<T>>>(async () => {
     setIsLoading(true);
     try {
       return await func();
