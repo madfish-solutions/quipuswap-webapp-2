e412c29d9b4286330b8173db4f859d4f8d7b393e
Artem Koziar <koziar.artem@gmail.com>
2022-04-25 15:06:03 +0300

    Add style of 404 page

--- src/modules/errors/page-not-found.page.tsx
+++ src/modules/errors/page-not-found.page.tsx
@@ -2,9 +2,12 @@ import { FC } from 'react';
 
 import { PageTitle, TestnetAlert } from '@shared/components';
 
+import s from './page-not-found.module.scss';
+
 export const PageNotFoundPage: FC = () => (
   <>
     <TestnetAlert />
     <PageTitle>Page Not Found</PageTitle>
+    <div className={s.statusCode}>404</div>
   </>
 );
