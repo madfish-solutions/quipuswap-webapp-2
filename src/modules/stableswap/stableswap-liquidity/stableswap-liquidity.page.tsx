582f691e4abc71432e32be98830a6ef2be6009e9
Artem Koziar <koziar.artem@gmail.com>
2022-04-25 13:47:36 +0300

    Add StableswapLiquidityPage

---
+++ src/modules/stableswap/stableswap-liquidity/stableswap-liquidity.page.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+
+export const StableswapLiquidityPage: FC = () => <h1>Stableswap Liquidity</h1>;
--- src/router.tsx
+++ src/router.tsx
@@ -1,35 +1,44 @@
 import { FC } from 'react';
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import { PageNotFoundPage } from '@modules/errors';
 import { Farming } from '@modules/farming';
 import { Home } from '@modules/home';
 import { Liquidity } from '@modules/liquidity';
 import { PrivacyPolicy } from '@modules/privacy-policy';
+import { StableswapLiquidityPage } from '@modules/stableswap';
 import { Send, Swap } from '@modules/swap/swap.page';
 import { TermsOfService } from '@modules/terms-of-service';
 import { VotingPage } from '@modules/voting';
 
 export const routes = {
   home: '/',
-  privacyPolicy: 'privacy-policy',
-  termsOfService: 'terms-of-service',
-  liquidity: 'liquidity/*',
-  swap: 'swap/*',
-  send: 'send/*',
-  farming: 'farming/*',
-  voting: 'voting/*'
+  privacyPolicy: '/privacy-policy',
+  termsOfService: '/terms-of-service',
+  liquidity: '/liquidity/*',
+  swap: '/swap/*',
+  send: '/send/*',
+  farming: '/farming/*',
+  voting: '/voting/*',
+  stableswap: '/stableswap',
+  stableswapLiquidity: '/stableswap/liquidity'
 };
 
 export const Router: FC = () => (
   <Routes>
     <Route path={routes.home} element={<Home />} />
+
     <Route path={routes.swap} element={<Swap />} />
     <Route path={routes.send} element={<Send />} />
     <Route path={routes.liquidity} element={<Liquidity />} />
     <Route path={routes.voting} element={<VotingPage />} />
+
+    <Route path={routes.stableswapLiquidity} element={<StableswapLiquidityPage />} />
+    <Route path={routes.stableswap} element={<Navigate replace to={routes.stableswapLiquidity} />} />
+
     <Route path={routes.farming} element={<Farming />} />
+
     <Route path={routes.privacyPolicy} element={<PrivacyPolicy />} />
     <Route path={routes.termsOfService} element={<TermsOfService />} />
 
