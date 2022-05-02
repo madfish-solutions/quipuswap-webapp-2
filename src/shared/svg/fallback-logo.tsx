e4becc14cb834ec0b5d122b71e429bb13849cd25
Artem Koziar <koziar.artem@gmail.com>
2022-05-02 15:37:54 +0300

    Fix FallbackLogo size

--- src/shared/svg/fallback-logo.tsx
+++ src/shared/svg/fallback-logo.tsx
@@ -11,8 +11,8 @@ export const FallbackLogo: FC<IconProps> = ({ className }) => {
 
   return (
     <svg
-      width="128"
-      height="128"
+      width="48"
+      height="48"
       viewBox="0 0 128 128"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
