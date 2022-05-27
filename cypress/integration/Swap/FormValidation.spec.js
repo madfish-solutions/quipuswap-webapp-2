8c3d0b6506143606d0f298c2b9aa159e322fb755
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2022-05-27 12:26:08 +0300

    QUIPU-13 Added test for fied validation on Swap page

--- cypress/integration/Swap/FormValidation.spec.js
+++ cypress/integration/Swap/FormValidation.spec.js
@@ -5,10 +5,33 @@ describe('Check error notification', () => {
         // Go to the swap page 
         cy.visit('/swap');
         cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.get('[for="swap-send-from"]').click().type('1.2222221');
-
-        //next string should change to correct selector, when it will be
-        cy.get('[class="ComplexInput_errorLabel__OA4q3"]')
-        .invoke('text').should('contains', 'The precision of TEZ token');
+        cy.get('[for="swap-send-from"]').click().type('1.33332222221');
+        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
+            .invoke('text')
+            .then(text => {
+                expect(text).to.contain('The precision of TEZ token')
+            })
     });
+    it('Should_DisplayError_When_AmountOfFromFieldTooBig', () => {
+        // Go to the swap page 
+        cy.visit('/swap');
+        cy.get('[data-test-id="acceptCookieButton"]').click();
+        cy.get('[for="swap-send-from"]').click().type('133332222221');
+        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
+            .invoke('text')
+            .then(text => {
+                expect(text).to.contain('Value has to be a number between 0.000001')
+            })
+    });
+    it('Should_DisplayError_When_FieldIsEmpty', () => {
+        // Input data in the From field and delete it. It should be notification that field is required
+        cy.visit('/swap');
+        cy.get('[data-test-id="acceptCookieButton"]').click();
+        cy.get('[for="swap-send-from"]').click().type('1').type('{backspace}');
+        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
+            .invoke('text')
+            .then(text => {
+                expect(text).to.contain('This field is required')
+            })
+    }); 
 });
--- cypress/integration/Swap/Validation.spec.js
+++ cypress/integration/Swap/Validation.spec.js
@@ -6,9 +6,7 @@ describe('Check input fields', () => {
         cy.visit('/swap');
         cy.get('[data-test-id="acceptCookieButton"]').click();
         cy.get('[for="swap-send-from"]').click().type('1');
-
-        //next string should change to correct selector, when it will be
-        cy.get('[data-test-id="to"] [class="ComplexInput_item3__ue+3C ComplexInput_input__QCsvP"]')
+        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="to"] [data-test-id="input"]')
             .invoke('val').then(parseFloat).should('be.gte', 0);
     });
     it('Should_CalculateFromField_When_ToFieldIsFilled', () => {
@@ -16,9 +14,7 @@ describe('Check input fields', () => {
         cy.visit('/swap');
         cy.get('[data-test-id="acceptCookieButton"]').click();
         cy.get('[data-test-id="to"]').click().type('1');
-
-        //next string should change to correct selector, when it will be
-        cy.get('[data-test-id="from"] [class="ComplexInput_item3__ue+3C ComplexInput_input__QCsvP"]')
+        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="input"]')
             .invoke('val').then(parseFloat).should('be.gte', 0);
     });
 });
