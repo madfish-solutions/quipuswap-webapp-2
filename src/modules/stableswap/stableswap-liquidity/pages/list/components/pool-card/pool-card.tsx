c31b161f5e4222f8a4a5f9fdd32534310cfdf0de
Artem <violence044@gmail.com>
2022-06-02 09:35:49 +0300

    QUIPU-16 update card layout

--- src/modules/stableswap/stableswap-liquidity/pages/list/components/pool-card/pool-card.tsx
+++ src/modules/stableswap/stableswap-liquidity/pages/list/components/pool-card/pool-card.tsx
@@ -1,12 +1,12 @@
 import { FC, useContext } from 'react';
 
-import { AppRootRoutes } from '@app.router';
+import { Link } from 'react-router-dom';
+
 import { DOLLAR, PERCENT } from '@config/constants';
 import { extractTokens } from '@modules/stableswap/helpers';
 import { StableswapItem } from '@modules/stableswap/types';
 import { ColorModes, ColorThemeContext } from '@providers/color-theme-context';
 import {
-  Button,
   Card,
   Iterator,
   ListItemCardCell,
@@ -16,7 +16,6 @@ import {
   TokensSymbols
 } from '@shared/components';
 
-import { StableswapRoutes, Tabs } from '../../../../../stableswap.page';
 import { preparePoolAmounts } from './pool-card.helpers';
 import styles from './pool-card.module.scss';
 import { usePoolCardViewModel } from './pool-card.vm';
@@ -30,23 +29,19 @@ interface Props extends StableswapItem {
   className?: string;
 }
 
-export const PoolCard: FC<Props> = ({
-  tokensInfo,
-  tvlInUsd,
-  isWhitelisted,
-  liquidityProvidersFee,
-  stableswapItemUrl
-}) => {
+export const PoolCard: FC<Props> = ({ id, tokensInfo, tvlInUsd, isWhitelisted, liquidityProvidersFee }) => {
   const { colorThemeMode } = useContext(ColorThemeContext);
 
   const { whitelistedTag, translation } = usePoolCardViewModel();
 
   const { status, label } = whitelistedTag;
-  const { totalValueTranslation, liquidityProvidersFeeTranslation, selectTranslation, valueTranslation } = translation;
+  const { totalValueTranslation, liquidityProvidersFeeTranslation, valueTranslation } = translation;
+
+  const selectLink = `${id}`;
 
   return (
-    <Card className={modeClass[colorThemeMode]} contentClassName={styles.poolCard}>
-      <div className={styles.info}>
+    <Link to={selectLink}>
+      <Card className={modeClass[colorThemeMode]} contentClassName={styles.poolCard}>
         <div className={styles.poolInfo}>
           <div className={styles.logoSymbols}>
             <TokensLogos className={styles.tokensLogos} tokens={extractTokens(tokensInfo)} width={48} />
@@ -55,51 +50,48 @@ export const PoolCard: FC<Props> = ({
 
           {isWhitelisted && <StatusLabel className={styles.whitelistedTag} status={status} label={label} filled />}
         </div>
-
-        <div className={styles.poolStats}>
+        <div className={styles.stats}>
           <ListItemCardCell
-            cellName={totalValueTranslation}
+            cellName={valueTranslation}
             cellNameClassName={styles.cardCellHeader}
-            cardCellClassName={styles.cardCell}
+            cardCellClassName={styles.tokensValue}
           >
-            <StateCurrencyAmount
-              className={styles.poolStatsAmount}
-              amount={tvlInUsd}
-              currency={DOLLAR}
-              isLeftCurrency
+            <Iterator
+              isGrouped
+              wrapperClassName={styles.tokensInfo}
+              render={StateCurrencyAmount}
+              data={preparePoolAmounts(tokensInfo)}
             />
           </ListItemCardCell>
 
-          <ListItemCardCell
-            cellName={liquidityProvidersFeeTranslation}
-            cellNameClassName={styles.cardCellHeader}
-            cardCellClassName={styles.cardCell}
-          >
-            <StateCurrencyAmount className={styles.poolStatsAmount} amount={liquidityProvidersFee} currency={PERCENT} />
-          </ListItemCardCell>
-        </div>
-      </div>
-      <div className={styles.stats}>
-        <ListItemCardCell
-          cellName={valueTranslation}
-          cellNameClassName={styles.cardCellHeader}
-          cardCellClassName={styles.tokensValue}
-        >
-          <Iterator
-            isGrouped
-            wrapperClassName={styles.tokensInfo}
-            render={StateCurrencyAmount}
-            data={preparePoolAmounts(tokensInfo)}
-          />
-        </ListItemCardCell>
+          <div className={styles.poolStats}>
+            <ListItemCardCell
+              cellName={totalValueTranslation}
+              cellNameClassName={styles.cardCellHeader}
+              cardCellClassName={styles.cardCell}
+            >
+              <StateCurrencyAmount
+                className={styles.poolStatsAmount}
+                amount={tvlInUsd}
+                currency={DOLLAR}
+                isLeftCurrency
+              />
+            </ListItemCardCell>
 
-        <Button
-          className={styles.button}
-          href={`${AppRootRoutes.Stableswap}${StableswapRoutes.liquidity}${Tabs.add}/${stableswapItemUrl}`}
-        >
-          {selectTranslation}
-        </Button>
-      </div>
-    </Card>
+            <ListItemCardCell
+              cellName={liquidityProvidersFeeTranslation}
+              cellNameClassName={styles.cardCellHeader}
+              cardCellClassName={styles.cardCell}
+            >
+              <StateCurrencyAmount
+                className={styles.poolStatsAmount}
+                amount={liquidityProvidersFee}
+                currency={PERCENT}
+              />
+            </ListItemCardCell>
+          </div>
+        </div>
+      </Card>
+    </Link>
   );
 };
