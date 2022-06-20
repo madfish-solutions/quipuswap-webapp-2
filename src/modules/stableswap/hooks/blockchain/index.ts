bc0f91ce69f8b53e56a9f4442509f181e88b931d
Digberi <orkepuma@gmail.com>
2022-06-20 21:41:50 +0300

    QUIPU-48 QUIPU-49 add stablefarm reward info implement harvest all button

--- src/modules/stableswap/hooks/blockchain/index.ts
+++ src/modules/stableswap/hooks/blockchain/index.ts
@@ -1,3 +1,4 @@
 export * from './use-add-stableswap-liquidity';
 export * from './use-stableswap-farm-stake';
 export * from './use-remove-stableswap-liquidity';
+export * from './use-stablesfarm-harvest-all';
