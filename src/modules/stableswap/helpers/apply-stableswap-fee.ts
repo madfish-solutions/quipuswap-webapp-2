19e387a81fb15bd176b192ac7cc78dbfbc625c64
Digberi <orkepuma@gmail.com>
2022-06-20 10:59:54 +0300

    QUIPU-167 delete unused code

--- src/modules/stableswap/helpers/apply-stableswap-fee.ts
+++ src/modules/stableswap/helpers/apply-stableswap-fee.ts
@@ -2,12 +2,8 @@ import { BigNumber } from 'bignumber.js';
 
 import { PERCENTAGE_100 } from '@config/constants';
 
-export const apllyStableswapFee = (value: BigNumber, fees: Array<BigNumber>, increase = false) => {
+export const apllyStableswapFee = (value: BigNumber, fees: Array<BigNumber>) => {
   const totalFee = BigNumber.sum(...fees);
-
-  if (increase) {
-    value.multipliedBy(PERCENTAGE_100.plus(totalFee)).dividedBy(PERCENTAGE_100);
-  }
 
   return value.multipliedBy(PERCENTAGE_100.minus(totalFee)).dividedBy(PERCENTAGE_100);
 };
