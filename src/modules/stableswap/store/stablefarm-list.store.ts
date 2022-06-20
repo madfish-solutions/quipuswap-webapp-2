856c3cde0eb5ae4281bec40fd8a05faeee54657b
Digberi <orkepuma@gmail.com>
2022-06-20 16:59:40 +0300

    QUIPU-58 fix earned value calculation

--- src/modules/stableswap/store/stablefarm-list.store.ts
+++ src/modules/stableswap/store/stablefarm-list.store.ts
@@ -1,10 +1,19 @@
+import { BigNumber } from 'bignumber.js';
 import { computed, makeObservable } from 'mobx';
 
+import { fromDecimals, isEmptyArray } from '@shared/helpers';
 import { LoadingErrorData, RootStore } from '@shared/store';
 
 import { getStableFarmListApi, getStableFarmStatsApi, getStakerInfo } from '../api';
 import { farmsListMapper, stakerInfoMapper, statsMapper } from '../mapping';
-import { RawStableFarmItem, RawStableFarmStats, StableFarmItem, StableFarmStats, StakerInfo } from '../types';
+import {
+  RawStableFarmItem,
+  RawStableFarmStats,
+  RawStakerInfo,
+  StableFarmItem,
+  StableFarmStats,
+  StakerInfo
+} from '../types';
 
 export class StableFarmListStore {
   readonly statsStore = new LoadingErrorData<RawStableFarmStats, Nullable<StableFarmStats>>(
@@ -19,7 +28,7 @@ export class StableFarmListStore {
     farmsListMapper
   );
 
-  readonly stakerInfo = new LoadingErrorData<Array<StakerInfo>, Array<StakerInfo>>(
+  readonly stakerInfo = new LoadingErrorData<Array<RawStakerInfo>, Array<RawStakerInfo>>(
     [],
     async () => await getStakerInfo(this.rootStore.tezos, this.list, this.rootStore.authStore.accountPkh),
     stakerInfoMapper
@@ -41,7 +50,27 @@ export class StableFarmListStore {
     return this.listStore.data;
   }
 
-  get info() {
-    return this.stakerInfo.data;
+  //TODO: find a better way to mapping
+  get info(): Array<StakerInfo> {
+    if (isEmptyArray(this.stakerInfo.data) || isEmptyArray(this.list)) {
+      return [];
+    }
+
+    return this.stakerInfo.data.map(({ yourReward, yourDeposit }: RawStakerInfo, infoIndex) => {
+      let yourEarned = new BigNumber('0');
+      const { tokensInfo } = this.list[infoIndex];
+
+      yourReward?.forEach((value, tokenIndex) => {
+        const { token, exchangeRate } = tokensInfo[tokenIndex.toNumber()];
+
+        const tokenValue = fromDecimals(value, token);
+        yourEarned = yourEarned.plus(tokenValue.multipliedBy(exchangeRate));
+      });
+
+      return {
+        yourDeposit,
+        yourEarned
+      };
+    });
   }
 }
