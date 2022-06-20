23a14cf5e4119ddfcdb8b3eebf6093d5da73873d
Digberi <orkepuma@gmail.com>
2022-06-20 19:56:27 +0300

    QUIPU-58 QUIPU-59 QUIPU-60 create stablefarm reward info

--- src/translation/locales/en/stable-swap.ts
+++ src/translation/locales/en/stable-swap.ts
@@ -17,5 +17,6 @@ export const stableswap = {
   createOwnPool: 'Can’t Find appropriate pool? Create Own!',
   stableFarmTitle: 'Stable Swap Farm',
   allZeroInpupts: "Investing of 0 assets doesn't make a lot of sense. Please, provide at least one non-zero input.",
-  totalValueLocked: 'Total Value Locked'
+  totalValueLocked: 'Total Value Locked',
+  Harvest: 'Harvest'
 } as const;
