e38662d24af255d668e9a5c1fc5a0400a4ee2f49
Artem Koziar <koziar.artem@gmail.com>
2022-06-20 09:54:47 +0300

    QUIPU-154: Add config to Cypress

--- cypress/integration/Liquidity/RemoveChangeToken.spec.js
+++ cypress/integration/Liquidity/RemoveChangeToken.spec.js
@@ -1,63 +1,83 @@
 /// <reference types="cypress" />
 
+import { DEFAULT_WAIT_TIMEOUT } from '../../const';
+
 describe('Remove liquidity tab, calculating field', () => {
-    before(() => {
-        // Go to the remove page from home page
-        cy.visit('/');
-        cy.get('[data-test-id="header"] [data-test-id="menuButton"]').click();
-        cy.get('[data-test-id="menu"] [data-test-id="navigationButton-Liquidity"]').click();
-        cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="cardTab-1"]').click();
-    });
-    it('Should_DisplayOutputFields_When_InputIs1', () => {
-        cy.get('[data-test-id="positionSelectInput"]').click().type('1');
-        cy.wait(2000);
-        cy.get('[data-test-id="outputA"] [data-test-id="outputA"]').invoke('val').then(parseFloat).should('be.gte', 0);
-        cy.get('[data-test-id="outputB"] [data-test-id="outputB"]').invoke('val').then(parseFloat).should('be.gte', 0);
-    });
-    //change tez/quipu to kusd/quipu, check if output is kusd/quipu
-    it('Should_HaveKusdQuipuPair_When_TokenIsChanged', () => {
-        cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="positionSelectInput"] [data-test-id="selectLPButton"]')
-            .click();
-        cy.get('[data-test-id="TEZ"] [data-test-id="checkbox"]').click();
-        cy.get('[data-test-id="KUSD"] [data-test-id="checkbox"]').click();
-        cy.get('[data-test-id="buttonSelect"]').click();
-        cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="positionSelectInput"] [data-test-id="selectLPButton"]')
-            .invoke('text').should('contain', 'QUIPU / KUSD');
-    });
-    it('Should_DisplayMaxInvested_When_InputIs1QuipuKusd', () => {
-        cy.reload();
-        cy.get('[data-test-id="positionSelectInput"]').click().type('1');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.wait(3000);
-        cy.get('[data-test-id="amount"]')
-            .invoke('text').then(parseFloat).should('be.gte', 0);
-        cy.get('[data-test-id="amount"]').eq(1)
-            .invoke('text').then(parseFloat).should('be.gte', 0);
-    });
-    it('Should_DisplayQuipuKusdInDetailsSection_When_InputIs1', () => {
-        //Sell price in pool details section 1 quipu 
-        cy.get('[data-test-id="detailsCardCells"] [data-test-id="amount"]').eq(0)
-            .invoke('text').then(parseFloat).should('be.eq', 1);
-        cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]').eq(0)
-            .invoke('text').should('contain', 'QUIPU');
-        //sell price in pool details section =kusd
-        cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]').eq(1)
-            .invoke('text').should('contain', 'KUSD');
-        //Buy price in pool details section 1kusd = ...quipu
-        cy.get('[data-test-id="detailsCardCells"] [data-test-id="amount"]').eq(2)
-            .invoke('text').then(parseFloat).should('be.eq', 1);
-        cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]').eq(2)
-            .invoke('text').should('contain', 'KUSD');
-        cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]').eq(3)
-            .invoke('text').should('contain', 'QUIPU');
-    });
-    it('Should_DisplayNotification_When_PoolDoesntExist', () => {
-        cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="positionSelectInput"] [data-test-id="selectLPButton"]')
-            .click();
-        cy.get('[data-test-id="QUIPU"] [data-test-id="checkbox"]').click();
-        cy.get('[data-test-id="WRAP"] [data-test-id="checkbox"]').click();
-        cy.get('[data-test-id="buttonSelect"]').click();
-        cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="connectButton"]').prev()
-            .should('contain', 'Note! The pool does')
-    });
+  before(() => {
+    // Go to the remove page from home page
+    cy.visit('/');
+    cy.get('[data-test-id="header"] [data-test-id="menuButton"]').click();
+    cy.get('[data-test-id="menu"] [data-test-id="navigationButton-Liquidity"]').click();
+    cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="cardTab-1"]').click();
+  });
+  it('Should_DisplayOutputFields_When_InputIs1', () => {
+    cy.get('[data-test-id="positionSelectInput"]').click().type('1');
+    cy.wait(DEFAULT_WAIT_TIMEOUT);
+    cy.get('[data-test-id="outputA"] [data-test-id="outputA"]').invoke('val').then(parseFloat).should('be.gte', 0);
+    cy.get('[data-test-id="outputB"] [data-test-id="outputB"]').invoke('val').then(parseFloat).should('be.gte', 0);
+  });
+  //change tez/quipu to kusd/quipu, check if output is kusd/quipu
+  it('Should_HaveKusdQuipuPair_When_TokenIsChanged', () => {
+    cy.get(
+      '[data-test-id="liquidityPageTokenSelect"] [data-test-id="positionSelectInput"] [data-test-id="selectLPButton"]'
+    ).click();
+    cy.get('[data-test-id="TEZ"] [data-test-id="checkbox"]').click();
+    cy.get('[data-test-id="KUSD"] [data-test-id="checkbox"]').click();
+    cy.get('[data-test-id="buttonSelect"]').click();
+    cy.get(
+      '[data-test-id="liquidityPageTokenSelect"] [data-test-id="positionSelectInput"] [data-test-id="selectLPButton"]'
+    )
+      .invoke('text')
+      .should('contain', 'QUIPU / KUSD');
+  });
+  it('Should_DisplayMaxInvested_When_InputIs1QuipuKusd', () => {
+    cy.reload();
+    cy.get('[data-test-id="positionSelectInput"]').click().type('1');
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+    cy.wait(DEFAULT_WAIT_TIMEOUT);
+    cy.get('[data-test-id="amount"]').invoke('text').then(parseFloat).should('be.gte', 0);
+    cy.get('[data-test-id="amount"]').eq(1).invoke('text').then(parseFloat).should('be.gte', 0);
+  });
+  it('Should_DisplayQuipuKusdInDetailsSection_When_InputIs1', () => {
+    //Sell price in pool details section 1 quipu
+    cy.get('[data-test-id="detailsCardCells"] [data-test-id="amount"]')
+      .eq(0)
+      .invoke('text')
+      .then(parseFloat)
+      .should('be.eq', 1);
+    cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]')
+      .eq(0)
+      .invoke('text')
+      .should('contain', 'QUIPU');
+    //sell price in pool details section =kusd
+    cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]')
+      .eq(1)
+      .invoke('text')
+      .should('contain', 'KUSD');
+    //Buy price in pool details section 1kusd = ...quipu
+    cy.get('[data-test-id="detailsCardCells"] [data-test-id="amount"]')
+      .eq(2)
+      .invoke('text')
+      .then(parseFloat)
+      .should('be.eq', 1);
+    cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]')
+      .eq(2)
+      .invoke('text')
+      .should('contain', 'KUSD');
+    cy.get('[data-test-id="detailsCardCells"] [data-test-id="rightVisibleCurrency"]')
+      .eq(3)
+      .invoke('text')
+      .should('contain', 'QUIPU');
+  });
+  it('Should_DisplayNotification_When_PoolDoesntExist', () => {
+    cy.get(
+      '[data-test-id="liquidityPageTokenSelect"] [data-test-id="positionSelectInput"] [data-test-id="selectLPButton"]'
+    ).click();
+    cy.get('[data-test-id="QUIPU"] [data-test-id="checkbox"]').click();
+    cy.get('[data-test-id="WRAP"] [data-test-id="checkbox"]').click();
+    cy.get('[data-test-id="buttonSelect"]').click();
+    cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="connectButton"]')
+      .prev()
+      .should('contain', 'Note! The pool does');
+  });
 });
--- cypress/integration/Swap/FormValidation.spec.js
+++ cypress/integration/Swap/FormValidation.spec.js
@@ -1,39 +1,41 @@
 /// <reference types="cypress" />
 
+import { DEFAULT_WAIT_TIMEOUT } from '../../const';
+
 describe('Check error notification', () => {
-    it('Should_DisplayError_When_FromTokenHaveTooManyDecimals', () => {
-        // Go to the swap page 
-        cy.visit('/swap');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.get('[for="swap-send-from"]').click().type('1.33332222221');
-        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
-            .invoke('text')
-            .then(text => {
-                expect(text).to.contain('The precision of TEZ token')
-            })
-    });
-    it('Should_DisplayError_When_AmountOfFromFieldTooBig', () => {
-        // Go to the swap page 
-        cy.visit('/swap');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.get('[for="swap-send-from"]').click().type('133332222221');
-        cy.wait(2000);
-        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
-            .invoke('text')
-            .then(text => {
-                expect(text).to.contain('Value has to be a number between 0.000001')
-            })
-    });
-    it('Should_DisplayError_When_FieldIsEmpty', () => {
-        // Input data in the From field and delete it. It should be notification that field is required
-        cy.visit('/swap');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.get('[for="swap-send-from"]').click().type('1').type('{backspace}');
-        cy.wait(2000);
-        cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
-            .invoke('text')
-            .then(text => {
-                expect(text).to.contain('This field is required')
-            })
-    }); 
+  it('Should_DisplayError_When_FromTokenHaveTooManyDecimals', () => {
+    // Go to the swap page
+    cy.visit('/swap');
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+    cy.get('[for="swap-send-from"]').click().type('1.33332222221');
+    cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
+      .invoke('text')
+      .then(text => {
+        expect(text).to.contain('The precision of TEZ token');
+      });
+  });
+  it('Should_DisplayError_When_AmountOfFromFieldTooBig', () => {
+    // Go to the swap page
+    cy.visit('/swap');
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+    cy.get('[for="swap-send-from"]').click().type('133332222221');
+    cy.wait(DEFAULT_WAIT_TIMEOUT);
+    cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
+      .invoke('text')
+      .then(text => {
+        expect(text).to.contain('Value has to be a number between 0.000001');
+      });
+  });
+  it('Should_DisplayError_When_FieldIsEmpty', () => {
+    // Input data in the From field and delete it. It should be notification that field is required
+    cy.visit('/swap');
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+    cy.get('[for="swap-send-from"]').click().type('1').type('{backspace}');
+    cy.wait(DEFAULT_WAIT_TIMEOUT);
+    cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="error"]')
+      .invoke('text')
+      .then(text => {
+        expect(text).to.contain('This field is required');
+      });
+  });
 });
