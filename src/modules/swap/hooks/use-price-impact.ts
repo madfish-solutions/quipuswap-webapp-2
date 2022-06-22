7084d4ca32243e9d0d64e1adb5b47f093d2281ea
Inokentii Mazhara <keshan3262@gmail.com>
2022-06-22 12:39:57 +0300

    Minor refactoring

--- src/modules/swap/hooks/use-price-impact.ts
+++ src/modules/swap/hooks/use-price-impact.ts
@@ -1,9 +1,8 @@
 import { TOKEN_TO_TOKEN_DEX } from '@config/config';
+import { PERCENTAGE_100 } from '@config/constants';
 import { getPriceImpact, toDecimals } from '@shared/helpers';
 
 import { SwapDetailsParams } from '../utils/types';
-
-const WHOLE_ITEM_PERCENT = 100;
 
 export const usePriceImpact = ({ inputToken, inputAmount, dexRoute, slippageTolerance }: SwapDetailsParams) => {
   return inputToken && inputAmount && dexRoute && slippageTolerance
@@ -11,7 +10,7 @@ export const usePriceImpact = ({ inputToken, inputAmount, dexRoute, slippageTole
         inputToken: inputToken,
         inputAmount: toDecimals(inputAmount, inputToken),
         dexChain: dexRoute,
-        slippageTolerance: slippageTolerance?.div(WHOLE_ITEM_PERCENT),
+        slippageTolerance: slippageTolerance?.div(PERCENTAGE_100),
         ttDexAddress: TOKEN_TO_TOKEN_DEX
       })
     : null;
