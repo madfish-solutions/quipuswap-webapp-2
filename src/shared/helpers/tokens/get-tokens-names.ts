3c5b7c6060203a81a140b1d95781136e507e36bf
Dmytro <herkodmytro@gmail.com>
2022-06-23 10:51:26 +0300

    QUIPU-184 fixed getTokensNames helper

--- src/shared/helpers/tokens/get-tokens-names.ts
+++ src/shared/helpers/tokens/get-tokens-names.ts
@@ -3,5 +3,4 @@ import { Token } from '@shared/types';
 
 import { getTokenSymbol } from '../get-token-appellation';
 
-export const getTokensNames = (tokens: Array<Token>) =>
-  tokens.reduce((accum, value) => accum + SLASH + getTokenSymbol(value), '');
+export const getTokensNames = (tokens: Array<Token>) => tokens.map(getTokenSymbol).join(SLASH);
