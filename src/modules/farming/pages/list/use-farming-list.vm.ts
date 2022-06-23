77c30b001378d775b51b667268e479616c01395a
Dmytro <herkodmytro@gmail.com>
2022-06-23 16:16:31 +0300

    QUIPU-209 Fixed displaying user info on farms list

--- src/modules/farming/pages/list/use-farming-list.vm.ts
+++ src/modules/farming/pages/list/use-farming-list.vm.ts
@@ -5,6 +5,7 @@ import { useFarmingListStore } from '@modules/farming/hooks';
 import { useGetFarmingList } from '@modules/farming/hooks/loaders/use-get-farming-list';
 import { useGetFarmingStats } from '@modules/farming/hooks/loaders/use-get-farming-stats';
 import { useReady } from '@providers/use-dapp';
+import { getTokenSymbol } from '@shared/helpers';
 import { useTranslation } from '@translation';
 
 export const useFarmingListViewModel = () => {
@@ -29,40 +30,63 @@ export const useFarmingListViewModel = () => {
   const { listStore, list } = farmingListStore;
   const { isLoading } = listStore;
 
-  const data2 =
-    list?.map(item => ({
-      href: `${item.id}`,
-      status: { status: item.stakeStatus, filled: true },
-      inputToken: item.tokens,
-      outputToken: item.rewardToken,
-      itemStats: [
-        {
-          cellName: t('farm|tvl'),
-          amounts: {
-            amount: item.tvlInStakedToken,
-            dollarEquivalent: item.tvlInUsd,
-            currency: item.stakedToken.metadata.symbol,
-            dollarEquivalentOnly: true
-          }
-        },
-        {
-          cellName: t('farm|apr'),
-          amounts: {
-            amount: item.apr,
-            currency: PERCENT,
-            amountDecimals: 2
-          }
-        },
-        {
-          cellName: t('farm|apy'),
-          amounts: {
-            amount: item.apy,
-            currency: PERCENT,
-            amountDecimals: 2
-          }
+  // eslint-disable-next-line no-console
+  console.log('list', list);
+
+  const data2 = list?.map(item => ({
+    href: `${item.id}`,
+    status: { status: item.stakeStatus, filled: true },
+    inputToken: item.tokens,
+    outputToken: item.rewardToken,
+    isNew: item.id.eq('16') || item.id.eq('17') || item.id.eq('18'),
+    itemStats: [
+      {
+        cellName: t('farm|tvl'),
+        amounts: {
+          amount: item.tvlInStakedToken,
+          dollarEquivalent: item.tvlInUsd,
+          currency: item.stakedToken.metadata.symbol,
+          dollarEquivalentOnly: true
         }
-      ]
-    })) ?? [];
+      },
+      {
+        cellName: t('farm|apr'),
+        amounts: {
+          amount: item.apr,
+          currency: PERCENT,
+          amountDecimals: 2
+        }
+      },
+      {
+        cellName: t('farm|apy'),
+        amounts: {
+          amount: item.apy,
+          currency: PERCENT,
+          amountDecimals: 2
+        }
+      }
+    ],
+    userStats: [
+      {
+        cellName: t('farm|yourDeposit'),
+        amounts: {
+          amount: item.depositBalance,
+          dollarEquivalent: item.depositBalance?.multipliedBy(item.depositExchangeRate ?? '0'),
+          currency: getTokenSymbol(item.stakedToken),
+          dollarEquivalentOnly: true
+        }
+      },
+      {
+        cellName: t('farm|yourEarned'),
+        amounts: {
+          amount: item.earnBalance,
+          dollarEquivalent: item.earnBalance?.multipliedBy(item.earnExchangeRate ?? '0'),
+          currency: getTokenSymbol(item.rewardToken),
+          dollarEquivalentOnly: true
+        }
+      }
+    ]
+  }));
 
   return {
     isLoading,
