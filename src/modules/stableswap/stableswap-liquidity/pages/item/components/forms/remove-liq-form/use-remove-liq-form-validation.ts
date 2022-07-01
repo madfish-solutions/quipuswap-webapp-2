8412f53db2217cf9b7c7f18d73f49ca234d7a96b
Inokentii Mazhara <keshan3262@gmail.com>
2022-07-01 18:17:49 +0300

    Stableswap liquidity form: fix decimals validation

--- src/modules/stableswap/stableswap-liquidity/pages/item/components/forms/remove-liq-form/use-remove-liq-form-validation.ts
+++ src/modules/stableswap/stableswap-liquidity/pages/item/components/forms/remove-liq-form/use-remove-liq-form-validation.ts
@@ -4,25 +4,44 @@ import BigNumber from 'bignumber.js';
 import * as yup from 'yup';
 
 import { LP_INPUT_KEY } from '@config/constants';
+import { StableswapItem } from '@modules/stableswap/types';
 import { operationAmountSchema } from '@shared/helpers';
 import { NumberAsStringSchema } from '@shared/validators';
 import { useTranslation } from '@translation';
 
 import { getInputSlugByIndex } from '../../../../../../helpers';
 
+const STABLESWAP_LP_DECIMALS = 18;
 export const useRemoveLiqFormValidation = (
   userLpBalance: Nullable<BigNumber>,
   userTokenBalance: Array<BigNumber>,
+  stableswapItem: Nullable<StableswapItem>,
   isBalancedProportion: boolean
 ) => {
   const { t } = useTranslation();
 
   return useMemo(() => {
     const isZeroInclusive = !isBalancedProportion;
-    const inputAmountSchemas: Array<NumberAsStringSchema> = userTokenBalance.map(balance =>
-      operationAmountSchema(balance, isZeroInclusive)
+    const inputAmountSchemas: Array<NumberAsStringSchema> = userTokenBalance.map((balance, index) => {
+      const tokenMetadata = stableswapItem?.tokensInfo[index].token.metadata;
+
+      return operationAmountSchema(
+        balance,
+        isZeroInclusive,
+        tokenMetadata?.decimals,
+        tokenMetadata &&
+          t('common|tokenDecimalsOverflowError', {
+            tokenSymbol: tokenMetadata.symbol,
+            decimalPlaces: tokenMetadata.decimals
+          })
+      );
+    });
+    const lpInputSchema = operationAmountSchema(
+      userLpBalance,
+      undefined,
+      STABLESWAP_LP_DECIMALS,
+      t('common|tokenDecimalsOverflowError', { tokenSymbol: 'Stableswap LP', decimalPlaces: STABLESWAP_LP_DECIMALS })
     );
-    const lpInputShema = operationAmountSchema(userLpBalance);
 
     const shapeMap: Array<[string, NumberAsStringSchema]> = inputAmountSchemas.map((item, index) => {
       const schema = isBalancedProportion ? item.required(t('common|Value is required')) : item;
@@ -33,10 +52,10 @@ export const useRemoveLiqFormValidation = (
     const shape: Record<string, NumberAsStringSchema> = Object.fromEntries(shapeMap);
 
     return yup.object().shape({
-      [LP_INPUT_KEY]: lpInputShema.required(
+      [LP_INPUT_KEY]: lpInputSchema.required(
         isBalancedProportion ? t('common|Value is required') : t('common|At least one output amount should be provided')
       ),
       ...shape
     });
-  }, [isBalancedProportion, t, userLpBalance, userTokenBalance]);
+  }, [isBalancedProportion, t, userLpBalance, userTokenBalance, stableswapItem]);
 };
