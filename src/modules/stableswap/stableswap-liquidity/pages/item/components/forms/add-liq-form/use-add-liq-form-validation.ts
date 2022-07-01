ad2750b3b8f2d0e14f623754d3184c7d83422977
Inokentii Mazhara <keshan3262@gmail.com>
2022-07-01 18:17:49 +0300

    Stableswap liquidity form: fix decimals validation

--- src/modules/stableswap/stableswap-liquidity/pages/item/components/forms/add-liq-form/use-add-liq-form-validation.ts
+++ src/modules/stableswap/stableswap-liquidity/pages/item/components/forms/add-liq-form/use-add-liq-form-validation.ts
@@ -3,17 +3,36 @@ import { useMemo } from 'react';
 import BigNumber from 'bignumber.js';
 import * as yup from 'yup';
 
+import { StableswapItem } from '@modules/stableswap/types';
 import { operationAmountSchema } from '@shared/helpers';
 import { NumberAsStringSchema } from '@shared/validators';
+import { useTranslation } from '@translation';
 
 import { getInputSlugByIndex } from '../../../../../../helpers';
 
-export const useAddLiqFormValidation = (userBalance: Array<Nullable<BigNumber>>, isBalancedProportion: boolean) => {
+export const useAddLiqFormValidation = (
+  userBalance: Array<Nullable<BigNumber>>,
+  stableswapItem: Nullable<StableswapItem>,
+  isBalancedProportion: boolean
+) => {
+  const { t } = useTranslation();
+
   return useMemo(() => {
     const isZeroInclusive = !isBalancedProportion;
-    const inputAmountSchemas: Array<NumberAsStringSchema> = userBalance.map(balance =>
-      operationAmountSchema(balance, isZeroInclusive)
-    );
+    const inputAmountSchemas: Array<NumberAsStringSchema> = userBalance.map((balance, index) => {
+      const tokenMetadata = stableswapItem?.tokensInfo[index]?.token.metadata;
+
+      return operationAmountSchema(
+        balance,
+        isZeroInclusive,
+        tokenMetadata?.decimals,
+        tokenMetadata &&
+          t('common|tokenDecimalsOverflowError', {
+            tokenSymbol: tokenMetadata.symbol,
+            decimalPlaces: tokenMetadata.decimals
+          })
+      );
+    });
 
     const shapeMap: Array<[string, NumberAsStringSchema]> = inputAmountSchemas.map((item, index) => {
       const fixedItem = isBalancedProportion ? item.required('Value is required') : item;
@@ -24,5 +43,5 @@ export const useAddLiqFormValidation = (userBalance: Array<Nullable<BigNumber>>,
     const shape: Record<string, NumberAsStringSchema> = Object.fromEntries(shapeMap);
 
     return yup.object().shape(shape);
-  }, [isBalancedProportion, userBalance]);
+  }, [isBalancedProportion, userBalance, stableswapItem, t]);
 };
