4853dd854049fb2d52e06f75d361414682a3b5b9
Artem <violence044@gmail.com>
2022-07-04 14:46:00 +0300

    QUIPU-112 Update coins icons

--- src/modules/coinflip/components/coinflip-game-select/coinflip-game-select.tsx
+++ src/modules/coinflip/components/coinflip-game-select/coinflip-game-select.tsx
@@ -1,28 +1,46 @@
 import { FC } from 'react';
 
 import { Button } from '@shared/components';
-import { CoinSelectedIcon } from '@shared/svg';
+import { CoinSelectedLightIcon, CoinSelectedDarkIcon } from '@shared/svg';
 
 import styles from './coinflip-game-select.module.scss';
 import { CoinflipGameSelectProps } from './coinflip-game-select.props';
 import { useCoinflipGameSelectViewModel } from './use-coinflip-game-select.vm';
 
-const COIN_SIZE = 88;
+const COIN_SIZE = 142;
 
 export const CoinflipGameSelect: FC<CoinflipGameSelectProps> = props => {
-  const { error, iSelectedSideA, iSelectedSideB, handleSelectSideA, handleSelectSideB, CoinSideAIcon, CoinSideBIcon } =
-    useCoinflipGameSelectViewModel(props);
+  const {
+    error,
+    iSelectedSideA,
+    iSelectedSideB,
+    handleSelectSideA,
+    handleSelectSideB,
+    CoinSideAIcon,
+    CoinSideBIcon,
+    isDarkTheme
+  } = useCoinflipGameSelectViewModel(props);
 
   return (
     <div className={styles.root}>
       <div className={styles.buttons}>
         <Button onClick={handleSelectSideA} theme="clean" className={styles.button}>
-          {iSelectedSideA && <CoinSelectedIcon size={COIN_SIZE} className={styles.selected} />}
+          {iSelectedSideA &&
+            (isDarkTheme ? (
+              <CoinSelectedDarkIcon size={COIN_SIZE} className={styles.selected} />
+            ) : (
+              <CoinSelectedLightIcon size={COIN_SIZE} className={styles.selected} />
+            ))}
           <CoinSideAIcon size={COIN_SIZE} className={styles.icon} />
         </Button>
 
         <Button onClick={handleSelectSideB} theme="clean" className={styles.button}>
-          {iSelectedSideB && <CoinSelectedIcon size={COIN_SIZE} className={styles.selected} />}
+          {iSelectedSideB &&
+            (isDarkTheme ? (
+              <CoinSelectedDarkIcon size={COIN_SIZE} className={styles.selected} />
+            ) : (
+              <CoinSelectedLightIcon size={COIN_SIZE} className={styles.selected} />
+            ))}
           <CoinSideBIcon size={COIN_SIZE} className={styles.icon} />
         </Button>
       </div>
