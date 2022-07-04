6ec43b3848644d0a814109568228caee42f81d27
Artem <violence044@gmail.com>
2022-07-04 12:24:22 +0300

    QUIPU-227 Use new component on stabledividends-list [with merged QUIPU-225]

--- src/modules/stableswap/stabledividends/pages/list/use-stabledividends-list.page.vm.ts
+++ src/modules/stableswap/stabledividends/pages/list/use-stabledividends-list.page.vm.ts
@@ -2,7 +2,15 @@ import { useEffect } from 'react';
 
 import BigNumber from 'bignumber.js';
 
+import { AppRootRoutes } from '@app.router';
+import { DOLLAR, PERCENT } from '@config/constants';
+import { extractTokens } from '@modules/stableswap/helpers';
+import { StableswapRoutes } from '@modules/stableswap/stableswap-routes.enum';
+import { StableDividendsFormTabs } from '@modules/stableswap/types';
 import { useReady } from '@providers/use-dapp';
+import { isNull } from '@shared/helpers';
+import { useAuthStore } from '@shared/hooks';
+import { ActiveStatus } from '@shared/types';
 import { useTranslation } from '@translation';
 
 import { useGetStableDividendsList, useStableDividendsListStore } from '../../../hooks';
@@ -15,6 +23,7 @@ export const useStableDividendsListPageViewModel = () => {
   const { getStableDividendsList } = useGetStableDividendsList();
   const { t } = useTranslation();
   const title = t('stableswap|stableDividendsTitle');
+  const { accountPkh } = useAuthStore();
 
   useEffect(() => {
     if (isReady) {
@@ -25,16 +34,88 @@ export const useStableDividendsListPageViewModel = () => {
   const { listStore, filteredList } = stableDividendsListStore;
   const { isLoading } = listStore;
 
-  const data = filteredList?.map(item => {
-    if (item.yourDeposit && item.yourEarned) {
+  const data = filteredList?.map(
+    ({
+      id,
+      stableDividendsItemUrl,
+      tokensInfo,
+      tvl,
+      apr,
+      apy,
+      stakedToken,
+      stakedTokenExchangeRate,
+      yourDeposit,
+      yourEarned
+    }) => {
+      const shouldShowUserStats =
+        !isNull(accountPkh) && (yourDeposit?.gt(DEFAULT_VALUE) || yourEarned?.gt(DEFAULT_VALUE));
+
+      const link = `${AppRootRoutes.Stableswap}${StableswapRoutes.dividends}/${StableDividendsFormTabs.stake}/${stableDividendsItemUrl}`;
+      const status = { status: ActiveStatus.ACTIVE, label: t('common|whiteListed'), filled: true };
+      const extractedTokens = extractTokens(tokensInfo);
+
+      const stableDividendsItemDTI = `stable-dividends-item-${id}`;
+
+      const itemStats = [
+        {
+          cellName: t('stableswap|tvl'),
+          amounts: {
+            amount: tvl,
+            dollarEquivalent: tvl,
+            currency: DOLLAR,
+            dollarEquivalentOnly: true
+          }
+        },
+        {
+          cellName: t('stableswap|apr'),
+          amounts: {
+            amount: apr,
+            currency: PERCENT,
+            amountDecimals: 2
+          }
+        },
+        {
+          cellName: t('stableswap|apy'),
+          amounts: {
+            amount: apy,
+            currency: PERCENT,
+            amountDecimals: 2
+          }
+        }
+      ];
+
+      const userStats = shouldShowUserStats
+        ? [
+            {
+              cellName: t('stableswap|yourDeposit'),
+              amounts: {
+                amount: yourDeposit,
+                dollarEquivalent: yourDeposit.multipliedBy(stakedTokenExchangeRate),
+                dollarEquivalentOnly: true
+              }
+            },
+            {
+              cellName: t('stableswap|yourEarned'),
+              amounts: {
+                amount: yourEarned,
+                dollarEquivalent: yourEarned.multipliedBy(stakedTokenExchangeRate),
+                dollarEquivalentOnly: true
+              }
+            }
+          ]
+        : undefined;
+
       return {
-        ...item,
-        shouldShowStakerInfo: item.yourDeposit.gt(DEFAULT_VALUE) || item.yourEarned.gt(DEFAULT_VALUE)
+        href: link,
+        status: status,
+        inputToken: stakedToken,
+        outputToken: extractedTokens,
+        itemStats,
+        userStats,
+        stableDividendsItemDTI
       };
     }
-
-    return { ...item, yourDeposit: DEFAULT_VALUE, yourEarned: DEFAULT_VALUE };
-  });
+  );
 
   return { title, isLoading, data: data ?? [] };
 };
