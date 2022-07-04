d0d74f63a7431bb18c5afbcaa5862306dfb0f2b5
Dmytro <herkodmytro@gmail.com>
2022-07-04 15:36:17 +0300

    QUIPU-104 Finished displaying common games info

--- src/modules/coinflip/coinflip-page.vm.ts
+++ src/modules/coinflip/coinflip-page.vm.ts
@@ -4,7 +4,7 @@ import { useRootStore } from '@providers/root-store-provider';
 import { useReady } from '@providers/use-dapp';
 import { useAuthStore } from '@shared/hooks';
 
-import { useCoinflipStore, useGamesUserInfo } from './hooks';
+import { useCoinflipGeneralStats, useCoinflipStore, useGamesUserInfo } from './hooks';
 
 export const useCoinflipPageViewModel = () => {
   const [isInitialized, setIsInitialized] = useState(false);
@@ -15,14 +15,19 @@ export const useCoinflipPageViewModel = () => {
   const { accountPkh } = useAuthStore();
   const prevAccountPkhRef = useRef<Nullable<string>>(accountPkh);
   const { getGamesUserInfo } = useGamesUserInfo();
+  const { getCoinflipGeneralStats } = useCoinflipGeneralStats();
+
+
 
   useEffect(() => {
+    const get
     if (!dAppReady && prevAccountPkhRef.current === accountPkh) {
       return;
     }
     void getGamesUserInfo(accountPkh);
+    void getCoinflipGeneralStats();
     prevAccountPkhRef.current = accountPkh;
-  }, [getGamesUserInfo, dAppReady, accountPkh, token]);
+  }, [getGamesUserInfo, getCoinflipGeneralStats, dAppReady, accountPkh, token]);
 
   useEffect(() => {
     (async () => {
