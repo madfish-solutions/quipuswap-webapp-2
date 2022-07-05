be8b759608bf68fbc85c04ad0c631002e744ca9d
Artem <violence044@gmail.com>
2022-07-05 14:19:32 +0300

    QUIPU-112 fix icons display, breakpoints

--- src/shared/svg/coin/coin-selected-light.tsx
+++ src/shared/svg/coin/coin-selected-light.tsx
@@ -4,19 +4,11 @@ import { IconProps } from '@shared/types';
 
 import { useSvgHelper } from '../../hooks';
 
-export const CoinSelectedLightIcon: FC<IconProps> = ({ size, className, ...props }) => {
+export const CoinSelectedLightIcon: FC<IconProps> = ({ className, ...props }) => {
   const { getId, getUrl } = useSvgHelper('CoinSelectedLight');
 
   return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 135 135"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      {...props}
-    >
+    <svg viewBox="0 0 135 135" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} {...props}>
       <rect x={2} y={2} width={128} height={128} rx={66} stroke={getUrl('a')} strokeWidth={4} />
       <defs>
         <linearGradient id={getId('a')} x1={0} y1={0} x2={158.828} y2={22.335} gradientUnits="userSpaceOnUse">
