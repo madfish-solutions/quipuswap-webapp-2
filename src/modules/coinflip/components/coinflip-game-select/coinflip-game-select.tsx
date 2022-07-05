5445ea6b77b700ea9b40bf821759141dff11afaf
Artem <violence044@gmail.com>
2022-07-05 14:19:32 +0300

    QUIPU-112 fix icons display, breakpoints

--- src/modules/coinflip/components/coinflip-game-select/coinflip-game-select.tsx
+++ src/modules/coinflip/components/coinflip-game-select/coinflip-game-select.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react';
 
+import cx from 'classnames';
+
 import { Button } from '@shared/components';
 
 import { CoinSelectedIcon } from '../coin-selected-icon';
 import styles from './coinflip-game-select.module.scss';
 import { CoinflipGameSelectProps } from './coinflip-game-select.props';
 import { useCoinflipGameSelectViewModel } from './use-coinflip-game-select.vm';
-
-const COIN_SIZE = 128;
 
 export const CoinflipGameSelect: FC<CoinflipGameSelectProps> = props => {
   const { error, iSelectedSideA, iSelectedSideB, handleSelectSideA, handleSelectSideB, CoinSideAIcon, CoinSideBIcon } =
@@ -17,13 +17,13 @@ export const CoinflipGameSelect: FC<CoinflipGameSelectProps> = props => {
     <div className={styles.root}>
       <div className={styles.buttons}>
         <Button onClick={handleSelectSideA} theme="clean" className={styles.button}>
-          {iSelectedSideA && <CoinSelectedIcon size={COIN_SIZE} className={styles.selected} />}
-          <CoinSideAIcon size={COIN_SIZE} className={styles.icon} />
+          {iSelectedSideA && <CoinSelectedIcon className={cx(styles.selected, styles.iconSize)} />}
+          <CoinSideAIcon className={styles.icon} />
         </Button>
 
         <Button onClick={handleSelectSideB} theme="clean" className={styles.button}>
-          {iSelectedSideB && <CoinSelectedIcon size={COIN_SIZE} className={styles.selected} />}
-          <CoinSideBIcon size={COIN_SIZE} className={styles.icon} />
+          {iSelectedSideB && <CoinSelectedIcon className={styles.selected} />}
+          <CoinSideBIcon className={styles.icon} />
         </Button>
       </div>
       {error ? <div className={styles.error}>{error}</div> : null}
