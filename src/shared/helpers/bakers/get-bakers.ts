93c6eb38f5275f72ed96578a3eeac9e60a1f8977
Digberi <orkepuma@gmail.com>
2022-07-06 16:48:25 +0300

    QUIPU-237 change Ithacanet to ghostnet

--- src/shared/helpers/bakers/get-bakers.ts
+++ src/shared/helpers/bakers/get-bakers.ts
@@ -1,12 +1,12 @@
 import { NetworkType } from '@airgap/beacon-sdk';
 
-import { ITHACANET_BAKERS } from '@config/bakers';
+import { GHOSTNET_BAKERS } from '@config/bakers';
 import { BAKERS_API } from '@config/config';
 import { NETWORK_ID } from '@config/enviroment';
 
 export const getBakers = async () => {
-  if (NETWORK_ID === NetworkType.ITHACANET) {
-    return ITHACANET_BAKERS;
+  if (NETWORK_ID === NetworkType.GHOSTNET) {
+    return GHOSTNET_BAKERS;
   }
 
   return fetch(BAKERS_API).then(async res => await res.json());
