50de1c98f691f1746f95a37909e0aa6db60e5a87
Digberi <orkepuma@gmail.com>
2022-07-08 18:15:09 +0300

    QUIPU-241 clean up home page

--- src/modules/home/home.page.tsx
+++ src/modules/home/home.page.tsx
@@ -1,34 +1,17 @@
-/* eslint-disable no-console */
 import { FC } from 'react';
 
-import { Button } from '@shared/components';
 import { TestnetAlert } from '@shared/components/testnet-alert';
-import { TokensModal, useChooseTokens } from '@shared/modals/tokens-modal';
 
 import { DexDashboard, News, Opportunities } from './components';
 
-export const HomePage: FC = () => {
-  const { chooseTokens } = useChooseTokens();
-
-  const handleButtonClick = async () => {
-    console.log('handleButtonClick');
-    const tokens = await chooseTokens({});
-    console.log('After chooseTokens');
-    // eslint-disable-next-line no-console
-    console.log(tokens);
-  };
-
-  return (
-    <>
-      <TokensModal />
-      <Button onClick={handleButtonClick}>Click</Button>
-      <TestnetAlert />
-      <News />
-      <DexDashboard />
-      <Opportunities />
-      {/* {IS_NETWORK_MAINNET && <TopPairs />} */}
-      {/* TODO: Implement it. */}
-      {/* <TopFarmings /> */}
-    </>
-  );
-};
+export const HomePage: FC = () => (
+  <>
+    <TestnetAlert />
+    <News />
+    <DexDashboard />
+    <Opportunities />
+    {/* {IS_NETWORK_MAINNET && <TopPairs />} */}
+    {/* TODO: Implement it. */}
+    {/* <TopFarmings /> */}
+  </>
+);
