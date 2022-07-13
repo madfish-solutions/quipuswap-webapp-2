dacd73ad690fd2a7b653385c4fcfc37a21d4f29c
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2022-07-13 09:25:12 +0300

    QUIPU-234 Stable version of cypress

--- cypress/integration/Liquidity/RemoveExist.spec.js
+++ cypress/integration/Liquidity/RemoveExist.spec.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Form exists', () => {
-  it('Should_DisplayExchangeDetails_When_OpenedRLiquidityPage', () => {
+  beforeEach(() => {
     // Go to the remove page from home page
     cy.visit('/');
     cy.get('[data-test-id="header"] [data-test-id="menuButton"]').click();
     cy.get('[data-test-id="menu"] [data-test-id="navigationButton-Liquidity"]').click();
     cy.get('[data-test-id="liquidityPageTokenSelect"] [data-test-id="cardTab-1"]').click();
-    // Check if Title 'Pool Details' ok
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+  });
+  it('Should_DisplayExchangeDetails_When_OpenedRLiquidityPage', () => {    
     cy.get('[data-test-id="poolDetails"] [data-test-id="headerContent"]').should('contain', 'Pool Details');
   });
   it('Should_DisplayPageTitle_When_OpenedRLiquidityPage', () => {
--- cypress/integration/Swap/PriceImpact.spec.js
+++ cypress/integration/Swap/PriceImpact.spec.js
@@ -1,17 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Check color of the smile in the Exchange Details section', () => {
-    it('Should_DisplayRedTextInPriceImpact_When_TooBigAmount', () => {
-        // Go to the swap page 
+    beforeEach(() => {
         cy.visit('/swap');
         cy.get('[data-test-id="acceptCookieButton"]').click();
+    });
+    it('Should_DisplayRedTextInPriceImpact_When_TooBigAmount', () => {
         cy.get('[for="swap-send-from"]').click().type('9999');
         cy.get('[data-test-id="priceImpact"] span span')
             .should('have.css', 'color', 'rgb(234, 36, 36)');
     });
     it('Should_DisplayNotification_When_TooBigAmount', () => {
-        cy.visit('/swap');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
         cy.get('[for="swap-send-from"]').click().type('567777');
         cy.get('[data-test-id="error"]')
             .invoke('text')
@@ -20,15 +19,11 @@ describe('Check color of the smile in the Exchange Details section', () => {
             });
     });
     it('Should_DisplayYellowTextInPriceImpact_When_AmountNotOK', () => {
-        cy.visit('/swap');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
         cy.get('[for="swap-send-from"]').click().type('888');
         cy.get('[data-test-id="priceImpact"] span span')
             .should('have.css', 'color', 'rgb(249, 166, 5)');
     });
     it('Should_DisplayGreenTextInPriceImpact_When_AmountIs5', () => {
-        cy.visit('/swap');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
         cy.get('[data-test-id="from"] [data-test-id="input"]').click().type('5');
         cy.get('[data-test-id="priceImpact"] span span')
             .should('have.css', 'color', 'rgb(46, 211, 62)');
