2f0b5db6ed1fdac634ff64ded5e978f9868d0618
Digberi <orkepuma@gmail.com>
2022-07-18 17:48:08 +0300

    QUIPU-193 add trade link for farming with single token

--- src/modules/farming/pages/item/components/farming-form-invest-link/farming-form-invest-link.vm.ts
+++ src/modules/farming/pages/item/components/farming-form-invest-link/farming-form-invest-link.vm.ts
@@ -1,22 +1,30 @@
 import { useMemo } from 'react';
 
 import { AppRootRoutes } from '@app.router';
+import { TEZOS_TOKEN } from '@config/tokens';
 import { useFarmingItemStore } from '@modules/farming/hooks';
 import { LiquidityTabs } from '@modules/liquidity/liquidity-cards/liquidity-tabs';
 import { StableswapRoutes } from '@modules/stableswap';
 import { StableswapLiquidityFormTabs } from '@modules/stableswap/types';
-import { getLiquidityUrl, isNull } from '@shared/helpers';
+import { getFirstElement, getLiquidityUrl, getTokenPairSlug, isNull, isSingleElement } from '@shared/helpers';
 
 export const useFarmingFormInvestLinkViewModel = () => {
   const { itemStore } = useFarmingItemStore();
   const { data: farmingItem } = itemStore;
 
-  const { investHref } = useMemo(() => {
+  const { investHref, tradeHref } = useMemo(() => {
     if (isNull(farmingItem)) {
       return {
-        investHref: null
+        investHref: null,
+        tradeHref: null
       };
     }
+    if (isSingleElement(farmingItem.tokens)) {
+      return {
+        tradeHref: `${AppRootRoutes.Swap}/${getTokenPairSlug(TEZOS_TOKEN, getFirstElement(farmingItem.tokens))}`
+      };
+    }
+
     if (farmingItem.stableswapItemId) {
       return {
         investHref: `${AppRootRoutes.Stableswap}${StableswapRoutes.liquidity}/${StableswapLiquidityFormTabs.add}/${farmingItem.stableswapItemId}`
@@ -29,5 +37,5 @@ export const useFarmingFormInvestLinkViewModel = () => {
     };
   }, [farmingItem]);
 
-  return { investHref };
+  return { investHref, tradeHref };
 };
