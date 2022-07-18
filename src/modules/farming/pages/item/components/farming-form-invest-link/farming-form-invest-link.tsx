47b4491e4ed7e858fc097a51dbb0df7c0335eb9b
Digberi <orkepuma@gmail.com>
2022-07-18 17:48:08 +0300

    QUIPU-193 add trade link for farming with single token

--- src/modules/farming/pages/item/components/farming-form-invest-link/farming-form-invest-link.tsx
+++ src/modules/farming/pages/item/components/farming-form-invest-link/farming-form-invest-link.tsx
@@ -1,24 +1,31 @@
 import { observer } from 'mobx-react-lite';
 
 import { Button } from '@shared/components';
-import { isNull } from '@shared/helpers';
+import { isExist, isNull } from '@shared/helpers';
 import styles from '@styles/CommonContainer.module.scss';
 import { i18n } from '@translation';
 
 import { useFarmingFormInvestLinkViewModel } from './farming-form-invest-link.vm';
 
 export const FarmingFormInvestLink = observer(() => {
-  const { investHref } = useFarmingFormInvestLinkViewModel();
+  const { investHref, tradeHref } = useFarmingFormInvestLinkViewModel();
 
-  if (isNull(investHref)) {
+  if (isNull(investHref) && isNull(tradeHref)) {
     return null;
   }
 
   return (
     <div className={styles.suggestedOperationsButtons}>
-      <Button theme="underlined" href={investHref} data-test-id="investButton">
-        {i18n.t('common|Invest')}
-      </Button>
+      {isExist(tradeHref) && (
+        <Button theme="underlined" href={tradeHref} data-test-id="tradeButton">
+          {i18n.t('common|Trade')}
+        </Button>
+      )}
+      {isExist(investHref) && (
+        <Button theme="underlined" href={investHref} data-test-id="investButton">
+          {i18n.t('common|Invest')}
+        </Button>
+      )}
     </div>
   );
 });
