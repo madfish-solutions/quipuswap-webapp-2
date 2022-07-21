c9fde4abe9cd51de0b86cbf46da82d8f2fbe1311
Artem <violence044@gmail.com>
2022-07-21 11:07:17 +0300

    QUIPU-223 fix

--- src/modules/coinflip/components/coinflip-token-selector/coinflip-token-selector.vm.ts
+++ src/modules/coinflip/components/coinflip-token-selector/coinflip-token-selector.vm.ts
@@ -1,17 +1,20 @@
-import { amplitudeService } from '@shared/services';
+import { useAmplitudeService } from '@shared/hooks';
 
 import { useCoinflipStore } from '../../hooks';
 import { TokenToPlay } from '../../stores';
 
 export const useTokenSelectorViewModel = () => {
   const coinflipStore = useCoinflipStore();
+
+  const { log } = useAmplitudeService();
+
   const { tokenToPlay, token } = coinflipStore;
 
   const handleSelectToken = (_tokenToPlay: string) => {
     coinflipStore.setToken(_tokenToPlay as TokenToPlay);
 
     if (tokenToPlay !== _tokenToPlay) {
-      amplitudeService.logEvent('CLICK_FLIP_TOKEN_SELECT', {
+      log('CLICK_FLIP_TOKEN_SELECT', {
         asset: _tokenToPlay
       });
     }
--- src/modules/coinflip/hooks/use-last-game-result-amplitude.ts
+++ src/modules/coinflip/hooks/use-last-game-result-amplitude.ts
@@ -4,16 +4,18 @@ import { IS_NETWORK_MAINNET } from '@config/config';
 import { TESTNET_EXCHANGE_RATE, TOKEN_DECIMALS_PRECISION } from '@config/constants';
 import { useNewExchangeRates } from '@providers/use-new-exchange-rate';
 import { getTokenSlug } from '@shared/helpers';
-import { amplitudeService } from '@shared/services';
+import { useAmplitudeService } from '@shared/hooks';
 
 import { getGameResult } from '../helpers';
 import { useCoinflipStore } from './stores';
 
 export const useLastGameResultAmplitude = () => {
   const coinflipStore = useCoinflipStore();
-  const exchangeRates = useNewExchangeRates();
-  const tokenSlug = getTokenSlug(coinflipStore?.token ?? '');
 
+  const exchangeRates = useNewExchangeRates();
+  const { log } = useAmplitudeService();
+
+  const tokenSlug = getTokenSlug(coinflipStore?.token ?? '');
   const gameResult = getGameResult(coinflipStore?.userLastGame?.status);
 
   const lastGameLogEvent = useCallback(() => {
@@ -28,8 +30,8 @@ export const useLastGameResultAmplitude = () => {
       result: gameResult
     };
 
-    amplitudeService.logEvent('LAST_GAME_RESULT', logData);
-  }, [coinflipStore?.tokenToPlay, coinflipStore?.userLastGame?.bidSize, exchangeRates, gameResult, tokenSlug]);
+    log('LAST_GAME_RESULT', logData);
+  }, [coinflipStore?.tokenToPlay, coinflipStore?.userLastGame?.bidSize, exchangeRates, gameResult, log, tokenSlug]);
 
   return { lastGameLogEvent };
 };
