098d25aab70d597a8634fa22d00abbf6a856e0df
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2022-08-01 14:42:45 +0300

    QUIPU-329 add selectors

--- cypress/integration/Liquidity/FieldsCalculation.spec.js
+++ cypress/integration/Liquidity/FieldsCalculation.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+import { notDeepEqual } from 'assert';
 import { DEFAULT_WAIT_TIMEOUT } from '../../const';
 
 describe('Input fields are recalculating after entering in another field data', () => {
@@ -8,14 +9,23 @@ describe('Input fields are recalculating after entering in another field data', 
     cy.get('[data-test-id="header"] [data-test-id="menuButton"]').click();
     cy.get('[data-test-id="menu"] [data-test-id="navigationButton-Liquidity"]').click();
   });
-  it('Should_DisplayDataInSecondInput_When_FirstInputIs1', () => {
-    cy.get('[data-test-id="addLiquidityTokenA"] [data-test-id="addLiquidityTokenA"]').click().type('1');
-    cy.wait(DEFAULT_WAIT_TIMEOUT);
-    cy.get('[data-test-id="addLiquidityTokenB"] [data-test-id="addLiquidityTokenB"]')
+  it.only('Should_DisplayDataInSecondInput_When_FirstInputIs1', () => {
+    // cy.get('[data-test-id="addLiquidityTokenA"] [data-test-id="addLiquidityTokenA"]').click().type('1');
+    // cy.get('[data-test-id="addLiquidityTokenB"] [data-test-id="addLiquidityTokenB"]')
+    //   .invoke('val')
+    //   .then(parseFloat)
+    //   .should('be.gte', 0)
+    // cy.reload();
+
+    // cy.waitUntil(() => cy.get('[data-test-id="addLiquidityTokenB"] [data-test-id="addLiquidityTokenB"]')
+    // .then(value => value != NaN));
+    cy.get('[data-test-id="addLiquidityTokenA"] [data-test-id="addLiquidityTokenA"]').click().type('1')
+  
+      cy.waitUntil(() => cy.get('[data-test-id="addLiquidityTokenB"] [data-test-id="addLiquidityTokenB"]')
       .invoke('val')
       .then(parseFloat)
-      .should('be.gte', 0);
-    cy.reload();
+      .should('be.gte', 0))
+
   });
   it('Should_DisplayDataInFirstInput_When_SecondInputIs1', () => {
     cy.get('[data-test-id="addLiquidityTokenB"] [data-test-id="addLiquidityTokenB"]').click().type('1');
--- cypress/integration/Swap/Validation.spec.js
+++ cypress/integration/Swap/Validation.spec.js
@@ -9,10 +9,12 @@ describe('Check input fields', () => {
     });
     it('Should_CalculateToField_When_FromFieldIsFilled', () => {
         cy.get('[for="swap-send-from"]').click().type('1');
-        cy.wait(FIELD_WAIT_TIMEOUT);
+        cy.waitUntil(() =>
         cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="to"] [data-test-id="input"]')
-            .invoke('val').then(parseFloat).should('be.gte', 0);
+            .invoke('val').then(parseFloat).should('be.gte', 0))
     });
+
+
     it.skip('Should_CalculateFromField_When_ToFieldIsFilled', () => {
         cy.get('[data-test-id="to"]').click().type('1');
         cy.get('[data-test-id="swapPageTokenSelect"] [data-test-id="from"] [data-test-id="input"]')
