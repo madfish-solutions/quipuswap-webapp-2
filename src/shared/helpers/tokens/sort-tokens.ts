6005a5c25f07e2d790b2ee41e61e5e30b63edd13
Artem Koziar <koziar.artem@gmail.com>
2022-08-02 08:33:25 +0300

    QUIPU-330 Fix sortTokens

--- src/shared/helpers/tokens/sort-tokens.ts
+++ src/shared/helpers/tokens/sort-tokens.ts
@@ -1,15 +1,18 @@
 import { SKIP, SWAP } from '@config/constants';
 import { Token } from '@shared/types';
 
-import { getTokenStandard } from './get-token-standard';
+import { getTokenSlug } from './token-slug';
 import { isTokenFa12, isTokenFa2 } from './token-type';
 
-const sortAddresses = (address1: string, address2: string) => (address1 > address2 ? SKIP : SWAP);
+const sortAddresses = (address1: string, address2: string) => (address1 > address2 ? SWAP : SKIP);
 
 export const sortTokens = (tokenA: Token, tokenB: Token) => {
-  if ((isTokenFa12(tokenA) && isTokenFa12(tokenB)) || (isTokenFa2(tokenA) && isTokenFa2(tokenB))) {
-    return sortAddresses(getTokenStandard(tokenA), getTokenStandard(tokenB));
+  const isFa12Tokens = isTokenFa12(tokenA) && isTokenFa12(tokenB);
+  const isFa2Tokens = isTokenFa2(tokenA) && isTokenFa2(tokenB);
+  const isTheSameStandard = isFa12Tokens || isFa2Tokens;
+  if (isTheSameStandard) {
+    return sortAddresses(getTokenSlug(tokenA), getTokenSlug(tokenB));
   }
 
-  return isTokenFa12(tokenA) ? SWAP : SKIP;
+  return isTokenFa12(tokenA) ? SKIP : SWAP;
 };
