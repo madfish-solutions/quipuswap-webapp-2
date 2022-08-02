7167614e2aa0a6e5ebc05175fbbbd807dd83bc2e
Dmytro <herkodmytro@gmail.com>
2022-08-02 11:39:30 +0300

    QUIPU-306 Fixed import/export

--- src/shared/model-builder/mapper/mapper.factory.ts
+++ src/shared/model-builder/mapper/mapper.factory.ts
@@ -1,4 +1,7 @@
-import { bigNumberMapper, booleanMapper, numberMapper, stringMapper } from '../../mapping';
+import { bigNumberMapper } from '../../mapping/big-number.map';
+import { booleanMapper } from '../../mapping/boolean.map';
+import { numberMapper } from '../../mapping/number.map';
+import { stringMapper } from '../../mapping/string.map';
 import { MapperKinds } from './mapper-kinds.enum';
 
 export const mapperFactory: Record<MapperKinds, (arg: unknown, optional: boolean, nullable: boolean) => unknown> = {
