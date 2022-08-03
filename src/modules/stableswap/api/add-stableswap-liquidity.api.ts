7bd95e0a52d1dbbc058ecb4f5ee769d5adcc40d1
Digberi <orkepuma@gmail.com>
2022-08-03 20:34:26 +0300

    QUIPU-344 fix adding imbalanced liquidity into stableswap pool

--- src/modules/stableswap/api/add-stableswap-liquidity.api.ts
+++ src/modules/stableswap/api/add-stableswap-liquidity.api.ts
@@ -4,6 +4,7 @@ import { BigNumber } from 'bignumber.js';
 import { withApproveApiForManyTokens } from '@blockchain';
 import { STABLESWAP_REFERRAL } from '@config/config';
 import { DEFAULT_STABLESWAP_POOL_ID } from '@config/constants';
+import { isGreaterThanZero } from '@shared/helpers';
 import { AmountToken } from '@shared/types';
 
 export const addStableswapLiquidityApi = async (
@@ -19,13 +20,19 @@ export const addStableswapLiquidityApi = async (
   const stableswapPoolContract = await tezos.wallet.at(stableswapPoolContractAddress);
 
   const michelsonAmounts = new MichelsonMap();
-  tokensAndAmounts.forEach(({ amount }, index) => michelsonAmounts.set(index, amount));
+  tokensAndAmounts.forEach(({ amount }, index) => {
+    if (isGreaterThanZero(amount)) {
+      michelsonAmounts.set(index, amount);
+    }
+  });
 
   const swableswapLiquidityParams = stableswapPoolContract.methods
     .invest(DEFAULT_STABLESWAP_POOL_ID, shares, michelsonAmounts, deadline, receiverFixed, STABLESWAP_REFERRAL)
     .toTransferParams();
 
-  return await withApproveApiForManyTokens(tezos, stableswapPoolContractAddress, tokensAndAmounts, accountPkh, [
+  const cleanedTokensAmount = tokensAndAmounts.filter(({ amount }) => isGreaterThanZero(amount));
+
+  return await withApproveApiForManyTokens(tezos, stableswapPoolContractAddress, cleanedTokensAmount, accountPkh, [
     swableswapLiquidityParams
   ]);
 };
