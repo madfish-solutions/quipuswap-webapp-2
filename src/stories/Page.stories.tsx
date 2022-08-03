23f869f3711005ea84b72f8cfb0f1601a9e9b3b6
Artem Koziar <koziar.artem@gmail.com>
2022-08-03 14:04:01 +0300

    QUIPU-308 Add storybook

---
+++ src/stories/Page.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+
+import { Page } from './Page';
+
+// eslint-disable-next-line
+export default {
+  title: 'Example/Page',
+  component: Page,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen'
+  }
+} as ComponentMeta<typeof Page>;
+
+const Template: ComponentStory<typeof Page> = args => <Page {...args} />;
+
+export const LoggedOut = Template.bind({});
+
+export const LoggedIn = Template.bind({});
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+LoggedIn.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const loginButton = canvas.getByRole('button', { name: /Log in/i });
+  userEvent.click(loginButton);
+};
