6b50a4bc86b355306d2ce033be0b2d8342432e5e
Artem Koziar <koziar.artem@gmail.com>
2022-08-04 13:24:08 +0300

    QUIPU-308 Add Typography v2 to storybook

--- src/stories/Typography.tsx
+++ src/stories/Typography.tsx
@@ -12,5 +12,32 @@ export const Typography: FC<Props> = ({ label }) => (
     <h4>{label ? label : 'Typography H4'}</h4>
     <h5>{label ? label : 'Typography H5'}</h5>
     <h6>{label ? label : 'Typography H6'}</h6>
+    <p>Paragraph</p>
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+      <li>List item 4</li>
+    </ul>
+    <ol>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+      <li>List item 4</li>
+    </ol>
+    <p>
+      Lollipop jelly beans liquorice. Ice cream gummies cheesecake. Chocolate cake lemon drops icing. Chocolate cake
+      lemon drops icing.
+    </p>
+    <p>
+      Lollipop jelly <strong>strong</strong> <b>beans</b> liquorice. Ice cream gummies <i>italic</i> cheesecake.
+      Chocolate cake lemon drops icing. Chocolate cake lemon drops icing. Lollipop jelly beans liquorice. Ice cream
+      gummies cheesecake. Chocolate cake lemon drops icing. Chocolate cake lemon drops icing. Lollipop jelly beans
+      liquorice. Ice cream gummies cheesecake. Chocolate cake lemon drops icing. Chocolate cake lemon drops icing.
+    </p>
+    <p>
+      Lollipop jelly beans liquorice. Ice cream gummies cheesecake. Chocolate cake lemon drops icing. Chocolate cake
+      lemon drops icing.
+    </p>
   </div>
 );
