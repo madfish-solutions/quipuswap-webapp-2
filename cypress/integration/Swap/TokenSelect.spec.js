7924a1ab1c8507e10f3b0f3183389bad37dd3d41
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2022-08-05 11:44:41 +0300

    QUIPU-329 add improvements

--- cypress/integration/Swap/TokenSelect.spec.js
+++ cypress/integration/Swap/TokenSelect.spec.js
@@ -4,41 +4,41 @@ import { FIELD_WAIT_TIMEOUT } from '../../const';
 import { MAINNET_QUIPU_TOKEN } from '../../const';
 
 describe('Select new tokens in the fields', () => {
-    beforeEach(() => {
-        cy.visit('/swap/tez-' + MAINNET_QUIPU_TOKEN);
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.get('[data-test-id="from"] [data-test-id="changeToken"]').click();
-        cy.wait(FIELD_WAIT_TIMEOUT);
-        cy.get('[data-test-id="KUSD"]').click();
-        cy.get('[data-test-id="from"] [data-test-id="changeToken"]')
-            .should('contain', 'KUSD');
-        cy.get('[data-test-id="to"] [data-test-id="changeToken"]').click();
-        cy.wait(FIELD_WAIT_TIMEOUT);
-        cy.get('[data-test-id="CRUNCH"]').click();
-    });
-    it('Should_DisplayNewTokens_When_SelectIt', () => {
-        cy.get('[data-test-id="to"] [data-test-id="changeToken"]')
-            .should('contain', 'CRUNCH');
-    });
-    it('Should_DisplayNewTitle_When_NewTokenAreSelected', () => {
-        cy.get('[data-test-id="swapPageTitle"]').should('contain', 'Swap KUSD / CRUNCH');
-    });
-    it('Should_DisplayNewInfoInExchangeDetailsSection_When_NewTokenAreSelected', () => {
-        cy.get('[data-test-id="sellPrice"] [data-test-id="rightVisibleCurrency"]')
-            .should('contain', 'KUSD');
-        cy.get('[data-test-id="sellPrice"] [data-test-id="rightVisibleCurrency"]')
-            .should('contain', 'CRUNCH');
-        cy.get('[data-test-id="buyPrice"] [data-test-id="rightVisibleCurrency"]')
-            .should('contain', 'KUSD');
-        cy.get('[data-test-id="buyPrice"] [data-test-id="rightVisibleCurrency"]')
-            .should('contain', 'CRUNCH');
-    });
-    it('Should_ChangeToken_When_ClickOnSwapArrow', () => {
-        // Check if right token are selected
-        cy.get('[data-test-id="swapButton"]').click();
-        cy.get('[data-test-id="from"] [data-test-id="changeToken"]')
-            .should('contain', 'CRUNCH');
-        cy.get('[data-test-id="to"] [data-test-id="changeToken"]')
-            .should('contain', 'KUSD');
-    });
+  beforeEach(() => {
+    cy.visit('/swap/tez-' + MAINNET_QUIPU_TOKEN);
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+    cy.get('[data-test-id="from"] [data-test-id="changeToken"]').click();
+    cy.wait(FIELD_WAIT_TIMEOUT);
+    cy.get('[data-test-id="KUSD"]').click();
+    cy.get('[data-test-id="from"] [data-test-id="changeToken"]')
+      .should('contain', 'KUSD');
+    cy.get('[data-test-id="to"] [data-test-id="changeToken"]').click();
+    cy.wait(FIELD_WAIT_TIMEOUT);
+    cy.get('[data-test-id="CRUNCH"]').click();
+  });
+  it('Should_DisplayNewTokens_When_SelectIt', () => {
+    cy.get('[data-test-id="to"] [data-test-id="changeToken"]')
+      .should('contain', 'CRUNCH');
+  });
+  it('Should_DisplayNewTitle_When_NewTokenAreSelected', () => {
+    cy.get('[data-test-id="swapPageTitle"]').should('contain', 'Swap KUSD / CRUNCH');
+  });
+  it('Should_DisplayNewInfoInExchangeDetailsSection_When_NewTokenAreSelected', () => {
+    cy.get('[data-test-id="sellPrice"] [data-test-id="rightVisibleCurrency"]')
+      .should('contain', 'KUSD');
+    cy.get('[data-test-id="sellPrice"] [data-test-id="rightVisibleCurrency"]')
+      .should('contain', 'CRUNCH');
+    cy.get('[data-test-id="buyPrice"] [data-test-id="rightVisibleCurrency"]')
+      .should('contain', 'KUSD');
+    cy.get('[data-test-id="buyPrice"] [data-test-id="rightVisibleCurrency"]')
+      .should('contain', 'CRUNCH');
+  });
+  it('Should_ChangeToken_When_ClickOnSwapArrow', () => {
+    // Check if right token are selected
+    cy.get('[data-test-id="swapButton"]').click();
+    cy.get('[data-test-id="from"] [data-test-id="changeToken"]')
+      .should('contain', 'CRUNCH');
+    cy.get('[data-test-id="to"] [data-test-id="changeToken"]')
+      .should('contain', 'KUSD');
+  });
 });
