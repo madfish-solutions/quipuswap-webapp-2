e8ab5e11cec5d8aca0de3187092ab42fc106d4a7
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2022-08-05 11:44:41 +0300

    QUIPU-329 add improvements

--- cypress/integration/SSL/VisualTesting.spec.js
+++ cypress/integration/SSL/VisualTesting.spec.js
@@ -3,14 +3,14 @@
 import { DEFAULT_WAIT_TIMEOUT } from '../../const';
 
 describe('Visual testing of the SSL form', () => {
-    it('Should_DisplayCorrectSSLForm_When_OpenedSSLPage', () => {
-        cy.visit('/stableswap/liquidity');
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-        cy.wait(DEFAULT_WAIT_TIMEOUT);
-        cy.percySnapshot('SSLPagelayout', {
-            percyCSS: `
+  it('Should_DisplayCorrectSSLForm_When_OpenedSSLPage', () => {
+    cy.visit('/stableswap/liquidity');
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+    cy.wait(DEFAULT_WAIT_TIMEOUT);
+    cy.percySnapshot('SSLPagelayout', {
+      percyCSS: `
      [data-test-id="SSLDashboardStatsInfo"], [data-test-id="stableliquidityList"] { display: none; };
      `
-        })
-    });
+    })
+  });
 });
--- cypress/integration/Swap/PriceImpact.spec.js
+++ cypress/integration/Swap/PriceImpact.spec.js
@@ -3,31 +3,31 @@
 import { MAINNET_QUIPU_TOKEN } from '../../const';
 
 describe('Check color of the smile in the Exchange Details section', () => {
-    beforeEach(() => {
-        cy.visit('/swap/tez-' + MAINNET_QUIPU_TOKEN);
-        cy.get('[data-test-id="acceptCookieButton"]').click();
-    });
-    it('Should_DisplayRedTextInPriceImpact_When_TooBigAmount', () => {
-        cy.get('[for="swap-send-from"]').click().type('9999');
-        cy.get('[data-test-id="priceImpact"] span span')
-            .should('have.css', 'color', 'rgb(234, 36, 36)');
-    });
-    it('Should_DisplayNotification_When_TooBigAmount', () => {
-        cy.get('[for="swap-send-from"]').click().type('567777');
-        cy.get('[data-test-id="error"]')
-            .invoke('text')
-            .then(text => {
-                expect(text).to.contain('Value has to be a number between')
-            });
-    });
-    it('Should_DisplayYellowTextInPriceImpact_When_AmountNotOK', () => {
-        cy.get('[for="swap-send-from"]').click().type('555');
-        cy.get('[data-test-id="priceImpact"] span span')
-            .should('have.css', 'color', 'rgb(249, 166, 5)');
-    });
-    it('Should_DisplayGreenTextInPriceImpact_When_AmountIs5', () => {
-        cy.get('[data-test-id="from"] [data-test-id="input"]').click().type('5');
-        cy.get('[data-test-id="priceImpact"] span span')
-            .should('have.css', 'color', 'rgb(46, 211, 62)');
-    });
+  beforeEach(() => {
+    cy.visit('/swap/tez-' + MAINNET_QUIPU_TOKEN);
+    cy.get('[data-test-id="acceptCookieButton"]').click();
+  });
+  it('Should_DisplayRedTextInPriceImpact_When_TooBigAmount', () => {
+    cy.get('[for="swap-send-from"]').click().type('9999');
+    cy.get('[data-test-id="priceImpact"] span span')
+      .should('have.css', 'color', 'rgb(234, 36, 36)');
+  });
+  it('Should_DisplayNotification_When_TooBigAmount', () => {
+    cy.get('[for="swap-send-from"]').click().type('567777');
+    cy.get('[data-test-id="error"]')
+      .invoke('text')
+      .then(text => {
+        expect(text).to.contain('Value has to be a number between')
+      });
+  });
+  it('Should_DisplayYellowTextInPriceImpact_When_AmountNotOK', () => {
+    cy.get('[for="swap-send-from"]').click().type('555');
+    cy.get('[data-test-id="priceImpact"] span span')
+      .should('have.css', 'color', 'rgb(249, 166, 5)');
+  });
+  it('Should_DisplayGreenTextInPriceImpact_When_AmountIs5', () => {
+    cy.get('[data-test-id="from"] [data-test-id="input"]').click().type('5');
+    cy.get('[data-test-id="priceImpact"] span span')
+      .should('have.css', 'color', 'rgb(46, 211, 62)');
+  });
 });
