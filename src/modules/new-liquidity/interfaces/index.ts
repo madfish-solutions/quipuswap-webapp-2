231cc9532fe9cebb983c8939a8648a229fb9f045
Artem <violence044@gmail.com>
2022-08-09 11:31:59 +0300

    QUIPU-287 delete unused interface

--- src/modules/new-liquidity/interfaces/index.ts
+++ src/modules/new-liquidity/interfaces/index.ts
@@ -3,4 +3,4 @@ export * from './liquidity-item.interface';
 export * from './liquidity-token-info.interface';
 export * from './new-liquidity-lables.interface';
 export * from './opportunities.interface';
-export * from './hot-pool.interface';
+export * from './pool-type-enum';
