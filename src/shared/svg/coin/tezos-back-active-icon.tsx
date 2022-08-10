467ce0aa9672ee6f7dc1ae72f8a651bc3b6ceb47
Artem Koziar <koziar.artem@gmail.com>
2022-08-10 18:41:08 +0300

    QUIPU-111 Fix Tezos icons

--- src/shared/svg/coin/tezos-back-active-icon.tsx
+++ src/shared/svg/coin/tezos-back-active-icon.tsx
@@ -8,54 +8,108 @@ export const TezosBackActiveIcon: FC<IconProps> = ({ ...props }) => {
   const { getId, getUrl } = useSvgHelper('CoinSideBQuipuIconNew');
 
   return (
-    <svg fill="none" viewBox="0 0 145 145" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <g clipPath={getUrl('a')}>
+    <svg width={160} height={160} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g filter={getUrl('a')}>
         <path
-          d="M71.008 135c35.346 0 64-28.654 64-64 0-35.346-28.654-64-64-64-35.346 0-64 28.654-64 64 0 35.346 28.654 64 64 64Z"
+          d="M80.007 144c35.346 0 64-28.654 64-64 0-35.346-28.654-64-64-64-35.346 0-64 28.654-64 64 0 35.346 28.654 64 64 64Z"
           fill="#FCBF12"
         />
         <path
-          d="M70.582 124.094c29.803 0 53.963-24.16 53.963-53.963S100.385 16.17 70.582 16.17 16.62 40.329 16.62 70.13c0 29.803 24.16 53.963 53.963 53.963Z"
-          fill="#F9A605"
+          d="M79.581 133.094c29.803 0 53.963-24.16 53.963-53.963S109.384 25.17 79.581 25.17 25.618 49.329 25.618 79.13c0 29.803 24.16 53.963 53.963 53.963Z"
+          fill="#DB9605"
         />
+        <g filter={getUrl('b')}>
+          <path d="M43.157 71.86v-5.262H65.81v5.261h-8.127V93.4h-6.385V71.86h-8.14Z" fill="#FF0" />
+        </g>
+        <g filter={getUrl('c')}>
+          <path
+            d="M68.991 93.4V66.6H87.68v5.261H75.47v5.497h11.256v5.273H75.47v5.51h12.21v5.26H68.99V93.4Z"
+            fill="#FF0"
+          />
+        </g>
+        <g filter={getUrl('d')}>
+          <path
+            d="M91.644 93.4v-3.69l13.021-17.85H91.656v-5.262h21.201v3.69L99.836 88.14h13.008v5.26h-21.2Z"
+            fill="#FF0"
+          />
+        </g>
         <path
-          opacity={0.29}
-          d="M22.232 72.165c0-29.802 24.16-53.963 53.963-53.963 5.349 0 10.515.782 15.393 2.23a53.771 53.771 0 0 0-21.114-4.287c-29.802 0-53.963 24.159-53.963 53.963 0 24.453 16.267 45.105 38.57 51.73-19.31-8.218-32.849-27.365-32.849-49.673Z"
-          fill="#0D161C"
-        />
-        <path
-          opacity={0.3}
-          d="M115.113 24.626c10.922 11.48 17.626 27.01 17.626 44.105 0 35.346-28.654 64-64 64-17.095 0-32.625-6.704-44.105-17.626C36.29 127.36 52.757 135 71.008 135c35.346 0 64-28.654 64-64 0-18.251-7.64-34.715-19.895-46.374Z"
-          fill="#000"
-        />
-        <path
-          opacity={0.52}
-          d="M72.218 8.32c18.213 0 34.646 7.607 46.302 19.817C106.808 15.159 89.853 7 70.993 7c-35.346 0-64 28.654-64 64 0 17.134 6.733 32.697 17.699 44.184-10.24-11.347-16.474-26.376-16.474-42.862 0-35.348 28.654-64.002 64-64.002Z"
-          fill="#fff"
-        />
-        <path
-          d="M36.157 62.86v-5.262H58.81v5.261h-8.127V84.4h-6.385V62.86h-8.14ZM61.991 84.4V57.6H80.68v5.261H68.47v5.497h11.256v5.273H68.47v5.51h12.21v5.26H61.99V84.4Zm22.653 0v-3.69l13.02-17.85H84.657v-5.262h21.201v3.69L92.836 79.14h13.008v5.26h-21.2Z"
-          fill="#F9C23A"
-        />
-        <path
-          opacity={0.3}
-          d="m36.157 62.86 1.829 1.828h6.312v-1.829h-8.14Zm22.654 0h-8.128V84.4h-6.385l1.828 1.828h6.386v-21.54h8.128v-5.26L58.81 57.6v5.26Zm21.866 0h-12.21v5.496h1.83v-3.668h12.209v-5.26L80.677 57.6v5.26ZM68.468 73.631v5.51h1.828V75.46h11.255v-5.276l-1.828-1.828v5.275H68.468ZM61.991 84.4l1.829 1.828h18.686V80.97l-1.829-1.828V84.4H61.991Zm22.665-21.54 1.829 1.828h9.845l1.335-1.829H84.656Zm23.03-3.431-1.829-1.829v3.69L92.836 79.14h3.162l11.688-16.021v-3.69ZM84.644 84.4l1.828 1.828h21.201V80.97l-1.829-1.828V84.4h-21.2Z"
-          fill="#000"
-        />
-        <path
-          d="m70.58 25.287-3.454 3.455 3.455 3.455 3.455-3.455-3.455-3.455Zm11.37 7.931-3.455 3.455 3.455 3.455 3.455-3.455-3.455-3.455Zm-22.741 0-3.455 3.455 3.455 3.455 3.455-3.455-3.455-3.455Zm14.828 78.277-3.455-3.455-3.455 3.455 3.455 3.455 3.455-3.455Zm11.37-7.931-3.455-3.455-3.455 3.455 3.455 3.455 3.455-3.455Zm-22.741 0-3.455-3.455-3.455 3.455 3.455 3.454 3.455-3.454Z"
-          fill="#FCBF12"
-        />
-        <path
-          opacity={0.3}
-          d="m74.035 28.744-3.455 3.455 1.098 1.097 3.455-3.455-1.098-1.097Zm11.371 7.929-3.455 3.455 1.098 1.097 3.455-3.455-1.098-1.097Zm-22.741.001-3.455 3.455 1.098 1.097 3.455-3.454-1.098-1.098ZM74.035 111.497l-3.455 3.455 1.098 1.098 3.455-3.455-1.098-1.098Zm11.371-7.931-3.455 3.455 1.098 1.098 3.455-3.455-1.098-1.098Zm-22.741 0-3.455 3.455 1.098 1.098 3.455-3.455-1.098-1.098Z"
-          fill="#000"
+          d="m79.58 34.287-3.454 3.455 3.455 3.455 3.455-3.455-3.455-3.455Zm11.37 7.931-3.455 3.455 3.455 3.455 3.455-3.455-3.455-3.455Zm-22.741 0-3.455 3.455 3.455 3.455 3.455-3.455-3.455-3.455Zm14.828 78.277-3.455-3.455-3.455 3.455 3.455 3.455 3.455-3.455Zm11.37-7.931-3.455-3.455-3.455 3.455 3.455 3.455 3.455-3.455Zm-22.741 0-3.455-3.455-3.455 3.455 3.455 3.454 3.455-3.454Z"
+          stroke="#FF0"
         />
       </g>
       <defs>
-        <clipPath id={getId('a')}>
-          <path fill="#fff" transform="translate(7 7)" d="M0 0h128v128H0z" />
-        </clipPath>
+        <filter
+          id={getId('a')}
+          x={0}
+          y={0}
+          width={160.007}
+          height={160}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset />
+          <feGaussianBlur stdDeviation={8} />
+          <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_10660_413942" />
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_10660_413942" result="shape" />
+        </filter>
+        <filter
+          id={getId('b')}
+          x={39.157}
+          y={62.598}
+          width={30.652}
+          height={34.801}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_10660_413942" />
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_10660_413942" result="shape" />
+        </filter>
+        <filter
+          id={getId('c')}
+          x={64.991}
+          y={62.6}
+          width={26.689}
+          height={34.801}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_10660_413942" />
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_10660_413942" result="shape" />
+        </filter>
+        <filter
+          id={getId('d')}
+          x={87.644}
+          y={62.598}
+          width={29.213}
+          height={34.801}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+          <feOffset />
+          <feGaussianBlur stdDeviation={2} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_10660_413942" />
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow_10660_413942" result="shape" />
+        </filter>
       </defs>
     </svg>
   );
