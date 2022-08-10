e85cfde0a85722e5c621f66553adac0abdc0d14e
Digberi <orkepuma@gmail.com>
2022-08-10 22:42:13 +0300

    QUIPU-359 concept

--- src/modules/stableswap/stableswap-liquidity/pages/item/components/details/details.tsx
+++ src/modules/stableswap/stableswap-liquidity/pages/item/components/details/details.tsx
@@ -1,139 +1,36 @@
 import { FC } from 'react';
 
-import cx from 'classnames';
 import { observer } from 'mobx-react-lite';
 
-import {
-  Button,
-  Card,
-  ContractHashWithCopy,
-  DashPlug,
-  DetailsCardCell,
-  Iterator,
-  StateCurrencyAmount,
-  StatePercentage,
-  StateWrapper
-} from '@shared/components';
-import { ExternalLink } from '@shared/svg';
+import { Card, Tabs } from '@shared/components';
 import commonContainerStyles from '@styles/CommonContainer.module.scss';
-import { useTranslation } from '@translation';
 
-import { TokenLocked } from '../../../../../components/token-locked';
-import styles from './details.module.scss';
+import { DetailsInfo } from '../details-info';
+import { YouTube } from '../youtube';
 import { useDetailsViewModel } from './use-details.vm';
+import { useTabs } from './use-tabs.vm';
 
 export const Details: FC = observer(() => {
-  const { t } = useTranslation();
-  const {
-    contractAddress,
-    poolId,
-    totalLpSupply,
-    providersFee,
-    interfaceFee,
-    stakersFee,
-    devFee,
-    poolContractUrl,
-    cardCellClassName,
-    isLoading,
-    tvlInUsd,
-    tokensLockedData
-  } = useDetailsViewModel();
+  const detailsInfoParams = useDetailsViewModel();
+  const { isDetails, tabsContent, activeId, setTabId } = useTabs();
 
   return (
     <Card
       header={{
-        content: t('common|Pool Details')
+        content: (
+          <Tabs
+            values={tabsContent}
+            activeId={activeId}
+            setActiveId={setTabId}
+            className={commonContainerStyles.tabs}
+          />
+        ),
+        className: commonContainerStyles.header
       }}
       contentClassName={commonContainerStyles.content}
       data-test-id="stableswapDetails"
     >
-      <DetailsCardCell
-        cellName={t('stableswap|poolAddress')}
-        tooltipContent={t('stableswap|pairAddress')}
-        className={cardCellClassName}
-        data-test-id="pairAddress"
-      >
-        <StateWrapper
-          isLoading={isLoading}
-          loaderFallback={<DashPlug />}
-          isError={!contractAddress}
-          errorFallback={<DashPlug animation={false} />}
-        >
-          {contractAddress && <ContractHashWithCopy contractAddress={contractAddress} />}
-        </StateWrapper>
-      </DetailsCardCell>
-      <DetailsCardCell
-        cellName={t('stableswap|poolId')}
-        tooltipContent={t('stableswap|pairId')}
-        className={cardCellClassName}
-        data-test-id="pairId"
-      >
-        <StateCurrencyAmount isLoading={isLoading} amount={poolId} />
-      </DetailsCardCell>
-
-      <DetailsCardCell
-        cellName={t('stableswap|tvl')}
-        tooltipContent={t('stableswap|tvlPoolTooltip')}
-        className={cardCellClassName}
-        data-test-id="tvl"
-      >
-        <StateCurrencyAmount isLoading={isLoading} amount={tvlInUsd} dollarEquivalent={tvlInUsd} dollarEquivalentOnly />
-      </DetailsCardCell>
-
-      <Iterator render={TokenLocked} data={tokensLockedData} />
-
-      <DetailsCardCell
-        cellName={t('stableswap|Total LP Supply')}
-        tooltipContent={t('stableswap|totalLPSupply')}
-        className={cardCellClassName}
-        data-test-id="totalLpSupply"
-      >
-        <StateCurrencyAmount isLoading={isLoading} amount={totalLpSupply} />
-      </DetailsCardCell>
-      <DetailsCardCell
-        cellName={t('stableswap|liquidityProvidersFee')}
-        tooltipContent={t('stableswap|liquidityProvidersFeeFeeDescription')}
-        className={cardCellClassName}
-        data-test-id="liquidityProvidersFee"
-      >
-        <StatePercentage isLoading={isLoading} value={providersFee} />
-      </DetailsCardCell>
-      <DetailsCardCell
-        cellName={t('stableswap|Interface Fee')}
-        tooltipContent={t('stableswap|interfaceFee')}
-        className={cardCellClassName}
-        data-test-id="interfaceFee"
-      >
-        <StatePercentage isLoading={isLoading} value={interfaceFee} />
-      </DetailsCardCell>
-      <DetailsCardCell
-        cellName={t('stableswap|QUIPU Stakers Fee')}
-        tooltipContent={t('stableswap|quipuStakersFee')}
-        className={cardCellClassName}
-        data-test-id="quipuStakersFee"
-      >
-        <StatePercentage isLoading={isLoading} value={stakersFee} />
-      </DetailsCardCell>
-      <DetailsCardCell
-        cellName={t('stableswap|Dev Fee')}
-        tooltipContent={t('stableswap|devFee')}
-        className={cardCellClassName}
-        data-test-id="devFee"
-      >
-        <StatePercentage isLoading={isLoading} value={devFee} />
-      </DetailsCardCell>
-      <div className={cx(commonContainerStyles.detailsButtons, styles.detailsButtons)}>
-        <Button
-          className={cx(commonContainerStyles.detailsButton, styles.detailsButton)}
-          theme="inverse"
-          href={poolContractUrl}
-          external
-          icon={<ExternalLink className={commonContainerStyles.linkIcon} />}
-          data-test-id="stableswapContractButton"
-        >
-          {t('liquidity|Pair Contract')}
-        </Button>
-      </div>
+      {isDetails ? <DetailsInfo {...detailsInfoParams} /> : <YouTube />}
     </Card>
   );
 });
