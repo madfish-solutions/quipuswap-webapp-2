f91b18cb7382848a35bae2ab4fca59808e1aaf53
Digberi <orkepuma@gmail.com>
2022-08-11 14:49:50 +0300

    hot fix coinflip rewards

--- src/modules/coinflip/components/coinflip-reward-info/helpers.ts
+++ src/modules/coinflip/components/coinflip-reward-info/helpers.ts
@@ -7,10 +7,14 @@ import { TokenWon } from '../../types';
 export const getDollarsWin = (tokensWon: Array<TokenWon>, exchangeRate: Record<string, BigNumber>) => {
   const firstValue: Nullable<BigNumber> = new BigNumber('0');
 
-  return tokensWon.reduce((accum: Nullable<BigNumber>, curr: TokenWon) => {
+  return tokensWon.reduce((accum: BigNumber, curr: TokenWon) => {
     const tokenSlug = getTokenSlug(curr.token);
     const tokenExchangeRate = exchangeRate[tokenSlug];
 
-    return isExist(accum) && isExist(tokenExchangeRate) ? accum.plus(tokenExchangeRate) : null;
+    if (isExist(curr.amount) && isExist(tokenExchangeRate)) {
+      return accum.plus(curr.amount?.multipliedBy(tokenExchangeRate));
+    }
+
+    return accum;
   }, firstValue);
 };
