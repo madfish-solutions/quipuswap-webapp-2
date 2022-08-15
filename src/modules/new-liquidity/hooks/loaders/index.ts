9f05e6bc0c652ee6c3c8b3b46480b8d0d0125856
Artem <violence044@gmail.com>
2022-08-15 17:11:49 +0300

    QUIPU-324 Implement empty details page for Dex 2.0

---
+++ src/modules/new-liquidity/hooks/loaders/index.ts
@@ -0,0 +1,2 @@
+export * from './use-get-new-liquidity-list.loader';
+export * from './use-get-new-liquidity-stats.loader';
---
+++ src/modules/new-liquidity/pages/item/index.ts
@@ -0,0 +1,2 @@
+export * from './dex-two-add-liq';
+export * from './dex-two-remove-liq';
