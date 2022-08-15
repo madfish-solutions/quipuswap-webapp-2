a8c02c2ff9a330c87f8e3eb8c4267085a9e4359a
Digberi <orkepuma@gmail.com>
2022-08-15 12:49:25 +0300

    QUIPU-350 fix by pr comments

--- src/shared/components/label-component/label-component.tsx
+++ src/shared/components/label-component/label-component.tsx
@@ -26,14 +26,21 @@ const statusClass = {
   [ActiveStatus.ACTIVE]: styles.active
 };
 
-const OrLabel: FC<{ label: [string, string] }> = ({ label }) => {
-  return (
-    <div className={styles.orLabelContainer}>
-      {label[0]}
-      <div className={styles.orLabel}>or</div>
-      {label[1]}
-    </div>
-  );
+const FIRST_TUPLE_INDEX = 0;
+const SECOND_TUPLE_INDEX = 1;
+
+const OrLabel: FC<{ label: string | [string, string] }> = ({ label }) => {
+  if (Array.isArray(label)) {
+    return (
+      <div className={styles.orLabelContainer}>
+        {label[FIRST_TUPLE_INDEX]}
+        <div className={styles.orLabel}>or</div>
+        {label[SECOND_TUPLE_INDEX]}
+      </div>
+    );
+  } else {
+    return <>{label}</>;
+  }
 };
 
 export const LabelComponent: FC<LabelComponentProps> = ({ className, status, label, filled, DTI, ...props }) => {
@@ -42,7 +49,7 @@ export const LabelComponent: FC<LabelComponentProps> = ({ className, status, lab
   return (
     <div className={cx(styles.container, themeClass[colorThemeMode], statusClass[status], className)} {...props}>
       <span className={cx(styles.status, filled ? styles.filled : styles.bordered)} data-test-id={DTI}>
-        {label ? Array.isArray(label) ? <OrLabel label={label} /> : label : status}
+        {label ? <OrLabel label={label} /> : status}
       </span>
     </div>
   );
