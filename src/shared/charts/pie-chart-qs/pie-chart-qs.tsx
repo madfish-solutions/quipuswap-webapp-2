79eafe5f5fa52a45f4b54fb202e8662467845c20
Dmytro <herkodmytro@gmail.com>
2022-08-16 10:37:40 +0300

    QUIPU-349 Fixed pie-chart

--- src/shared/charts/pie-chart-qs/pie-chart-qs.tsx
+++ src/shared/charts/pie-chart-qs/pie-chart-qs.tsx
@@ -1,46 +1,25 @@
-import { PieChart, Pie, Cell } from 'recharts';
+import { FC } from 'react';
 
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 }
-];
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+import styles from './pie-chart-qs.module.scss';
+
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-export const PieChartQs = () => {
-  return (
-    <PieChart width={800} height={400}>
-      <Pie
-        data={data}
-        cx={120}
-        cy={200}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={5}
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Pie
-        data={data}
-        cx={420}
-        cy={200}
-        startAngle={180}
-        endAngle={0}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={5}
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChart>
-  );
-};
+interface Props {
+  data: Array<{ value: number }>;
+}
+
+export const PieChartQS: FC<Props> = ({ data }) => (
+  <div className={styles.chartContainer}>
+    <ResponsiveContainer>
+      <PieChart>
+        <Pie data={data} cx="40%" innerRadius={80} outerRadius={100} dataKey="value">
+          {data.map((_, index) => (
+            <Cell key={`cell-${index}`} stroke="none" fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  </div>
+);
