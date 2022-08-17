e3e22cafc21b5737bea3b81257339c1e0bd55889
Artem <violence044@gmail.com>
2022-08-17 13:32:20 +0300

    QUIPU-317 Create Item store

---
+++ src/modules/new-liquidity/store/new-liquidity-item.store.ts
@@ -0,0 +1,23 @@
+import { BigNumber } from 'bignumber.js';
+import { computed, makeObservable } from 'mobx';
+
+import { RootStore } from '@shared/store';
+
+import { PoolType } from '../interfaces';
+
+export class NewLiquidityItemStore {
+  id: BigNumber = new BigNumber(2);
+  type: string = PoolType.DEX_TWO;
+
+  constructor(private rootStore: RootStore) {
+    makeObservable(this, {
+      item: computed
+    });
+  }
+
+  get item() {
+    return this.rootStore.newLiquidityListStore?.list.filter(
+      ({ item: { id, type } }) => Number(id) === Number(this.id) && type === this.type
+    )[0].item;
+  }
+}
