642534337228c4bb549cefad1a3c119869a944ae
Artem Koziar <koziar.artem@gmail.com>
2022-08-23 10:19:29 +0300

    QUIPU-152 Try to fix LoadingErrorDataNew and add small refactoring

--- src/shared/components/state-components/state-wrapper/error-boundary/error-boundary.tsx
+++ src/shared/components/state-components/state-wrapper/error-boundary/error-boundary.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import * as Sentry from '@sentry/react';
 
 import { CFC } from '@shared/types';
@@ -8,11 +10,14 @@ import { ErrorBoundaryProps } from './error-boundary.types';
 export const ErrorBoundary: CFC<ErrorBoundaryProps> = ({ fallback, children }) => {
   const { showErrorToast } = useToasts();
 
-  const onError = (error: Error, componentStack: string, eventId: string) => {
-    showErrorToast(error);
-    // eslint-disable-next-line no-console
-    console.error(eventId, error, componentStack);
-  };
+  const onError = useCallback(
+    (error: Error, componentStack?: string, eventId?: string) => {
+      showErrorToast(error);
+      // eslint-disable-next-line no-console
+      console.error(eventId, error, componentStack);
+    },
+    [showErrorToast]
+  );
 
   return (
     <Sentry.ErrorBoundary fallback={fallback} onError={onError}>
