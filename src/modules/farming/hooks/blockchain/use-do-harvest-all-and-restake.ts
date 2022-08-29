73363d2374cacd67bb8d32a640f129ffc02678fb
Dmytro <herkodmytro@gmail.com>
2022-08-29 14:50:04 +0300

    QUIPU-373 Harvested from disabled farms

--- src/modules/farming/hooks/blockchain/use-do-harvest-all-and-restake.ts
+++ src/modules/farming/hooks/blockchain/use-do-harvest-all-and-restake.ts
@@ -1,18 +1,13 @@
 import { useCallback } from 'react';
 
-import { BigNumber } from 'bignumber.js';
-
-import { FarmingItemModel } from '@modules/farming/models';
 import { useRootStore } from '@providers/root-store-provider';
-import { defined, isNull } from '@shared/helpers';
+import { defined } from '@shared/helpers';
 import { useConfirmOperation, useToasts } from '@shared/utils';
 import { useTranslation } from '@translation';
 
 import { harvestAllAndRestake } from '../../api';
-import { getUserInfoLastStakedTime, getEndTimestamp, getIsHarvestAvailable } from '../../helpers';
 import { useFarmingListStore } from '../stores';
-
-const ZERO_AMOUNT = 0;
+import { useStakedOnlyFarmIds } from '../use-staked-only-farm-ids';
 
 export const useDoHarvestAllAndRestake = () => {
   const { t } = useTranslation();
@@ -20,46 +15,35 @@ export const useDoHarvestAllAndRestake = () => {
   const { showErrorToast } = useToasts();
   const farmingListStore = useFarmingListStore();
   const confirmOperation = useConfirmOperation();
+  const { getStakedOnlyFarmIds } = useStakedOnlyFarmIds();
 
-  const doHarvestAllAndRestake = useCallback(
-    async (stakeList: Array<FarmingItemModel>) => {
-      if (isNull(farmingListStore)) {
-        return;
-      }
+  const doHarvestAllAndRestake = useCallback(async () => {
+    const rewardsInQuipu = await farmingListStore.getQuipuPendingRewards();
+    const stakedOnlyFarmIds = getStakedOnlyFarmIds();
 
-      const rewardsInQuipu = await farmingListStore.getQuipuPendingRewards();
+    try {
+      const operation = await harvestAllAndRestake(
+        defined(rootStore.tezos),
+        stakedOnlyFarmIds,
+        defined(rootStore.authStore.accountPkh),
+        rewardsInQuipu
+      );
 
-      //TODO: same code in "use-do-harvest-all" make helper
-      const farmingIds: BigNumber[] = stakeList
-        .filter(({ id }) =>
-          farmingListStore.getFarmingItemBalancesModelById(id.toFixed())?.earnBalance?.gt(ZERO_AMOUNT)
-        )
-        .filter(farmingItem => {
-          const userInfo = farmingListStore.findUserInfo(farmingItem.id.toFixed());
-          const lastStakedTime = getUserInfoLastStakedTime(userInfo);
-          const endTimestamp = getEndTimestamp(farmingItem, lastStakedTime);
-
-          return getIsHarvestAvailable(endTimestamp);
-        })
-        .map(({ id }) => id);
-
-      try {
-        const operation = await harvestAllAndRestake(
-          defined(rootStore.tezos),
-          farmingIds,
-          defined(rootStore.authStore.accountPkh),
-          rewardsInQuipu
-        );
-
-        await confirmOperation(operation.opHash, { message: t('farm|Stake successful') });
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log('error', error);
-        showErrorToast(error as Error);
-      }
-    },
-    [farmingListStore, rootStore.tezos, rootStore.authStore.accountPkh, confirmOperation, t, showErrorToast]
-  );
+      await confirmOperation(operation.opHash, { message: t('farm|Stake successful') });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log('error', error);
+      showErrorToast(error as Error);
+    }
+  }, [
+    farmingListStore,
+    getStakedOnlyFarmIds,
+    rootStore.tezos,
+    rootStore.authStore.accountPkh,
+    confirmOperation,
+    t,
+    showErrorToast
+  ]);
 
   return { doHarvestAllAndRestake };
 };
