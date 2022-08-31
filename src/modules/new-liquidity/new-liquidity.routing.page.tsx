f784e10aa1a547b6c839cb957ed1cd263da4210e
Artem <violence044@gmail.com>
2022-08-31 13:14:17 +0300

    QUIPU-379 Add UI form

--- src/modules/new-liquidity/new-liquidity.routing.page.tsx
+++ src/modules/new-liquidity/new-liquidity.routing.page.tsx
@@ -8,6 +8,7 @@ import { SentryRoutes } from '@shared/services';
 import { PageNotFoundPage } from '../errors';
 import { NewLiquidityRoutes } from './new-liquidity-routes.enum';
 import { useNewLiquidityViewModel } from './new-liquidity.routing.vm';
+import { NewLiquidityCreatePage } from './pages/create';
 import { DexTwoAddLiq, DexTwoRemoveLiq } from './pages/item';
 import { NewLiquidityListPage } from './pages/list';
 import { NewLiquidityFormTabs } from './types';
@@ -24,6 +25,11 @@ export const NewLiquidityPageRouter: FC = () => {
         <Route
           path={`${NewLiquidityRoutes.cpmm}/${NewLiquidityFormTabs.remove}/:pairSlug`}
           element={<DexTwoRemoveLiq />}
+        />
+
+        <Route
+          path={`${NewLiquidityRoutes.cpmm}/${NewLiquidityFormTabs.create}`}
+          element={<NewLiquidityCreatePage />}
         />
 
         <Route path="*" element={<PageNotFoundPage />} />
---
+++ src/modules/new-liquidity/pages/create/index.ts
@@ -0,0 +1,1 @@
+export * from './new-liquidity-create-page';
