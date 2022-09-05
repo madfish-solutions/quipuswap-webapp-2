15333f01ba3fa6eb9c571a58959cb7f4daee75d8
Artem <violence044@gmail.com>
2022-09-05 16:11:03 +0300

    QUIPU-379 Add amplitude, r-fctr and improvements

--- src/modules/new-liquidity/hooks/blockchain/use-new-liquidity-create-pool.ts
+++ src/modules/new-liquidity/hooks/blockchain/use-new-liquidity-create-pool.ts
@@ -1,54 +1,30 @@
 import { useCallback } from 'react';
 
-import BigNumber from 'bignumber.js';
-
 import { DEX_TWO_CONTRACT_ADDRESS } from '@config/environment';
 import { createNewLiquidityPoolApi } from '@modules/new-liquidity/api';
 import { useRootStore } from '@providers/root-store-provider';
-import { getTransactionDeadline, isEqual, isNull, isTokenFa2, saveBigNumber } from '@shared/helpers';
-import { useAuthStore } from '@shared/hooks';
+import { useNewExchangeRates } from '@providers/use-new-exchange-rate';
+import { getTokenSlug, getTokenSymbol, getTransactionDeadline, isNull } from '@shared/helpers';
+import { useAmplitudeService, useAuthStore } from '@shared/hooks';
 import { useSettingsStore } from '@shared/hooks/use-settings-store';
-import { AmountToken, Token } from '@shared/types';
+import { AmountToken } from '@shared/types';
 import { useToasts, useConfirmOperation } from '@shared/utils';
 import { useTranslation } from '@translation';
 
 const FIRST_TOKEN = 0;
-
-enum TokenChar {
-  a = 'a',
-  b = 'b'
-}
-
-export const mapTokenToBlockchainToken = (token: Token, index: number) => {
-  const tokenSymbol = isEqual(index, FIRST_TOKEN) ? TokenChar.a : TokenChar.b;
-
-  if (isTokenFa2(token)) {
-    return {
-      [`token_${tokenSymbol}`]: {
-        fa2: {
-          token: token.contractAddress,
-          id: saveBigNumber(token.fa2TokenId, new BigNumber(0))
-        }
-      }
-    };
-  } else {
-    return {
-      [`token_${tokenSymbol}`]: {
-        fa12: token.contractAddress
-      }
-    };
-  }
-};
+const SECOND_TOKEN = 1;
 
 export const useCreateNewLiquidityPool = () => {
   const { tezos } = useRootStore();
   const { t } = useTranslation();
   const { showErrorToast } = useToasts();
   const confirmOperation = useConfirmOperation();
+  const { log } = useAmplitudeService();
   const { accountPkh } = useAuthStore();
   const {
-    settings: { transactionDeadline }
+    settings: { transactionDeadline, liquiditySlippage }
   } = useSettingsStore();
+  const exchangeRates = useNewExchangeRates();
 
   const createNewLiquidityPool = useCallback(
     async (tokensAndAmounts: AmountToken[], candidate: string) => {
@@ -56,8 +32,28 @@ export const useCreateNewLiquidityPool = () => {
         return;
       }
 
-      const [token_a_in, token_b_in] = tokensAndAmounts.map(({ amount }) => amount);
-      const tokensPairParams = tokensAndAmounts.map(({ token }, index) => mapTokenToBlockchainToken(token, index));
+      const tokensExchangeRates = tokensAndAmounts.map(({ token }) => exchangeRates[getTokenSlug(token)]);
+
+      const logData = {
+        tezos,
+        contractAddress: DEX_TWO_CONTRACT_ADDRESS,
+        tokenASlug: getTokenSlug(tokensAndAmounts[FIRST_TOKEN].token),
+        tokenBSlug: getTokenSlug(tokensAndAmounts[SECOND_TOKEN].token),
+        tokenASymbol: getTokenSymbol(tokensAndAmounts[FIRST_TOKEN].token),
+        tokenBSymbol: getTokenSymbol(tokensAndAmounts[SECOND_TOKEN].token),
+        tokenAInput: tokensAndAmounts[FIRST_TOKEN].token,
+        tokenBInput: tokensAndAmounts[SECOND_TOKEN].token,
+        tokenAAmount: tokensAndAmounts[FIRST_TOKEN].amount,
+        tokenBAmount: tokensAndAmounts[SECOND_TOKEN].amount,
+        tokenAInUsd: tokensExchangeRates[FIRST_TOKEN] ?? 'testnet',
+        tokenBInUsd: tokensExchangeRates[SECOND_TOKEN] ?? 'testnet',
+        poolCreator: accountPkh,
+        baker: candidate,
+        transactionDeadline: Number(transactionDeadline.toFixed()),
+        liquiditySlippage: Number(liquiditySlippage.toFixed())
+      };
+
+      log('CREATE_POOL', logData);
 
       const deadline = await getTransactionDeadline(tezos, transactionDeadline);
 
@@ -66,21 +62,20 @@ export const useCreateNewLiquidityPool = () => {
           tezos,
           DEX_TWO_CONTRACT_ADDRESS,
           tokensAndAmounts,
-          tokensPairParams,
-          token_a_in,
-          token_b_in,
           accountPkh,
           candidate,
           deadline
         );
 
+        log('CREATE_POOL_SUCCESSFULL', logData);
         await confirmOperation(operation.opHash, { message: t('newLiquidity|succsess') });
       } catch (error) {
+        log('CREATE_POOL_FAILED', { ...logData, error });
         showErrorToast(error as Error);
         throw error;
       }
     },
-    [accountPkh, confirmOperation, showErrorToast, t, tezos, transactionDeadline]
+    [accountPkh, confirmOperation, exchangeRates, liquiditySlippage, log, showErrorToast, t, tezos, transactionDeadline]
   );
 
   return { createNewLiquidityPool };
