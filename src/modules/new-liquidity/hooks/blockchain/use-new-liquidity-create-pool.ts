1dd6115791bf55c97ae62fc9d2906cc2a286de07
Artem <violence044@gmail.com>
2022-09-05 19:48:42 +0300

    QUIPU-379 Add amplitude helper

--- src/modules/new-liquidity/hooks/blockchain/use-new-liquidity-create-pool.ts
+++ src/modules/new-liquidity/hooks/blockchain/use-new-liquidity-create-pool.ts
@@ -2,17 +2,15 @@ import { useCallback } from 'react';
 
 import { DEX_TWO_CONTRACT_ADDRESS } from '@config/environment';
 import { createNewLiquidityPoolApi } from '@modules/new-liquidity/api';
+import { getNewLiquidityCreatePoolData } from '@modules/new-liquidity/helpers/get-new-liquidity-create-pool-data';
 import { useRootStore } from '@providers/root-store-provider';
 import { useNewExchangeRates } from '@providers/use-new-exchange-rate';
-import { getTokenSlug, getTokenSymbol, getTransactionDeadline, isNull } from '@shared/helpers';
+import { getTokenSlug, getTransactionDeadline, isNull } from '@shared/helpers';
 import { useAmplitudeService, useAuthStore } from '@shared/hooks';
 import { useSettingsStore } from '@shared/hooks/use-settings-store';
 import { AmountToken } from '@shared/types';
 import { useToasts, useConfirmOperation } from '@shared/utils';
 import { useTranslation } from '@translation';
-
-const FIRST_TOKEN = 0;
-const SECOND_TOKEN = 1;
 
 export const useCreateNewLiquidityPool = () => {
   const { tezos } = useRootStore();
@@ -34,24 +32,16 @@ export const useCreateNewLiquidityPool = () => {
 
       const tokensExchangeRates = tokensAndAmounts.map(({ token }) => exchangeRates[getTokenSlug(token)]);
 
-      const logData = {
+      const logData = getNewLiquidityCreatePoolData(
         tezos,
-        contractAddress: DEX_TWO_CONTRACT_ADDRESS,
-        tokenASlug: getTokenSlug(tokensAndAmounts[FIRST_TOKEN].token),
-        tokenBSlug: getTokenSlug(tokensAndAmounts[SECOND_TOKEN].token),
-        tokenASymbol: getTokenSymbol(tokensAndAmounts[FIRST_TOKEN].token),
-        tokenBSymbol: getTokenSymbol(tokensAndAmounts[SECOND_TOKEN].token),
-        tokenAInput: tokensAndAmounts[FIRST_TOKEN].token,
-        tokenBInput: tokensAndAmounts[SECOND_TOKEN].token,
-        tokenAAmount: tokensAndAmounts[FIRST_TOKEN].amount,
-        tokenBAmount: tokensAndAmounts[SECOND_TOKEN].amount,
-        tokenAInUsd: tokensExchangeRates[FIRST_TOKEN] ?? 'testnet',
-        tokenBInUsd: tokensExchangeRates[SECOND_TOKEN] ?? 'testnet',
-        poolCreator: accountPkh,
-        baker: candidate,
-        transactionDeadline: Number(transactionDeadline.toFixed()),
-        liquiditySlippage: Number(liquiditySlippage.toFixed())
-      };
+        DEX_TWO_CONTRACT_ADDRESS,
+        tokensAndAmounts,
+        tokensExchangeRates,
+        candidate,
+        accountPkh,
+        transactionDeadline,
+        liquiditySlippage
+      );
 
       log('CREATE_POOL', logData);
 
