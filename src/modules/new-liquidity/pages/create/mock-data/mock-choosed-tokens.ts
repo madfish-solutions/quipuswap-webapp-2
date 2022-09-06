5a01eaab60fdadd3ace93b9d4d95669aec52cb83
Artem <violence044@gmail.com>
2022-09-06 13:52:02 +0300

    QUIPU-379 fix comments, add token selection to input

--- src/modules/new-liquidity/pages/create/mock-data/mock-choosed-tokens.ts
+++
@@ -1,3 +0,0 @@
-import { QUIPU_TOKEN, TEZOS_TOKEN } from '@config/tokens';
-
-export const MOCK_CHOOSED_TOKENS = [TEZOS_TOKEN, QUIPU_TOKEN];
--- src/shared/helpers/bakers/can-delegate.ts
+++ src/shared/helpers/bakers/can-delegate.ts
@@ -1,5 +1,6 @@
-import { Token } from '@shared/types';
+import { Optional, Token } from '@shared/types';
 
 import { isTezosToken } from '../get-token-appellation';
+import { isExist } from '../type-checks';
 
-export const canDelegate = (tokensInfo: Array<Token>) => tokensInfo.some(isTezosToken);
+export const canDelegate = (tokensInfo: Array<Optional<Token>>) => tokensInfo.filter(isExist).some(isTezosToken);
