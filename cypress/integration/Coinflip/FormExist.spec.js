9d25913c3fa86b64eaf9fe077b4aac3c485beeb8
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2022-09-12 16:08:57 +0300

    Coinflip fixes

--- cypress/integration/Coinflip/FormExist.spec.js
+++ cypress/integration/Coinflip/FormExist.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { cssTransition } from "react-toastify";
+
 describe('Form exists', () => {
   beforeEach(() => {
     cy.visit('/coinflip');
@@ -21,5 +23,18 @@ describe('Form exists', () => {
     cy.get('[data-test-id="coinflipRules"] [data-test-id="headerContent"]')
       .should('contain', 'Play Coinflip - get a chance to double your bid!')
   })
-
+  it('Should_Have_TotalWinsGteThan2900_When_PageIsLoaded', () =>{
+    cy.waitUntil(() => (cy.get('[data-test-id="coinflipTotalWins"] [data-test-id="amount"]')
+    .invoke('text').then(parseFloat).then((val) => !Number.isNaN(val))))
+   cy.get('[data-test-id="coinflipTotalWins"] [data-test-id="amount"]')
+   .invoke('text').then(parseInt).should('gte', 2)
+  });
+  it('Should_Have_GameCountGteThan473_When_PageIsLoaded', () =>{
+    cy.get('[data-test-id="coinflipGamesCount"] [data-test-id="amount"]')
+    .invoke('text').then(parseFloat).should('gte', 473)
+  });
+  it('Should_Have_MaxBetGteThan0_When_PageIsLoaded', () =>{
+cy.get('[data-test-id="coinflipMaxAllowableBid"] [data-test-id="amount"]')
+.invoke('text').then(parseFloat).should('gte', 0)
+  });
 });
