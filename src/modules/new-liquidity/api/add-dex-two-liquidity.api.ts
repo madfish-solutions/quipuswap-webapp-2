923b9df9a1fedd3852642159f8a7104f13b22e49
Artem <violence044@gmail.com>
2022-10-03 15:04:55 +0300

    QUIPU-454 Add referral code

--- src/modules/new-liquidity/api/add-dex-two-liquidity.api.ts
+++ src/modules/new-liquidity/api/add-dex-two-liquidity.api.ts
@@ -2,7 +2,7 @@ import { TezosToolkit } from '@taquito/taquito';
 import { BigNumber } from 'bignumber.js';
 
 import { withApproveApiForManyTokens } from '@blockchain';
-import { ZERO_BAKER_ADDRESS } from '@config/constants';
+import { QUIPUSWAP_REFERRAL_CODE, ZERO_BAKER_ADDRESS } from '@config/constants';
 import { DEX_TWO_CONTRACT_ADDRESS } from '@config/environment';
 import { isGreaterThanZero } from '@shared/helpers';
 import { AmountToken } from '@shared/types';
@@ -29,7 +29,16 @@ export const addDexTwoLiquidityApi = async (
   const [tokenA, tokenB] = tokensAndAmounts;
 
   const dexTwoLiquidityParams = dexTwoContract.methods
-    .invest_liquidity(itemId, tokenA.amount, tokenB.amount, shares, accountPkh, candidate, deadline)
+    .invest_liquidity(
+      itemId,
+      tokenA.amount,
+      tokenB.amount,
+      shares,
+      accountPkh,
+      candidate,
+      deadline,
+      QUIPUSWAP_REFERRAL_CODE
+    )
     .toTransferParams({ mutez: true, amount: tezValue.toNumber() });
 
   const cleanedTokensAmount = tokensAndAmounts.filter(({ amount }) => isGreaterThanZero(amount));
