7091d617e70125e86d5c389fcf44c861f3cb9d76
Artem <violence044@gmail.com>
2022-10-04 14:52:25 +0300

    QUIPU-303 fix border color

--- src/shared/modals/tokens-modal/components/managed-tokens-modal-cell/managed-tokens-list-modal-cell.tsx
+++ src/shared/modals/tokens-modal/components/managed-tokens-modal-cell/managed-tokens-list-modal-cell.tsx
@@ -1,11 +1,19 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 
+import cx from 'classnames';
+
+import { ColorModes, ColorThemeContext } from '@providers/color-theme-context';
 import { Switcher, TokensLogos } from '@shared/components';
 import { FavoriteButton } from '@shared/elements';
 import { getTokenName, getTokenSymbol } from '@shared/helpers';
 import { ManagedToken } from '@shared/types';
 
 import styles from './managed-tokens-list-modal-cell.module.scss';
+
+const modeClass = {
+  [ColorModes.Light]: styles.light,
+  [ColorModes.Dark]: styles.dark
+};
 
 export interface ManagedTokensModalCellProps {
   token: ManagedToken;
@@ -14,18 +22,22 @@ export interface ManagedTokensModalCellProps {
 }
 const BIG_SLICE_AMOUNT = 50;
 
-export const ManagedTokensModalCell: FC<ManagedTokensModalCellProps> = ({ token, onFavoriteClick, onHideClick }) => (
-  <div className={styles.tokensModalCell}>
-    <TokensLogos width={32} tokens={token} />
+export const ManagedTokensModalCell: FC<ManagedTokensModalCellProps> = ({ token, onFavoriteClick, onHideClick }) => {
+  const { colorThemeMode } = useContext(ColorThemeContext);
 
-    <div>
-      <h6 className={styles.tokenSymbol}>{getTokenSymbol(token, BIG_SLICE_AMOUNT)}</h6>
-      <div className={styles.tokenName}>{getTokenName(token, BIG_SLICE_AMOUNT)}</div>
-    </div>
+  return (
+    <div className={cx(styles.tokensModalCell, modeClass[colorThemeMode])}>
+      <TokensLogos width={32} tokens={token} />
 
-    <div className={styles.checkboxContainer}>
-      <FavoriteButton checked={Boolean(token.isFavorite)} onClick={onFavoriteClick} />
-      <Switcher onClick={onHideClick} value={!Boolean(token.isHidden)} />
+      <div>
+        <h6 className={styles.tokenSymbol}>{getTokenSymbol(token, BIG_SLICE_AMOUNT)}</h6>
+        <div className={styles.tokenName}>{getTokenName(token, BIG_SLICE_AMOUNT)}</div>
+      </div>
+
+      <div className={styles.checkboxContainer}>
+        <FavoriteButton checked={Boolean(token.isFavorite)} onClick={onFavoriteClick} />
+        <Switcher onClick={onHideClick} value={!Boolean(token.isHidden)} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
