c7de2756bdd17faf64ce0e5a1ac241c941b52b1b
Digberi <orkepuma@gmail.com>
2022-10-11 17:40:44 +0300

    QUIPU-382 add contract interaction

--- src/modules/new-liquidity/pages/item/components/dex-two-claim-rewards/use-dex-two-claim-rewards.vm.ts
+++ src/modules/new-liquidity/pages/item/components/dex-two-claim-rewards/use-dex-two-claim-rewards.vm.ts
@@ -1,34 +1,34 @@
 import { useCallback } from 'react';
 
-import BigNumber from 'bignumber.js';
-
 import { TEZOS_TOKEN } from '@config/tokens';
 import { useNewLiquidityItemStore } from '@modules/new-liquidity/hooks';
 import { useNewExchangeRates } from '@providers/use-new-exchange-rate';
 import { getTokenSlug, isNull, multipliedIfPossible } from '@shared/helpers';
 import { useTokenBalance } from '@shared/hooks';
 
+import { useClaimRewards } from './use-claim-rewards';
+import { useNewLiquidityRewards } from './use-new-liquidity-rewards';
+
 export const useDexTwoClaimRewardsFromViewModel = () => {
   const balance = useTokenBalance(TEZOS_TOKEN);
   const { item } = useNewLiquidityItemStore();
   const exchangeRate = useNewExchangeRates();
+  const { claim } = useClaimRewards();
+  const { rewards } = useNewLiquidityRewards();
 
-  const doClaim = useCallback(() => {
-    // eslint-disable-next-line no-console
-    console.log('doClaim');
-  }, []);
+  const doClaim = useCallback(async () => {
+    await claim();
+  }, [claim]);
 
   if (!isNull(item)) {
     const tokenSlug = getTokenSlug(TEZOS_TOKEN);
     const tokenExchangeRate = exchangeRate[tokenSlug];
 
-    const rewards = new BigNumber('15');
-
     return {
       balance,
       rewardDollarEquivalent: multipliedIfPossible(tokenExchangeRate, rewards),
       doClaim,
-      rewardValue: rewards.toFixed()
+      rewardValue: rewards?.toFixed() ?? 'wait'
     };
   }
 
