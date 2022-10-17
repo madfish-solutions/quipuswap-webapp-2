ab32f6aba211e4b76e95acf1b8346f25adf00d9f
Artem <violence044@gmail.com>
2022-10-17 13:34:57 +0300

    QUIPU-475 Add link to pair contract button

--- src/modules/new-liquidity/pages/item/components/dex-two-details/dex-two-details.vm.ts
+++ src/modules/new-liquidity/pages/item/components/dex-two-details/dex-two-details.vm.ts
@@ -1,11 +1,14 @@
 import cx from 'classnames';
 
 import { DEFAULT_DECIMALS } from '@config/constants';
+import { TZKT_EXPLORER_URL } from '@config/environment';
 import { getTokenSymbol, toReal } from '@shared/helpers';
 import commonContainerStyles from '@styles/CommonContainer.module.scss';
 
 import { useNewLiquidityItemStore } from '../../../../hooks';
 import styles from './dex-two-details.module.scss';
+
+const DEX_TWO_PAIR_CONTRACT = 'KT1GPJDTf8GZspCcanaG2KhMvGu3NJRqurat';
 
 export const useDexTwoDetailsViewModel = () => {
   const item = useNewLiquidityItemStore();
@@ -15,13 +18,15 @@ export const useDexTwoDetailsViewModel = () => {
     tokenSymbol: getTokenSymbol(token)
   }));
 
+  const poolContractUrl = `${TZKT_EXPLORER_URL}/${DEX_TWO_PAIR_CONTRACT}`;
+
   return {
     apr: null,
     feesRate: null,
     weeklyVolume: null,
     tvlInUsd: item?.item?.tvlInUsd,
     totalLpSupply: item?.item?.totalSupply && toReal(item?.item?.totalSupply, DEFAULT_DECIMALS),
-    poolContractUrl: '/',
+    poolContractUrl,
     cardCellClassName: cx(commonContainerStyles.cellCenter, commonContainerStyles.cell, styles.vertical),
     pieChartData: pieChartData ?? []
   };
