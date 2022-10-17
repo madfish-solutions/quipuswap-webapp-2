be53e644467d392026268b14edeb02b2c7d2a9bb
Dmytro <herkodmytro@gmail.com>
2022-10-17 18:58:11 +0300

    Improved tokens categories component

--- src/modules/new-liquidity/pages/list/components/tokens-categories-filter/tokens-categories-filter.tsx
+++ src/modules/new-liquidity/pages/list/components/tokens-categories-filter/tokens-categories-filter.tsx
@@ -1,3 +1,6 @@
+import { observer } from 'mobx-react-lite';
+
+import { Button } from '@shared/components';
 import {
   BridgeCategory,
   BtcCategory,
@@ -7,20 +10,45 @@ import {
   TezotopiaCategory
 } from '@shared/svg/categories';
 
-import { useNewLiquidityListStore } from '../../../../hooks';
 import styles from './tokens-categories-filter.module.scss';
+import { useTokensCategoriesFilter } from './tokens-categories-filter.vm';
 
-export const TokensCategoriesFilter = () => {
-  const { showStable, showBridged, showQuipu, showTezotopia, showBTC, showDexTwo } = useNewLiquidityListStore();
+export const TokensCategoriesFilter = observer(() => {
+  const {
+    showStable,
+    showBridged,
+    showQuipu,
+    showTezotopia,
+    showBTC,
+    showDexTwo,
+    toggleShowStable,
+    toggleShowBridged,
+    toggleShowQuipu,
+    toggleShowTezotopia,
+    toggleShowBTC,
+    toggleShowDexTwo
+  } = useTokensCategoriesFilter();
 
   return (
     <div className={styles.root}>
-      <StableCategory colored={showStable} />
-      <BridgeCategory colored={showBridged} />
-      <QuipuCategory colored={showQuipu} />
-      <TezotopiaCategory colored={showTezotopia} />
-      <BtcCategory colored={showBTC} />
-      <DexTwoCategoryIcon colored={showDexTwo} />
+      <Button theme="quaternary" onClick={toggleShowStable}>
+        <StableCategory colored={showStable} />
+      </Button>
+      <Button theme="quaternary" onClick={toggleShowBridged}>
+        <BridgeCategory colored={showBridged} />
+      </Button>
+      <Button theme="quaternary" onClick={toggleShowQuipu}>
+        <QuipuCategory colored={showQuipu} />
+      </Button>
+      <Button theme="quaternary" onClick={toggleShowTezotopia}>
+        <TezotopiaCategory colored={showTezotopia} />
+      </Button>
+      <Button theme="quaternary" onClick={toggleShowBTC}>
+        <BtcCategory colored={showBTC} />
+      </Button>
+      <Button theme="quaternary" onClick={toggleShowDexTwo}>
+        <DexTwoCategoryIcon colored={showDexTwo} />
+      </Button>
     </div>
   );
-};
+});
