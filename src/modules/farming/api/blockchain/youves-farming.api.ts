10535e5aa7008aab975d7e9bb2a0fc601a6ad20f
Artem Koziar <koziar.artem@gmail.com>
2022-10-18 14:58:41 +0300

    QUIPU-444 Refactor namespace

--- src/modules/farming/api/blockchain/youves-farming.api.ts
+++ src/modules/farming/api/blockchain/youves-farming.api.ts
@@ -5,15 +5,15 @@ import { withApproveApi } from '@blockchain';
 import { getStorageInfo } from '@shared/dapp';
 import { Standard, TokenIdFa2 } from '@shared/types';
 
-export namespace YouvesFarmingApi {
-  export interface IFarmingStorage {
-    deposit_token: {
-      id: BigNumber;
-      address: string;
-    };
-  }
+interface IFarmingStorage {
+  deposit_token: {
+    id: BigNumber;
+    address: string;
+  };
+}
 
-  export const getToken = async (tezos: TezosToolkit, contractAddress: string): Promise<TokenIdFa2> => {
+export class YouvesFarmingApi {
+  static async getToken(tezos: TezosToolkit, contractAddress: string): Promise<TokenIdFa2> {
     const storage = await getStorageInfo<IFarmingStorage>(tezos, contractAddress);
     const { id, address } = storage.deposit_token;
 
@@ -22,27 +22,27 @@ export namespace YouvesFarmingApi {
       fa2TokenId: id.toNumber(),
       contractAddress: address
     };
-  };
+  }
 
-  export const getStakes = async (tezos: TezosToolkit, accountPkh: string, contractAddress: string) => {
+  static async getStakesIds(tezos: TezosToolkit, accountPkh: string, contractAddress: string) {
     const contract = await tezos.contract.at(contractAddress);
 
     return await contract.contractViews.view_owner_stakes(accountPkh).executeView({ viewCaller: accountPkh });
-  };
+  }
 
-  export const deposit = async (
+  static async deposit(
     tezos: TezosToolkit,
     accountPkh: string,
     contractAddress: string,
     stakeId: BigNumber.Value,
     tokenAmount: BigNumber.Value
-  ) => {
+  ) {
     const contract = await tezos.contract.at(contractAddress);
 
     const params = contract.methods.deposit(new BigNumber(stakeId), new BigNumber(tokenAmount)).toTransferParams();
 
-    const token = await getToken(tezos, contractAddress);
+    const token = await this.getToken(tezos, contractAddress);
 
     return await withApproveApi(tezos, contractAddress, token, accountPkh, tokenAmount, [params]);
-  };
+  }
 }
