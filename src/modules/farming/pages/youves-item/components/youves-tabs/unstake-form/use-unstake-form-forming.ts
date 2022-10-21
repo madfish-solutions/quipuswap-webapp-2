1f49219cbb07db2c0fbbccbca9e3653448ef6479
Artem Koziar <koziar.artem@gmail.com>
2022-10-21 17:27:04 +0300

    QUIPU-445 Add navigation on unstake

--- src/modules/farming/pages/youves-item/components/youves-tabs/unstake-form/use-unstake-form-forming.ts
+++ src/modules/farming/pages/youves-item/components/youves-tabs/unstake-form/use-unstake-form-forming.ts
@@ -1,10 +1,14 @@
 import { FormEvent, useState } from 'react';
 
 import BigNumber from 'bignumber.js';
+import { useNavigate } from 'react-router-dom';
 
+import { AppRootRoutes } from '@app.router';
 import { defined } from '@shared/helpers';
 
+import { FarmingRoutes } from '../../../../../farming.router';
 import { useDoYouvesFarmingWithdraw } from '../../../../../hooks';
+import { YouvesFormTabs } from '../../../types';
 
 export const useUnstakeFormForming = (
   contractAddress: Nullable<string>,
@@ -13,6 +17,7 @@ export const useUnstakeFormForming = (
 ) => {
   const { doWithdraw } = useDoYouvesFarmingWithdraw();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -20,6 +25,7 @@ export const useUnstakeFormForming = (
     setIsSubmitting(true);
     await doWithdraw(defined(contractAddress, 'Contract address'), stakeId, defined(balance, 'Balance'));
     setIsSubmitting(false);
+    navigate(`${AppRootRoutes.Farming}${FarmingRoutes.Youves}/${contractAddress}/${YouvesFormTabs.stake}`);
   };
 
   return {
