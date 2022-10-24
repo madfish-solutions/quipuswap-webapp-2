b00dd8a2b181be043af9555856c4c9abe423121d
Artem <violence044@gmail.com>
2022-10-24 16:19:56 +0300

    QUIPU-448 simplify func

--- src/modules/farming/pages/youves-item/api/get-user-rewards.ts
+++ src/modules/farming/pages/youves-item/api/get-user-rewards.ts
@@ -2,12 +2,12 @@ import { TezosToolkit } from '@taquito/taquito';
 import { BigNumber } from 'bignumber.js';
 
 import { getUserBalance } from '@blockchain';
-import { MS_IN_SECOND, PRECISION_FACTOR, ZERO_AMOUNT, ZERO_AMOUNT_BN } from '@config/constants';
+import { PRECISION_FACTOR, ZERO_AMOUNT, ZERO_AMOUNT_BN } from '@config/constants';
 import { getStorageInfo } from '@shared/dapp';
 import { getLastElement, isNull, isUndefined } from '@shared/helpers';
 import { Standard, Undefined } from '@shared/types';
 
-import { getTokenDecimalsAndPrecision } from '../helpers';
+import { getRewards, getTokenDecimalsAndPrecision } from '../helpers';
 import { YouvesFarmStakes, YouvesFarmStorage } from './types';
 
 // TODO: Add fa12 support when contracts will be ready
@@ -46,30 +46,13 @@ export const getUserRewards = async (
   }
 
   const ids: Array<BigNumber> = (await stakes_owner_lookup.get(accountPkh)) ?? [];
-
   const last_stake = (await stakes.get<YouvesFarmStakes>(Number(getLastElement(ids)))) ?? undefined;
 
-  if (isUndefined(last_stake)) {
+  if (isUndefined(last_stake) || isUndefined(_disc_factor)) {
     return DEFAULT_REWARDS;
   }
 
-  const { stake: ls_stake, age_timestamp: ls_age_timestamp, disc_factor: ls_disc_factor } = last_stake;
-
-  if (
-    isUndefined(ls_age_timestamp) ||
-    isUndefined(ls_disc_factor) ||
-    isUndefined(ls_stake) ||
-    isUndefined(_disc_factor)
-  ) {
-    return DEFAULT_REWARDS;
-  }
-
-  const max_release_period_ms = max_release_period.multipliedBy(MS_IN_SECOND).toNumber();
-
-  const stake_age = Math.min(Date.now() - new Date(ls_age_timestamp).getTime(), max_release_period_ms);
-  const full_reward = ls_stake.multipliedBy(_disc_factor.minus(ls_disc_factor)).dividedToIntegerBy(PRECISION_FACTOR);
-  const claimable_reward = full_reward.multipliedBy(stake_age).dividedToIntegerBy(max_release_period_ms);
-
+  const { claimable_reward, full_reward } = getRewards(last_stake, max_release_period, _disc_factor);
   const { tokenDecimals, tokenPrecision } = await getTokenDecimalsAndPrecision(deposit_token.address, deposit_token.id);
 
   return {
