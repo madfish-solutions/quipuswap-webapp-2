a39069ef66f02f28650f0bb10cf7d01f43a68aa1
Artem Koziar <koziar.artem@gmail.com>
2022-10-25 14:34:56 +0300

    QUIPU-509 Add tooltips

--- src/modules/new-liquidity/pages/list/components/tokens-categories-filter/tokens-categories-filter.tsx
+++ src/modules/new-liquidity/pages/list/components/tokens-categories-filter/tokens-categories-filter.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import cx from 'classnames';
 import { observer } from 'mobx-react-lite';
 
-import { Button } from '@shared/components';
+import { Button, Tooltip } from '@shared/components';
 import {
   BridgeCategory,
   BtcCategory,
@@ -38,24 +38,48 @@ export const TokensCategoriesFilter: FC<Props> = observer(({ className }) => {
 
   return (
     <div className={cx(className, styles.root)}>
-      <Button theme="quaternary" onClick={toggleShowStable}>
-        <StableCategory colored={showStable} />
-      </Button>
-      <Button theme="quaternary" onClick={toggleShowBridged}>
-        <BridgeCategory colored={showBridged} />
-      </Button>
-      <Button theme="quaternary" onClick={toggleShowQuipu}>
-        <QuipuCategory colored={showQuipu} />
-      </Button>
-      <Button theme="quaternary" onClick={toggleShowTezotopia}>
-        <TezotopiaCategory colored={showTezotopia} />
-      </Button>
-      <Button theme="quaternary" onClick={toggleShowBTC}>
-        <BtcCategory colored={showBTC} />
-      </Button>
-      <Button theme="quaternary" onClick={toggleShowDexTwo}>
-        <DexTwoCategoryIcon colored={showDexTwo} />
-      </Button>
+      <Tooltip content="StableSwap Pools">
+        <div>
+          <Button theme="quaternary" onClick={toggleShowStable}>
+            <StableCategory colored={showStable} />
+          </Button>
+        </div>
+      </Tooltip>
+      <Tooltip content="Pools with Bridged tokens">
+        <div>
+          <Button theme="quaternary" onClick={toggleShowBridged}>
+            <BridgeCategory colored={showBridged} />
+          </Button>
+        </div>
+      </Tooltip>
+      <Tooltip content="Pools with Quipu">
+        <div>
+          <Button theme="quaternary" onClick={toggleShowQuipu}>
+            <QuipuCategory colored={showQuipu} />
+          </Button>
+        </div>
+      </Tooltip>
+      <Tooltip content="Tezotopia Pools">
+        <div>
+          <Button theme="quaternary" onClick={toggleShowTezotopia}>
+            <TezotopiaCategory colored={showTezotopia} />
+          </Button>
+        </div>
+      </Tooltip>
+      <Tooltip content="BTC Pools">
+        <div>
+          <Button theme="quaternary" onClick={toggleShowBTC}>
+            <BtcCategory colored={showBTC} />
+          </Button>
+        </div>
+      </Tooltip>
+      <Tooltip content="Dex 2.0 Pools">
+        <div>
+          <Button theme="quaternary" onClick={toggleShowDexTwo}>
+            <DexTwoCategoryIcon colored={showDexTwo} />
+          </Button>
+        </div>
+      </Tooltip>
     </div>
   );
 });
