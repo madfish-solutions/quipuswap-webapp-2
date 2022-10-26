7d687cb6906988bfd2a53a9cafff1e46ce46db4c
Inokentii Mazhara <keshan3262@gmail.com>
2022-10-26 23:12:28 +0300

    Move userTotalDepositDollarEquivalent calculation

--- src/modules/farming/pages/youves-item/components/youves-reward-info/use-youves-reward-info.vm.ts
+++ src/modules/farming/pages/youves-item/components/youves-reward-info/use-youves-reward-info.vm.ts
@@ -1,6 +1,6 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-import { ZERO_AMOUNT, ZERO_AMOUNT_BN } from '@config/constants';
+import { ZERO_AMOUNT } from '@config/constants';
 import { QUIPU_TOKEN, TEZOS_TOKEN } from '@config/tokens';
 import { useDoYouvesHarvest, useFarmingYouvesItemStore, useGetYouvesFarmingItem } from '@modules/farming/hooks';
 import { useRootStore } from '@providers/root-store-provider';
@@ -10,11 +10,10 @@ import { useOnBlock, useToken, useTokenBalance } from '@shared/hooks';
 import { amplitudeService } from '@shared/services';
 
 import { getRewardsDueDate } from '../../api/get-rewards-due-date';
-import { getTotalDeposit } from '../../api/get-total-deposit';
+import { useYouvesFarmingItemRewards } from './use-youves-rewards';
 
 export const useYouvesRewardInfoViewModel = () => {
-  // TODO: remove useState when store will be ready
-  const [userTotalDeposit, setTotalDeposit] = useState(ZERO_AMOUNT_BN);
+  // TODO: remove useState using store
   const [rewadsDueDate, setRewardsDueDate] = useState(ZERO_AMOUNT);
   const { tezos } = useRootStore();
   const { doHarvest } = useDoYouvesHarvest();
@@ -25,10 +24,18 @@ export const useYouvesRewardInfoViewModel = () => {
   const stakedToken = useToken(youvesFarmingItem?.stakedToken ?? null);
   const rewardToken = useToken(youvesFarmingItem?.rewardToken ?? null);
   const earnBalance = useTokenBalance(stakedToken);
-  const { claimableRewards, longTermRewards } = youvesFarmingItemStore;
 
   const symbolsString = getSymbolsString([QUIPU_TOKEN, TEZOS_TOKEN]);
   const rewardTokenCurrency = getSymbolsString(youvesFarmingItemStore.item?.rewardToken ?? null);
+
+  const {
+    claimableRewards,
+    claimablePendingRewardsInUsd,
+    longTermRewards,
+    longTermPendingRewardsInUsd,
+    userTotalDeposit,
+    userTotalDepositDollarEquivalent
+  } = useYouvesFarmingItemRewards();
 
   const handleHarvest = async () => {
     // TODO: add real balances, which are important for analytics
@@ -46,21 +53,13 @@ export const useYouvesRewardInfoViewModel = () => {
   const getUserStakeInfo = useCallback(async () => {
     if (!youvesFarmingItem) {
       setRewardsDueDate(ZERO_AMOUNT);
-      setTotalDeposit(ZERO_AMOUNT_BN);
 
       return;
     }
 
     const dueDate = await getRewardsDueDate(tezos, accountPkh, youvesFarmingItem.contractAddress);
     setRewardsDueDate(dueDate);
-    const totalDeposit = await getTotalDeposit(tezos, accountPkh, youvesFarmingItem.contractAddress);
-    setTotalDeposit(totalDeposit);
   }, [accountPkh, tezos, youvesFarmingItem]);
-
-  const userTotalDepositDollarEquivalent = useMemo(
-    () => userTotalDeposit.times(youvesFarmingItem?.depositExchangeRate ?? ZERO_AMOUNT_BN) ?? null,
-    [userTotalDeposit, youvesFarmingItem]
-  );
 
   useEffect(() => {
     getUserStakeInfo();
@@ -71,8 +70,8 @@ export const useYouvesRewardInfoViewModel = () => {
   return {
     claimablePendingRewards: claimableRewards,
     longTermPendingRewards: longTermRewards,
-    claimablePendingRewardsInUsd: youvesFarmingItemStore.claimableRewardsInUsd,
-    longTermPendingRewardsInUsd: youvesFarmingItemStore.longTermRewardsInUsd,
+    claimablePendingRewardsInUsd,
+    longTermPendingRewardsInUsd,
     shouldShowCountdown: true,
     shouldShowCountdownValue: true,
     farmingLoading: false,
