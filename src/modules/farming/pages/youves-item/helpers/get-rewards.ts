6dd10b4848b077d8d97dc3bf1c8494d725aa945e
Inokentii Mazhara <keshan3262@gmail.com>
2022-10-28 18:26:34 +0300

    Remove redundant console.log

--- src/modules/farming/pages/youves-item/helpers/get-rewards.ts
+++ src/modules/farming/pages/youves-item/helpers/get-rewards.ts
@@ -13,15 +13,6 @@ export const getRewards = (
   const { stake: ls_stake, age_timestamp: ls_age_timestamp, disc_factor: ls_disc_factor } = userStake;
   const max_release_period_ms = maxReleasePerios.multipliedBy(MS_IN_SECOND).toNumber();
 
-  // eslint-disable-next-line no-console
-  console.log(
-    'get_rewards',
-    ls_stake.toFixed(),
-    ls_age_timestamp,
-    ls_disc_factor.toFixed(),
-    max_release_period_ms,
-    discFactor.toFixed()
-  );
   const stake_age = Math.min(Date.now() - new Date(ls_age_timestamp).getTime(), max_release_period_ms);
   const full_reward = ls_stake.multipliedBy(discFactor.minus(ls_disc_factor)).dividedToIntegerBy(PRECISION_FACTOR);
   const claimable_reward = full_reward.multipliedBy(stake_age).dividedToIntegerBy(max_release_period_ms);
