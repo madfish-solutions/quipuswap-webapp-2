1ade6a91929e91113e6f05defcbd56314a5fe59f
Inokentii Mazhara <keshan3262@gmail.com>
2022-10-28 21:58:20 +0300

    Rewrite getRewards function

--- src/modules/farming/pages/youves-item/helpers/get-rewards.ts
+++ src/modules/farming/pages/youves-item/helpers/get-rewards.ts
@@ -2,20 +2,22 @@ import { BigNumber } from 'bignumber.js';
 
 import { MS_IN_SECOND, PRECISION_FACTOR } from '@config/constants';
 import { YouvesStakeDto } from '@modules/farming/dto';
+import { getNowTimestampInSeconds } from '@shared/helpers';
 
 import { YouvesFarmStakes } from '../api/types';
 
 export const getRewards = (
   userStake: YouvesFarmStakes | YouvesStakeDto,
-  maxReleasePerios: BigNumber,
+  maxReleasePeriod: BigNumber,
   discFactor: BigNumber
 ) => {
-  const { stake: ls_stake, age_timestamp: ls_age_timestamp, disc_factor: ls_disc_factor } = userStake;
-  const max_release_period_ms = maxReleasePerios.multipliedBy(MS_IN_SECOND).toNumber();
+  const { stake: stakeAmount, age_timestamp: ageTimestamp, disc_factor: userDiscFactor } = userStake;
+  const nowTimestamp = getNowTimestampInSeconds();
+  const ageTimestampInSeconds = Math.floor(new Date(ageTimestamp).getTime() / MS_IN_SECOND);
 
-  const stake_age = Math.min(Date.now() - new Date(ls_age_timestamp).getTime(), max_release_period_ms);
-  const full_reward = ls_stake.multipliedBy(discFactor.minus(ls_disc_factor)).dividedToIntegerBy(PRECISION_FACTOR);
-  const claimable_reward = full_reward.multipliedBy(stake_age).dividedToIntegerBy(max_release_period_ms);
+  const stakeAge = BigNumber.min(nowTimestamp - ageTimestampInSeconds, maxReleasePeriod);
+  const fullReward = stakeAmount.times(discFactor.minus(userDiscFactor)).dividedToIntegerBy(PRECISION_FACTOR);
+  const claimableReward = fullReward.times(stakeAge).dividedToIntegerBy(maxReleasePeriod);
 
-  return { claimable_reward, full_reward };
+  return { claimableReward, fullReward };
 };
