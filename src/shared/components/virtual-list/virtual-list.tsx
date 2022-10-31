c35286c8da10e74457d063d3429e1365f7077ded
Digberi <orkepuma@gmail.com>
2022-10-31 10:09:53 +0200

    QUIPU-458 clean up

--- src/shared/components/virtual-list/virtual-list.tsx
+++ src/shared/components/virtual-list/virtual-list.tsx
@@ -2,21 +2,23 @@
 import { memo, useEffect, useRef, useState } from 'react';
 
 import { isEmptyArray } from 'formik';
-import { nextTick } from 'process';
+// import { nextTick } from 'process';
 
 import { isExist, isNull } from '@shared/helpers';
 
 import { Iterator, IteratorComponent } from '../iterator';
 
+//TODO: all comments belongs to next iteration https://madfish.atlassian.net/browse/QUIPU-558
+
 const START_INDEX = 0;
 const STEP = 10;
-const MAX_ITEMS = 50;
+// const MAX_ITEMS = 50;
 
 function useVirtualListViewModel<T>(items: Array<T>) {
   const lastElementRef = useRef<HTMLDivElement>(null);
   const lastElementIntersectionObserver = useRef<Nullable<IntersectionObserver>>(null);
 
-  const tenLastItemsOffset = useRef(0);
+  // const tenLastItemsOffset = useRef(0);
 
   const [shownItems, setShownItems] = useState<typeof items>([]);
 
@@ -34,24 +36,24 @@ function useVirtualListViewModel<T>(items: Array<T>) {
 
       lastIndexOfShownItem = lastIndexOfShownItem + STEP;
 
-      const startIndex = Math.max(START_INDEX, lastIndexOfShownItem - MAX_ITEMS);
+      // const startIndex = Math.max(START_INDEX, lastIndexOfShownItem - MAX_ITEMS);
 
-      const newItems = items.slice(startIndex, lastIndexOfShownItem);
+      const newItems = items.slice(START_INDEX, lastIndexOfShownItem);
 
-      const isLastIterationBeforeFirstElementsWillBeHidden = MAX_ITEMS - lastIndexOfShownItem < STEP;
-      const isEndOfItems = items.length - lastIndexOfShownItem < STEP;
+      // const isLastIterationBeforeFirstElementsWillBeHidden = MAX_ITEMS - lastIndexOfShownItem < STEP;
+      // const isEndOfItems = items.length - lastIndexOfShownItem < STEP;
 
-      if (isLastIterationBeforeFirstElementsWillBeHidden && !isEndOfItems) {
-        if (tenLastItemsOffset.current == 0) {
-          tenLastItemsOffset.current = lastElementRef.current.offsetTop;
-        }
-        window.scrollTo({ top: tenLastItemsOffset.current });
-      }
+      // if (isLastIterationBeforeFirstElementsWillBeHidden && !isEndOfItems) {
+      //   if (tenLastItemsOffset.current == 0) {
+      //     tenLastItemsOffset.current = lastElementRef.current.offsetTop;
+      //   }
+      //   window.scrollTo({ top: tenLastItemsOffset.current });
+      // }
 
-      console.log({ startIndex, lastIndexOfShownItem, newItems, originalLength: items.length });
+      // console.log({ startIndex, lastIndexOfShownItem, newItems, originalLength: items.length });
 
       setShownItems(newItems);
-      nextTick(() => console.log(lastElementRef.current?.offsetTop));
+      // nextTick(() => console.log(lastElementRef.current?.offsetTop));
     });
 
     lastElementIntersectionObserver.current.observe(lastElementRef.current);
