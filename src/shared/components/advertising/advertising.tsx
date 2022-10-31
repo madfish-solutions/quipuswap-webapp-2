e951d5774ea5d40a664fb29fc04e0c3033f1d2a6
Artem <violence044@gmail.com>
2022-10-31 12:43:55 +0200

    QUIPU-469 Add amplitude to Yupana banner

--- src/shared/components/advertising/advertising.tsx
+++ src/shared/components/advertising/advertising.tsx
@@ -1,12 +1,12 @@
 import { YUPANA_URL } from '@config/config';
 import Yupana from '@images/yupana.png';
-import { useAmplitudeService } from '@shared/hooks';
 import { YupanaLogo } from '@shared/svg';
 
 import styles from './advertising.module.scss';
+import { useAdvertisingViewModel } from './use-advertising.vm';
 
 export const Advertising = () => {
-  const { log } = useAmplitudeService();
+  const { handleLogEvent } = useAdvertisingViewModel();
 
   return (
     <a
@@ -15,7 +15,7 @@ export const Advertising = () => {
       className={styles.container}
       style={{ background: `url(${Yupana})` }}
       rel="noreferrer"
-      onClick={() => log('CLICK_YUPANA_BANNER')}
+      onClick={handleLogEvent}
     >
       <div className={styles.yupanaLogoContainer}>
         <YupanaLogo className={styles.yupanaLogo} />
