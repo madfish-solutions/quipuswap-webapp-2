1563a8c061087ea1392b67f064825a056d57506d
Inokentii Mazhara <keshan3262@gmail.com>
2022-10-31 18:03:55 +0200

    Fix adding new liquidity

--- src/modules/liquidity/pages/cpmm-item/components/forms/add-liq-form/use-dex-two-add-liq-form.vm.ts
+++ src/modules/liquidity/pages/cpmm-item/components/forms/add-liq-form/use-dex-two-add-liq-form.vm.ts
@@ -10,6 +10,7 @@ import {
   calculateShares,
   defined,
   getInputsAmountFormFormikValues,
+  getLastElementFromArray,
   isEqual,
   isTezosToken,
   numberAsString,
@@ -44,7 +45,12 @@ export const useDexTwoAddLiqFormViewModel = () => {
     actions.setSubmitting(true);
 
     const candidate = values[Input.THIRD_INPUT];
-    const inputAmounts = getInputsAmountFormFormikValues(values);
+    const inputAmounts = getInputsAmountFormFormikValues(values, ([aKey], [bKey]) => {
+      const aValue = Number(getLastElementFromArray(aKey.split('-')));
+      const bValue = Number(getLastElementFromArray(bKey.split('-')));
+
+      return aValue - bValue;
+    });
 
     if (poolIsEmpty) {
       await createNewLiquidityPool(
