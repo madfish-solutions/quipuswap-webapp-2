3e3c1e62613675d3063e6c8159327f91c2ea4869
Inokentii Mazhara <keshan3262@gmail.com>
2022-11-02 17:36:42 +0200

    Add a current baker to Dex 2.0 pool details

--- src/translation/locales/en/liquidity.ts
+++ src/translation/locales/en/liquidity.ts
@@ -70,5 +70,8 @@ export const liquidity = {
   tooltipQuipu: 'QuipuSwap',
   tooltipTezotopia: 'Tezotopia',
   tooltipBTC: 'BTC-based',
-  tooltipDexTwo: 'Dex 2.0 Pools'
+  tooltipDexTwo: 'Dex 2.0 Pools',
+  currentBaker: 'Current baker',
+  currentBakerTooltip:
+    'A current baker elected by a simple majority of votes. All TEZ in the pool are delegated for him.'
 } as const;
