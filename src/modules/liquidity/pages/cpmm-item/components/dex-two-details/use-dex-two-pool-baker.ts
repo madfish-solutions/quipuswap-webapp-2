555797021485b729c885bfb9b746998545f9380c
Inokentii Mazhara <keshan3262@gmail.com>
2022-11-02 18:26:09 +0200

    Handle Dex 2.0 pools without bucket contracts better

--- src/modules/liquidity/pages/cpmm-item/components/dex-two-details/use-dex-two-pool-baker.ts
+++ src/modules/liquidity/pages/cpmm-item/components/dex-two-details/use-dex-two-pool-baker.ts
@@ -7,27 +7,32 @@ import { useLiquidityItemStore } from '@modules/liquidity/hooks';
 import { useBakers } from '@providers/dapp-bakers';
 import { useTezos } from '@providers/use-dapp';
 import { getStorageInfo } from '@shared/dapp';
-import { defined, isExist } from '@shared/helpers';
+import { isExist } from '@shared/helpers';
 import { Optional } from '@shared/types';
 
-import { BucketStorage, DexTwoContractStorage } from './storage.types';
+import { BucketContractStorage, DexTwoContractStorage } from './storages.types';
 
 export const useDexTwoPoolBaker = () => {
   const { data: bakers, loading: bakersLoading } = useBakers();
   const liquidityItemStore = useLiquidityItemStore();
   const tezos = useTezos();
-  const poolAddress = liquidityItemStore.item?.contractAddress;
-  const poolId = liquidityItemStore.item?.id;
+  const dexTwoPool = liquidityItemStore?.item;
 
   const getDexTwoPoolBakerAddress = useCallback(
-    async (_: string, dexTwoPoolAddress: Optional<string>, dexTwoPoolId: Optional<BigNumber>) => {
-      if (!isExist(dexTwoPoolAddress) || !isExist(tezos) || !isExist(dexTwoPoolId)) {
+    async (_: string, dexTwoContractAddress: Optional<string>, dexTwoPoolId: Optional<BigNumber>) => {
+      if (!isExist(dexTwoContractAddress) || !isExist(tezos) || !isExist(dexTwoPoolId)) {
         return null;
       }
 
-      const { storage: dexTwoPoolStorage } = await getStorageInfo<DexTwoContractStorage>(tezos, dexTwoPoolAddress);
-      const pair = defined(await dexTwoPoolStorage.pairs.get(dexTwoPoolId), 'Pair was not found in storage');
-      const bucketStorage = await getStorageInfo<BucketStorage>(tezos, pair.bucket);
+      const { storage: dexTwoPoolStorage } = await getStorageInfo<DexTwoContractStorage>(tezos, dexTwoContractAddress);
+      const pair = await dexTwoPoolStorage.pairs.get(dexTwoPoolId);
+      const bucketAddress = pair?.bucket;
+
+      if (!isExist(bucketAddress)) {
+        return null;
+      }
+
+      const bucketStorage = await getStorageInfo<BucketContractStorage>(tezos, bucketAddress);
 
       return bucketStorage.current_delegated;
     },
@@ -35,7 +40,7 @@ export const useDexTwoPoolBaker = () => {
   );
 
   const { data: dexTwoPoolBakerAddress, isValidating: dexTwoPoolBakerAddressLoading } = useSWR(
-    ['dex-two-pool-baker-address', poolAddress, poolId],
+    ['dex-two-pool-baker-address', dexTwoPool?.contractAddress, dexTwoPool?.id],
     getDexTwoPoolBakerAddress
   );
 
