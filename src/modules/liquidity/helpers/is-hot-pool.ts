4ee0c0419a99a20b51045bcacd28a083e444e0e3
Artem Koziar <koziar.artem@gmail.com>
2022-11-03 16:27:14 +0200

    Sort HotPools

--- src/modules/liquidity/helpers/is-hot-pool.ts
+++
@@ -1,7 +0,0 @@
-import { BigNumber } from 'bignumber.js';
-
-import { HOT_POOLS } from '@config/config';
-
-export const isHotPool = (id: BigNumber, type: string) => {
-  return HOT_POOLS.some(item => item.id === id.toString() && item.type === type);
-};
