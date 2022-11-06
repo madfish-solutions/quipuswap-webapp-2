4c7c8f20044c0f9e228d7bdee346bcd7ca42be3b
Digberi <orkepuma@gmail.com>
2022-11-06 16:55:00 +0200

    *

--- src/shared/model-builder/loading-error-data.decorator.ts
+++ src/shared/model-builder/loading-error-data.decorator.ts
@@ -1,8 +1,43 @@
+import { LoadingErrorDataProposal } from '@shared/store';
+
 import { LED_METADATA_KEY } from './led-metadata-key';
-import { LedDecoratorMetadata, LedMetadataValue } from './types';
+import { LedDecoratorMetadata, LedDecoratorMetadataProposal, LedMetadataValue } from './types';
+
+//#endregion proposal
 
 export function Led({ default: defaultData, loader, model }: LedDecoratorMetadata): PropertyDecorator {
   return function (target: object, propertyKey: string | symbol): void {
+    const pervValues = Reflect.getOwnMetadata<Array<LedMetadataValue>>(LED_METADATA_KEY, target) ?? [];
+    Reflect.defineMetadata(
+      LED_METADATA_KEY,
+      [...pervValues, { propertyKey, default: defaultData, loader, model }],
+      target
+    );
+  };
+}
+
+//#region proposal
+type Target<
+  K extends string,
+  ModelType extends object = object,
+  Data extends Nullable<object> = object,
+  LoaderArgs extends unknown[] = unknown[],
+  RawData extends object = object
+> = {
+  [A in K]: LoadingErrorDataProposal<ModelType, Data, LoaderArgs, RawData>;
+};
+
+export function LedProposal<
+  ModelType extends object = object,
+  Data extends Nullable<object> = object,
+  LoaderArgs extends unknown[] = unknown[],
+  RawData extends object = object
+>({ default: defaultData, loader, model }: LedDecoratorMetadataProposal<ModelType, Data, LoaderArgs, RawData>) {
+  // eslint-disable-next-line sonarjs/no-identical-functions
+  return function <K extends string, C extends Target<K, ModelType, Data, LoaderArgs, RawData>>(
+    target: C,
+    propertyKey: K
+  ): void {
     const pervValues = Reflect.getOwnMetadata<Array<LedMetadataValue>>(LED_METADATA_KEY, target) ?? [];
     Reflect.defineMetadata(
       LED_METADATA_KEY,
--- src/shared/store/utils/loading-error-data.store.ts
+++ src/shared/store/utils/loading-error-data.store.ts
@@ -1,10 +1,103 @@
+/* eslint-disable sonarjs/no-identical-functions */
 import { action, computed, makeObservable, observable } from 'mobx';
 
 import { mapperReader, MapperConfig } from '@shared/model-builder';
 import { Constructable } from '@shared/types';
 
+//#region proposal
+export class LoadingErrorDataProposal<
+  ModelType extends object,
+  //TODO: remove Nullable
+  Default extends Nullable<object> = object,
+  LoaderArgs extends unknown[] = unknown[],
+  RawData extends object = object
+> {
+  rawData: RawData | undefined;
+
+  isInitialized = false;
+  isLoading = false;
+  error: Error | string | null = null;
+  model: ModelType | Default;
+
+  constructor(
+    private defaultData: Default,
+    private getDate: (...args: LoaderArgs) => Promise<RawData>,
+    private mappingConfig: MapperConfig,
+    private modelRef: Constructable<ModelType>
+  ) {
+    this.model = defaultData as unknown as ModelType;
+
+    makeObservable(this, {
+      rawData: observable,
+      isInitialized: observable,
+      isLoading: observable,
+      error: observable,
+      model: observable,
+
+      setRawData: action,
+      startLoading: action,
+      finishLoading: action,
+      setError: action,
+
+      isReady: computed
+    });
+  }
+
+  get isReady() {
+    return !this.isLoading && this.isInitialized;
+  }
+
+  resetData() {
+    this.model = this.defaultData;
+  }
+
+  setRawData(rawData: RawData) {
+    this.error = null;
+    this.rawData = rawData;
+    const data = mapperReader(rawData, this.mappingConfig) as unknown as Default;
+
+    this.model = new this.modelRef(data);
+  }
+
+  setError(error: Error | string) {
+    this.error = error;
+    this.rawData = undefined;
+    this.model = this.defaultData;
+  }
+
+  startLoading() {
+    this.isLoading = true;
+    this.isInitialized = true;
+  }
+
+  finishLoading() {
+    this.isLoading = false;
+  }
+
+  async load(...args: LoaderArgs) {
+    try {
+      this.startLoading();
+      const rawData = await this.getDate(...args);
+      this.setRawData(rawData);
+    } catch (error) {
+      this.setError(error as Error);
+
+      throw error;
+    } finally {
+      this.finishLoading();
+    }
+
+    return undefined;
+  }
+}
+//#endregion proposal
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export class LoadingErrorData<ModelType extends object, Default = any, RawData = any> {
+export class LoadingErrorData<
+  ModelType extends object,
+  Default extends Nullable<object> = object,
+  RawData extends object = object
+> {
   rawData: RawData | undefined;
 
   isInitialized = false;
