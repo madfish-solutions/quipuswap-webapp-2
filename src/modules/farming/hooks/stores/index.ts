0fc81d1669146e1a24ea8574c730abc310301732
Artem Koziar <koziar.artem@gmail.com>
2022-11-07 12:08:04 +0200

    QUIPU-567: Refactor: Add FarmingListStatsStore

--- src/modules/farming/hooks/stores/index.ts
+++ src/modules/farming/hooks/stores/index.ts
@@ -4,3 +4,4 @@ export * from './use-farming-list-store';
 export * from './use-farming-list-common.store';
 export * from './use-farming-youves-item-store';
 export * from './use-harvest-and-roll-store';
+export * from './use-farming-list-stats-store';
