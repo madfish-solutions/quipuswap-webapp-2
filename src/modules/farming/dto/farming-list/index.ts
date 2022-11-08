bfccc2d8a1581b809b241228b9290152e51aa882
Artem Koziar <koziar.artem@gmail.com>
2022-11-08 13:17:14 +0200

    QUIPU-567: Refactor V4: Move DTOs

--- src/modules/farming/dto/farming-list/index.ts
+++ src/modules/farming/dto/farming-list/index.ts
@@ -1,6 +1,2 @@
-export * from './farming-list-stats.dto';
-export * from './farming-list-stats-response.dto';
 export * from './farming-list-balances.dto';
-export * from './farming-list-user-info.dto';
-export * from './farming-list-user-info-response.dto';
 export * from './farming-list-response.dto';
