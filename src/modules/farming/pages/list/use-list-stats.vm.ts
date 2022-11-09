22d4a5488a789c3af4d1dc663129aee0f99cd30e
Dmytro <herkodmytro@gmail.com>
2022-11-09 15:14:39 +0200

    QUIPU-605 Added maxApr to farm stast

--- src/modules/farming/pages/list/use-list-stats.vm.ts
+++ src/modules/farming/pages/list/use-list-stats.vm.ts
@@ -26,10 +26,11 @@ export const useListStatsViewModel = () => {
       testId: FarmingListStatsDTA.TOTAL_PENDING_REWARD
     },
     {
-      title: t('farm|totalClaimedReward'),
+      title: t('farm|maxApr'),
       tooltip: t('farm|totalClaimedRewardTooltip'),
-      amount: amounts?.totalClaimedReward,
-      testId: FarmingListStatsDTA.TOTAL_CLAIMED_REWARD
+      amount: amounts?.maxApr,
+      testId: FarmingListStatsDTA.MAX_APR,
+      currency: '%'
     }
   ];
 
