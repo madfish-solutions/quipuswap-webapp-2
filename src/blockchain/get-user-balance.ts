de029c9752c9ca24ab7047977f91b31bcca89f99
Inokentii Mazhara <keshan3262@gmail.com>
2022-11-11 12:24:53 +0200

    Revert getUserBalance function changes

--- src/blockchain/get-user-balance.ts
+++ src/blockchain/get-user-balance.ts
@@ -3,47 +3,31 @@ import BigNumber from 'bignumber.js';
 import memoizee from 'memoizee';
 
 import { getReadOnlyTezos } from '@shared/dapp';
-import { Nullable, Standard, TokenId } from '@shared/types';
+import { Standard, TokenId } from '@shared/types';
 
 const loadChainId = memoizee(async (tezos: TezosToolkit) => tezos.rpc.getChainId(), {
   normalizer: ([tezos]) => tezos.rpc.getRpcUrl()
 });
 
-export async function getUserBalance(
-  tezos: TezosToolkit,
-  account: string,
-  token: TokenId
-): Promise<Nullable<BigNumber>>;
-export async function getUserBalance(
+export const getUserBalance = async (
   tezos: TezosToolkit,
   account: string,
   contractAddress: string,
-  type: Standard,
-  tokenId?: number | string
-): Promise<Nullable<BigNumber>>;
-export async function getUserBalance(
-  tezos: TezosToolkit,
-  account: string,
-  tokenOrTokenAddress: string | TokenId,
   type: Standard = Standard.Fa12,
-  fa2TokenId?: number | string
-) {
-  const tokenAddress =
-    typeof tokenOrTokenAddress === 'string' ? tokenOrTokenAddress : tokenOrTokenAddress.contractAddress;
-  const tokenStandard = typeof tokenOrTokenAddress === 'string' ? type : tokenOrTokenAddress.type;
-  const tokenId = typeof tokenOrTokenAddress === 'string' ? fa2TokenId : tokenOrTokenAddress.fa2TokenId;
+  tokenId?: number | string
+) => {
   const newTezos = getReadOnlyTezos(tezos);
 
-  if (tokenAddress === 'tez') {
+  if (contractAddress === 'tez') {
     return newTezos.tz.getBalance(account);
   }
-  const contract = await newTezos.contract.at(tokenAddress);
+  const contract = await newTezos.contract.at(contractAddress);
 
   const chainId = (await loadChainId(newTezos)) as ChainIds;
 
   let nat: BigNumber | undefined;
 
-  if (tokenStandard === Standard.Fa2) {
+  if (type === Standard.Fa2) {
     try {
       const response = await contract.views.balance_of([{ owner: account, token_id: tokenId }]).read(chainId);
       nat = response[0].balance;
@@ -63,7 +47,7 @@ export async function getUserBalance(
   }
 
   return nat;
-}
+};
 
 export const getUserTokenBalance = async (tezos: TezosToolkit, account: string, token: TokenId) =>
   await getUserBalance(tezos, account, token.contractAddress, token.type, token.fa2TokenId);
