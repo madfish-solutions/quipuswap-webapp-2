af747089b8fcf251efaed1c0381d5f33ce6458d0
Artem <violence044@gmail.com>
2022-11-15 23:04:43 +0200

    QUIPU-603 fix <RewardInfo /> issue

--- src/modules/farming/pages/list/components/farming-rewards-list/farming-rewards-list.tsx
+++ src/modules/farming/pages/list/components/farming-rewards-list/farming-rewards-list.tsx
@@ -2,10 +2,14 @@ import { FC } from 'react';
 
 import { observer } from 'mobx-react-lite';
 
+import { DOLLAR } from '@config/constants';
 import { useFarmingListRewardsStore } from '@modules/farming/hooks';
+import { DetailsCardCell, StateCurrencyAmount } from '@shared/components';
+import { RewardInfo } from '@shared/structures';
+import { i18n } from '@translation';
 
-import { RewardInfo } from '../reward-info';
 import { RewardTokensList } from '../reward-tokens-list';
+import styles from './farming-rewards-list.module.scss';
 import { useFarmingRewardsListViewModel } from './use-farming-rewards-list.vm';
 
 export const FarmingRewardsList: FC = observer(() => {
@@ -15,15 +19,34 @@ export const FarmingRewardsList: FC = observer(() => {
 
   return (
     <RewardInfo
+      containerClassName={styles.containerRewardInfo}
+      userInfoContainerClassName={styles.userInfoContainer}
+      childrenContainerClassName={styles.childrenRewardInfo}
+      buttonContainerClassName={styles.buttonRewardInfo}
+      viewDetailsButtonClassName={styles.viewDetailsButton}
       claimablePendingRewards={claimablePendingRewardsInUsd}
       totalPendingRewards={totalPendingRewardsInUsd}
-      userTotalDepositInfo={userTotalDepositInfo}
       onButtonClick={handleHarvestAll}
       rewardTooltip={rewardsTooltipTranslation}
       buttonText={harvestAllTranslation}
       currency="$"
       buttonUp
       details={<RewardTokensList />}
-    />
+    >
+      <DetailsCardCell
+        className={styles.totalDeposit}
+        cellName={i18n.t('farm|yourTotalDeposit')}
+        data-test-id="yourTotalDeposit"
+      >
+        <StateCurrencyAmount
+          amount={userTotalDepositInfo.totalDepositAmount}
+          labelSize="large"
+          currency={DOLLAR}
+          isLoading={userTotalDepositInfo.totalDepositLoading}
+          isError={Boolean(userTotalDepositInfo.totalDepositError)}
+          isLeftCurrency
+        />
+      </DetailsCardCell>
+    </RewardInfo>
   );
 });
