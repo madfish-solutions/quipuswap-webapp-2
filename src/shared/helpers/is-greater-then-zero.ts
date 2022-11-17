7e1d904b9fd3313764c96e1d62dabbe506a738ed
Inokentii Mazhara <keshan3262@gmail.com>
2022-11-17 11:22:51 +0200

    Revert changes to isGreaterThanZero function

--- src/shared/helpers/is-greater-then-zero.ts
+++ src/shared/helpers/is-greater-then-zero.ts
@@ -3,8 +3,6 @@ import { BigNumber } from 'bignumber.js';
 import { ZERO_AMOUNT } from '@config/constants';
 
 import { Optional } from '../types';
-import { isExist } from './type-checks';
 
-export const isGreaterThanZero = (value: Optional<BigNumber>) =>
-  isExist(value) ? value.isGreaterThan(ZERO_AMOUNT) : false;
+export const isGreaterThanZero = (value: BigNumber) => value.isGreaterThan(ZERO_AMOUNT);
 export const isOptionalGreaterThanZero = (value: Optional<BigNumber>) => value && isGreaterThanZero(value);
