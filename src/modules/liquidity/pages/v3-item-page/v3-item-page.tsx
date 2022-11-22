6c633eaa2d305cee86ece9501c4e6d439b5e1ea5
Artem Koziar <koziar.artem@gmail.com>
2022-11-22 11:28:23 +0200

    QUIPU-671: Add PageTitleContainer

--- src/modules/liquidity/pages/v3-item-page/v3-item-page.tsx
+++ src/modules/liquidity/pages/v3-item-page/v3-item-page.tsx
@@ -1,19 +1,15 @@
 import { FC } from 'react';
 
-import { observer } from 'mobx-react-lite';
+import { StickyBlock } from '@shared/components';
 
-import { PageTitle, StickyBlock } from '@shared/components';
+import { PageTitleContainer } from './components';
 
-import { useV3ItemPageViewModel } from './use-v3-item-page.vm';
-
-export const V3ItemPage: FC = observer(() => {
-  const { title } = useV3ItemPageViewModel();
-
+export const V3ItemPage: FC = () => {
   return (
     <>
-      <PageTitle data-test-id="v3LiqTitle">{title}</PageTitle>
+      <PageTitleContainer />
 
       <StickyBlock>body</StickyBlock>
     </>
   );
-});
+};
