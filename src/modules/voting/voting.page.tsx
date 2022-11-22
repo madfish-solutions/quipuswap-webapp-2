54d54ee82b1738deda0dd60d711f36fc59a15792
Inokentii Mazhara <keshan3262@gmail.com>
2022-11-22 15:51:52 +0200

    Implement redirections to 404 page for 'Voting' page

--- src/modules/voting/voting.page.tsx
+++ src/modules/voting/voting.page.tsx
@@ -1,27 +1,10 @@
 import { FC } from 'react';
 
-import { Route } from 'react-router-dom';
-
-import { NOT_FOUND_ROUTE_NAME } from '@config/constants';
-import { PageNotFoundPage } from '@modules/errors';
-import { SentryRoutes } from '@shared/services';
-
 import { VotingProvider } from './helpers/voting.provider';
 import { VotingInner } from './voting-inner';
 
-interface VotingProps {
-  className?: string;
-}
-
-export const VotingPage: FC<VotingProps> = ({ className }) => {
-  return (
-    <VotingProvider>
-      <SentryRoutes>
-        <Route path="/" element={<VotingInner className={className} />} />
-        <Route path={`/${NOT_FOUND_ROUTE_NAME}`} element={<PageNotFoundPage />} />
-        <Route path={`/:method/${NOT_FOUND_ROUTE_NAME}`} element={<PageNotFoundPage />} />
-        <Route path=":method/:fromTo" element={<VotingInner className={className} />} />
-      </SentryRoutes>
-    </VotingProvider>
-  );
-};
+export const VotingPage: FC = () => (
+  <VotingProvider>
+    <VotingInner />
+  </VotingProvider>
+);
--- src/shared/helpers/handle-search-token.ts
+++ src/shared/helpers/handle-search-token.ts
@@ -2,10 +2,11 @@ import { Dispatch, SetStateAction } from 'react';
 
 import { TezosToolkit } from '@taquito/taquito';
 
-import { TEZOS_TOKEN } from '@config/tokens';
-import { localSearchToken, TokenWithRequiredNetwork, localSearchSortSymbol } from '@shared/helpers';
+import { isNull, localSearchToken, TokenWithRequiredNetwork, localSearchSortSymbol } from '@shared/helpers';
 import { isTokenEqual } from '@shared/helpers/tokens/is-token-equal';
 import { Nullable, QSNetwork, Token, TokenPair } from '@shared/types';
+
+import { NotFoundPairError } from './not-found-pair-error.error';
 
 interface SearchTokenType {
   tokens: Token[];
@@ -37,7 +38,7 @@ export const handleSearchToken = async ({
 SearchTokenType) => {
   setInitialLoad(true);
   setUrlLoaded(false);
-  const searchPart = async (str: string | string[]): Promise<Token> => {
+  const searchPart = async (str: string | string[]): Promise<Nullable<Token>> => {
     const strStr = Array.isArray(str) ? str[0] : str;
     const inputValue = strStr.split('_')[0];
     const inputToken = strStr.split('_')[1] ?? 0;
@@ -45,36 +46,23 @@ SearchTokenType) => {
       .sort((a, b) => localSearchSortSymbol(b, a, inputValue, inputToken))
       .filter(token => localSearchToken(token as TokenWithRequiredNetwork, network, inputValue, +inputToken));
     if (isTokens.length === 0) {
-      return searchCustomToken(inputValue, +inputToken, true).then(x => {
-        if (x) {
-          return x;
-        }
-
-        return TEZOS_TOKEN;
-      });
+      return searchCustomToken(inputValue, +inputToken, true);
     }
 
     return isTokens[0];
   };
-  let res: Token[] = [];
-  if (from) {
-    if (to) {
-      const resTo = await searchPart(to);
-      res = [resTo];
-    }
-    let resFrom;
-    if (!fixTokenFrom) {
-      resFrom = await searchPart(from);
-    } else {
-      resFrom = fixTokenFrom;
-    }
-    res = [resFrom, ...res];
-  }
+  const tokenFrom = fixTokenFrom ?? (await searchPart(from));
+  const tokenTo = await searchPart(to);
   setUrlLoaded(true);
-  if (!isTokenEqual(res[0], res[1])) {
-    setTokens(res);
+
+  if (isNull(tokenFrom) || isNull(tokenTo)) {
+    throw new NotFoundPairError(from, to);
+  }
+
+  if (!isTokenEqual(tokenFrom, tokenTo)) {
+    setTokens([tokenFrom, tokenTo]);
     if (setTokenPair && tezos) {
-      setTokenPair({ token1: res[0], token2: res[1] } as TokenPair);
+      setTokenPair({ token1: tokenFrom, token2: tokenTo });
     }
   }
 };
