dabb763d551ff91cd893d23653152ee2190308dd
Dmytro <herkodmytro@gmail.com>
2022-11-28 13:54:33 +0200

    QUIPU-673 Deleted unnecessary div

--- src/modules/liquidity/pages/v3-item-page/create-new-position.page.tsx
+++ src/modules/liquidity/pages/v3-item-page/create-new-position.page.tsx
@@ -6,8 +6,8 @@ export const CreateNewPositionPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
+    <>
       <PageTitleContainer dataTestId="v3LiqCreate" titleText={t('liquidity|createPosition')} />
-    </div>
+    </>
   );
 };
