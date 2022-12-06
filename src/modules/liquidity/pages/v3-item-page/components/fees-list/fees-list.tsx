814a8d26a9e2bce92700a585458e957748a013ce
Artem <violence044@gmail.com>
2022-12-06 11:05:42 +0200

    QUIPU-660 Add UI change architecture

---
+++ src/modules/liquidity/pages/v3-item-page/components/fees-list/fees-list.tsx
@@ -0,0 +1,71 @@
+import { FC } from 'react';
+
+import { BigNumber } from 'bignumber.js';
+import { observer } from 'mobx-react-lite';
+
+import { DOLLAR, USD_DECIMALS } from '@config/constants';
+import { DetailsCardCell, StateCurrencyAmount } from '@shared/components';
+import { RewardInfo } from '@shared/structures';
+import { Nullable } from '@shared/types';
+
+import { FeeTokensList } from '../fee-tokens-list';
+import styles from './fees-list.module.scss';
+
+interface TotalDepositInfo {
+  totalDepositAmount: BigNumber;
+  totalDepositLoading: boolean;
+  totalDepositError: string | Error | null;
+}
+
+interface Props {
+  claimablePendingRewards: Nullable<BigNumber>;
+  onButtonClick: () => void;
+  userTotalDepositInfo: TotalDepositInfo;
+  isUserTotalDepositExist: boolean;
+  translation: Record<string, string>;
+}
+
+export const FeesList: FC<Props> = observer(
+  ({ claimablePendingRewards, onButtonClick, userTotalDepositInfo, isUserTotalDepositExist, translation }) => {
+    const { rewardsTooltipTranslation, harvestAllTranslation, totalFeesTranslation, totalDepositTranslation } =
+      translation;
+    const { totalDepositAmount, totalDepositLoading, totalDepositError } = userTotalDepositInfo;
+
+    return (
+      <RewardInfo
+        containerClassName={styles.containerFeeInfo}
+        userInfoContainerClassName={styles.userInfoContainer}
+        childrenContainerClassName={styles.childrenFeeInfo}
+        buttonContainerClassName={styles.buttonFeeInfo}
+        viewDetailsButtonClassName={styles.viewDetailsButton}
+        claimablePendingRewards={claimablePendingRewards}
+        onButtonClick={onButtonClick}
+        rewardTooltip={rewardsTooltipTranslation}
+        buttonText={harvestAllTranslation}
+        currency="$"
+        buttonUp
+        details={<FeeTokensList />}
+        rewardsLabel={totalFeesTranslation}
+      >
+        {isUserTotalDepositExist && (
+          <DetailsCardCell
+            className={styles.totalDeposit}
+            cellNameClassName={styles.totalDepositCellName}
+            cellName={totalDepositTranslation}
+            data-test-id="totalDeposit"
+          >
+            <StateCurrencyAmount
+              amount={totalDepositAmount}
+              amountDecimals={USD_DECIMALS}
+              currency={DOLLAR}
+              labelSize="large"
+              isLoading={totalDepositLoading}
+              isError={Boolean(totalDepositError)}
+              isLeftCurrency
+            />
+          </DetailsCardCell>
+        )}
+      </RewardInfo>
+    );
+  }
+);
--- src/modules/liquidity/pages/v3-item-page/use-v3-item-page.vm.ts
+++ src/modules/liquidity/pages/v3-item-page/use-v3-item-page.vm.ts
@@ -1,11 +1,21 @@
 import { useEffect } from 'react';
 
+import { useLiquidityV3ItemStore, useLiquidityV3ItemTokens } from '@modules/liquidity/hooks';
+import { isNull } from '@shared/helpers';
+
 import { useGetLiquidityV3Item } from '../../../liquidity/hooks/loaders/use-get-liquidity-v3-item';
 
 export const useV3ItemPageViewModel = () => {
+  const v3ItemStore = useLiquidityV3ItemStore();
   const { getLiquidityV3Item } = useGetLiquidityV3Item();
+  const { tokenX, tokenY } = useLiquidityV3ItemTokens();
+
+  const isLoading = v3ItemStore.itemIsLoading || isNull(tokenX) || isNull(tokenY);
+  const error = v3ItemStore.error ?? v3ItemStore.contractBalanceStore.error;
 
   useEffect(() => {
     void getLiquidityV3Item();
   }, [getLiquidityV3Item]);
+
+  return { isLoading, error };
 };
