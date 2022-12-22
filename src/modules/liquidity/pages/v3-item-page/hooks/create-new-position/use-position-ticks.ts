523c2a1dd6896e2dcbd8abee4a06be72f1a44279
Inokentii Mazhara <keshan3262@gmail.com>
2022-12-22 13:04:54 +0200

    Additional minor refactoring

--- src/modules/liquidity/pages/v3-item-page/hooks/create-new-position/use-position-ticks.ts
+++ src/modules/liquidity/pages/v3-item-page/hooks/create-new-position/use-position-ticks.ts
@@ -3,16 +3,16 @@ import { useMemo } from 'react';
 import BigNumber from 'bignumber.js';
 import { useFormik } from 'formik';
 
-import { useLiquidityV3PoolStore, useV3PoolPriceDecimals } from '@modules/liquidity/hooks';
+import { useV3PoolPriceDecimals } from '@modules/liquidity/hooks';
 import { toAtomic } from '@shared/helpers';
 
 import { calculateTicks } from '../../helpers';
 import { CreatePositionInput } from '../../types/create-position-form';
+import { useTickSpacing } from './use-tick-spacing';
 
 export const usePositionTicks = (formik: ReturnType<typeof useFormik>) => {
   const priceDecimals = useV3PoolPriceDecimals();
-  const poolStore = useLiquidityV3PoolStore();
-  const tickSpacing = poolStore.item?.storage.constants.tick_spacing.toNumber();
+  const tickSpacing = useTickSpacing();
 
   const { [CreatePositionInput.MIN_PRICE]: rawMinPrice, [CreatePositionInput.MAX_PRICE]: rawMaxPrice } = formik.values;
 
