74cad3160c371f2c77318f29fb9fd30334373f3b
Digberi <orkepuma@gmail.com>
2022-12-29 16:02:25 +0200

    QUIPU-679 remove mocks and use create pool function

--- src/modules/liquidity/pages/v3-item-page/use-create-new-pool-page.vm.ts
+++ src/modules/liquidity/pages/v3-item-page/use-create-new-pool-page.vm.ts
@@ -5,68 +5,38 @@ import { V3LiquidityPoolApi } from '@modules/liquidity/api';
 import { useAccountPkh, useTezos } from '@providers/use-dapp';
 import { isNull, toAtomic, toFraction } from '@shared/helpers';
 import { useAmplitudeService } from '@shared/hooks';
-import { Standard, Token } from '@shared/types';
+import { Token } from '@shared/types';
 import { useConfirmOperation, useToasts } from '@shared/utils';
 
 import { calculateTickIndex, calculateTokenPriceDecimals } from './helpers';
 import { getCreateV3PoolLogData } from './helpers/get-create-v3-pool-log-data';
 
-const MOCK_PRICE = new BigNumber('0.044');
-const MOCK_FEE_PERCENTAGE = new BigNumber('3');
-const MOCK_TOKEN_X = {
-  type: Standard.Fa12,
-  metadata: {
-    decimals: 18,
-    name: 'Test Kolibri USD',
-    symbol: 'kUSD2',
-    thumbnailUri: 'https://kolibri-data.s3.amazonaws.com/logo.png',
-    categories: ['stablecoin']
-  },
-  isWhitelisted: false,
-  contractAddress: 'KT18uv7PtGedfudtG7QwFTrDBRjdLe5qj2my'
-};
-const MOCK_TOKEN_Y: Token = {
-  type: Standard.Fa2,
-  metadata: {
-    decimals: 6,
-    name: 'Grape Token',
-    symbol: 'GRP',
-    thumbnailUri: 'https://pngicon.ru/file/uploads/vinogradnyye-grozdi.png',
-    categories: []
-  },
-  isWhitelisted: false,
-  contractAddress: 'KT1Q1qqJAzzxzGzQB6RmxnAKgxttf7Hntceg'
-};
-
 const INVERSION_DIVIDEND = 1;
 
-export const useCreateNewPoolPageViewModel = () => {
+export const useDoCreateV3Pool = () => {
   const tezos = useTezos();
   const accountPkh = useAccountPkh();
   const confirmOperation = useConfirmOperation();
   const { showErrorToast } = useToasts();
   const { log } = useAmplitudeService();
 
-  const handleCreatePoolClick = async () => {
+  const doCreatePool = async (feeRate: BigNumber, token0: Token, token1: Token, initialPrice: BigNumber) => {
     if (isNull(tezos) || isNull(accountPkh)) {
       return;
     }
 
-    const logData = getCreateV3PoolLogData(accountPkh, MOCK_TOKEN_X, MOCK_TOKEN_Y, MOCK_PRICE, MOCK_FEE_PERCENTAGE);
+    const logData = getCreateV3PoolLogData(accountPkh, token0, token1, initialPrice, feeRate);
 
     try {
       log('CREATE_V3_POOL', logData);
       const operation = await V3LiquidityPoolApi.createPool(
         tezos,
-        MOCK_TOKEN_X,
-        MOCK_TOKEN_Y,
+        token0,
+        token1,
         calculateTickIndex(
-          toAtomic(
-            new BigNumber(INVERSION_DIVIDEND).div(MOCK_PRICE),
-            calculateTokenPriceDecimals(MOCK_TOKEN_X, MOCK_TOKEN_Y)
-          )
+          toAtomic(new BigNumber(INVERSION_DIVIDEND).div(initialPrice), calculateTokenPriceDecimals(token0, token1))
         ),
-        toFraction(MOCK_FEE_PERCENTAGE).multipliedBy(FEE_BASE_POINTS_PRECISION)
+        toFraction(feeRate).multipliedBy(FEE_BASE_POINTS_PRECISION)
       );
 
       await confirmOperation(operation.opHash, { message: 'Pool was successfully created' });
@@ -77,5 +47,5 @@ export const useCreateNewPoolPageViewModel = () => {
     }
   };
 
-  return { handleCreatePoolClick };
+  return { doCreatePool };
 };
