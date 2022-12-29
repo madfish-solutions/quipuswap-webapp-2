cd6f7b863a30abee4491f7c606d184e943855537
Digberi <orkepuma@gmail.com>
2022-12-29 17:08:56 +0200

    fix by pr comments

--- src/modules/liquidity/pages/v3-item-page/helpers/calculate-tick-index.ts
+++
@@ -1,6 +0,0 @@
-import BigNumber from 'bignumber.js';
-
-const tickMultiplierLn = 0.0001;
-
-export const calculateTickIndex = (price: BigNumber) =>
-  new BigNumber(Math.log(price.toNumber()) / tickMultiplierLn).integerValue(BigNumber.ROUND_FLOOR);
--- src/modules/liquidity/pages/v3-item-page/helpers/calculate-token-price-decimals.ts
+++
@@ -1,5 +0,0 @@
-import { getTokenDecimals } from '@shared/helpers';
-import { Nullable, Token } from '@shared/types';
-
-export const calculateTokenPriceDecimals = (tokenX: Nullable<Token>, tokenY: Nullable<Token>) =>
-  getTokenDecimals(tokenY) - getTokenDecimals(tokenX);
