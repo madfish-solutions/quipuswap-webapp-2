61c47cdf8046b0d68f6f1fd389ff5fcb50b34d33
Artem <violence044@gmail.com>
2023-01-06 12:46:05 +0200

    QUIPU-676 remove excess code

--- src/shared/helpers/get-user-balances.ts
+++
@@ -1,6 +0,0 @@
-import { useTokenBalance } from '@shared/hooks';
-import { Nullable, Token } from '@shared/types';
-
-export const getUserBalances = (tokens: Array<Nullable<Token>>) => {
-  return tokens.map(token => useTokenBalance(token) ?? null);
-};
