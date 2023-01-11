287573d6cf6dd08255244b75b07ca1ae725078a1
Inokentii Mazhara <keshan3262@gmail.com>
2023-01-11 14:14:17 +0200

    Implement immediate harvest in case reward in QUIPU is zero

---
+++ src/modules/farming/hooks/use-harvest-all.ts
@@ -0,0 +1,41 @@
+import { CoinSide, TokenToPlay } from '@modules/coinflip';
+import { useCoinflipStore } from '@modules/coinflip/hooks';
+import { useAmplitudeService } from '@shared/hooks';
+import { Nullable } from '@shared/types';
+
+import { useDoHarvestAll } from './blockchain';
+import { useHarvestAndRollStore } from './stores';
+
+export const useHarvestAll = () => {
+  const harvestAndRollStore = useHarvestAndRollStore();
+  const { doHarvestAll } = useDoHarvestAll();
+  const coinflipStore = useCoinflipStore();
+  const { log } = useAmplitudeService();
+
+  const onCoinSideSelect = (_coinSide: Nullable<CoinSide>) => {
+    harvestAndRollStore.setCoinSide(harvestAndRollStore.coinSide === _coinSide ? null : _coinSide);
+    harvestAndRollStore.setCoinSideError(null);
+  };
+
+  const onClose = () => {
+    onCoinSideSelect(null);
+    harvestAndRollStore.close();
+  };
+
+  const harvestAll = async () => {
+    harvestAndRollStore.startHarvestLoading();
+    coinflipStore.setPendingGameTokenToPlay(TokenToPlay.Quipu);
+    log('HARVEST_AND_ROLL_HARVEST_ALL_CLICK');
+
+    await doHarvestAll();
+    log('HARVEST_AND_ROLL_HARVEST_ALL_SUCCESS');
+    harvestAndRollStore.finishHarvestLoading();
+    onClose();
+  };
+
+  return {
+    onCoinSideSelect,
+    onClose,
+    harvestAll
+  };
+};
