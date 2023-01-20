dd0ee756a86ced1094e52a40d8717d479b42563f
Artem <violence044@gmail.com>
2023-01-20 14:25:06 +0200

    QUIPU-760 Redirect to new position after create

--- src/modules/liquidity/hooks/blockchain/use-v3-new-position.ts
+++ src/modules/liquidity/hooks/blockchain/use-v3-new-position.ts
@@ -1,5 +1,9 @@
 import { BigNumber } from 'bignumber.js';
+import { useNavigate } from 'react-router-dom';
 
+import { AppRootRoutes } from '@app.router';
+import { LAST_INDEX, SLASH } from '@config/constants';
+import { LiquidityRoutes, LiquidityTabs } from '@modules/liquidity/liquidity-routes.enum';
 import { useCurrentTick, useTickSpacing } from '@modules/liquidity/pages/v3-item-page/hooks';
 import { useRootStore } from '@providers/root-store-provider';
 import { useAccountPkh } from '@providers/use-dapp';
@@ -11,11 +15,14 @@ import { useTranslation } from '@translation';
 
 import { V3Positions } from '../../api/blockchain/v3-liquidity-pool-positions';
 import { useLiquidityV3ItemTokens, useV3PoolPriceDecimals } from '../helpers';
-import { useLiquidityV3PoolStore } from '../store';
+import { useGetLiquidityV3ItemWithPositions } from '../loaders';
+import { useLiquidityV3PoolStore, useLiquidityV3PositionsStore } from '../store';
 
 export const useV3NewPosition = () => {
   const { tezos } = useRootStore();
   const { showErrorToast } = useToasts();
+  const v3PositionsStore = useLiquidityV3PositionsStore();
+  const { getLiquidityV3ItemWithPositions } = useGetLiquidityV3ItemWithPositions();
   const confirmOperation = useConfirmOperation();
   const { t } = useTranslation();
   const {
@@ -27,6 +34,7 @@ export const useV3NewPosition = () => {
   const currentTick = useCurrentTick();
   const priceDecimals = useV3PoolPriceDecimals();
   const tickSpacing = useTickSpacing();
+  const navigate = useNavigate();
 
   const createNewV3Position = async (
     minPrice: BigNumber,
@@ -83,6 +91,13 @@ export const useV3NewPosition = () => {
         return;
       }
       await confirmOperation(operation.opHash, { message: t('liquidity|successfullyAdded') });
+      await getLiquidityV3ItemWithPositions();
+
+      navigate(
+        `${AppRootRoutes.Liquidity}${LiquidityRoutes.v3}${SLASH}${v3PositionsStore.poolId}/${
+          LiquidityTabs.add
+        }${SLASH}${v3PositionsStore.positions?.at(LAST_INDEX)?.id}`
+      );
       amplitudeService.logEvent('DEX_V3_NEW_POSITION_SUCCESS', logData);
     } catch (error) {
       showErrorToast(error as Error);
