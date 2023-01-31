6a0ae0057cd6b4094ddad8a52c7b0f070e84de4d
Inokentii Mazhara <keshan3262@gmail.com>
2023-01-31 19:46:46 +0200

    Add a placeholder for V3 pool creation guides

--- src/modules/liquidity/pages/v3-item-page/create-new-pool.page.tsx
+++ src/modules/liquidity/pages/v3-item-page/create-new-pool.page.tsx
@@ -1,7 +1,7 @@
 import { PageTitle, StickyBlock } from '@shared/components';
 import { useTranslation } from '@translation';
 
-import { CreatePoolForm } from './components';
+import { CreatePoolForm, Guides } from './components';
 
 export const CreateNewPoolPage = () => {
   const { t } = useTranslation();
@@ -11,6 +11,7 @@ export const CreateNewPoolPage = () => {
       <PageTitle data-test-id="v3LiqCreatePool">{t('liquidity|newPool')}</PageTitle>
       <StickyBlock>
         <CreatePoolForm />
+        <Guides />
       </StickyBlock>
     </>
   );
