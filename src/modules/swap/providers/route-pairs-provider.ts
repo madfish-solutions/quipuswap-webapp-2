7e13623fb6c235693d0f7ab38a04119e013d6e81
Inokentii Mazhara <keshan3262@gmail.com>
2023-02-06 13:48:22 +0200

    QUIPU-798 Implement making dynamic tokens whitelist

--- src/modules/swap/providers/route-pairs-provider.ts
+++ src/modules/swap/providers/route-pairs-provider.ts
@@ -1,10 +1,32 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import constate from 'constate';
-import { useAllRoutePairs } from 'swap-router-sdk';
+import { RouteDirectionEnum, useAllRoutePairs } from 'swap-router-sdk';
 import { RoutePair } from 'swap-router-sdk/dist/interface/route-pair.interface';
 
+import { TOKENS } from '@config/config';
+import { ZERO_AMOUNT_BN } from '@config/constants';
+import { QUIPU_TOKEN, TEZOS_TOKEN, WTEZ_TOKEN } from '@config/tokens';
+import { getTokenSlug, getUniqArray } from '@shared/helpers';
+import { mapBackendToken } from '@shared/mapping';
+import { Token } from '@shared/types';
+
 import { KNOWN_DEX_TYPES, TEZOS_DEXES_API_URL } from '../config';
+import { extractTokensPools } from '../utils/map-dex-pairs';
+
+const tokensMap = new Map<string, Token>(
+  TOKENS.tokens
+    .map((token): [string, Token] => {
+      const mappedToken = mapBackendToken(token);
+
+      return [getTokenSlug(mappedToken), mappedToken];
+    })
+    .concat([
+      [getTokenSlug(TEZOS_TOKEN), TEZOS_TOKEN],
+      [getTokenSlug(WTEZ_TOKEN), WTEZ_TOKEN],
+      [getTokenSlug(QUIPU_TOKEN), QUIPU_TOKEN]
+    ])
+);
 
 // TODO: replace it with store after route pairs become available not only from hook
 export const [RoutePairsProvider, useRoutePairs] = constate(() => {
@@ -27,5 +49,30 @@ export const [RoutePairsProvider, useRoutePairs] = constate(() => {
     }
   }, [updateRoutePairs, loading, dataIsStale]);
 
-  return { dataIsStale, routePairs: displayedRoutePairs, updateRoutePairs, loading };
+  const whitelistedPairs = useMemo(() => {
+    return getUniqArray(
+      filteredRoutePairs
+        .filter(pair => {
+          try {
+            extractTokensPools(
+              {
+                ...pair,
+                aTokenAmount: ZERO_AMOUNT_BN,
+                bTokenAmount: ZERO_AMOUNT_BN,
+                direction: RouteDirectionEnum.Direct
+              },
+              tokensMap
+            );
+
+            return true;
+          } catch {
+            return false;
+          }
+        })
+        .map(({ dexType, dexAddress, dexId }) => ({ dexType, dexAddress, dexId })),
+      ({ dexAddress, dexId }) => getTokenSlug({ contractAddress: dexAddress, fa2TokenId: dexId?.toNumber() })
+    );
+  }, [filteredRoutePairs]);
+
+  return { dataIsStale, routePairs: displayedRoutePairs, updateRoutePairs, loading, whitelistedPairs };
 });
