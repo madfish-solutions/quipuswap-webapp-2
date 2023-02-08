3e0f7022b26006a552dbce1e3b5ac61414a69a66
Inokentii Mazhara <keshan3262@gmail.com>
2023-02-08 13:34:51 +0200

    Fix sorting for TEZ token

--- src/modules/liquidity/pages/v3-item-page/helpers/sort-tokens.ts
+++ src/modules/liquidity/pages/v3-item-page/helpers/sort-tokens.ts
@@ -1,10 +1,18 @@
 import { SKIP, SWAP } from '@config/constants';
-import { isTokenFa12, isTokenFa2, sortAddresses } from '@shared/helpers';
+import { WTEZ_TOKEN } from '@config/tokens';
+import { isTezosToken, isTokenFa12, isTokenFa2, sortAddresses } from '@shared/helpers';
 import { Token } from '@shared/types';
 
 export const sortTokens = (tokenA: Token, tokenB: Token) => {
+  if (isTezosToken(tokenA)) {
+    tokenA = WTEZ_TOKEN;
+  }
+  if (isTezosToken(tokenB)) {
+    tokenB = WTEZ_TOKEN;
+  }
+
   if (isTokenFa12(tokenA) && isTokenFa12(tokenB)) {
-    return sortAddresses(tokenA.contractAddress, tokenB.contractAddress);
+    return -sortAddresses(tokenA.contractAddress, tokenB.contractAddress);
   }
 
   if (isTokenFa2(tokenA) && isTokenFa2(tokenB)) {
@@ -16,7 +24,7 @@ export const sortTokens = (tokenA: Token, tokenB: Token) => {
       return SWAP;
     }
 
-    return sortAddresses(tokenA.contractAddress, tokenB.contractAddress);
+    return -sortAddresses(tokenA.contractAddress, tokenB.contractAddress);
   }
 
   if (isTokenFa2(tokenA) && isTokenFa12(tokenB)) {
