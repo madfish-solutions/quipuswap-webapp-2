9f1d6f3bf8fdf04d965f2f68a7c6c1d4f000658b
Inokentii Mazhara <keshan3262@gmail.com>
2023-02-23 18:09:19 +0200

    Change tokens modal UI

--- src/shared/components/tokens-filter/tokens-filter.vm.ts
+++ src/shared/components/tokens-filter/tokens-filter.vm.ts
@@ -2,8 +2,10 @@ import { useCallback } from 'react';
 
 import { useLiquidityListFiltersStore } from '@modules/liquidity/hooks';
 import { useChooseTokens } from '@shared/modals/tokens-modal';
+import { useTranslation } from '@translation';
 
 export const useTokensFilterViewModel = () => {
+  const { t } = useTranslation();
   const { chooseTokens } = useChooseTokens();
   const liquidityListFiltersStore = useLiquidityListFiltersStore();
   const { tokens } = liquidityListFiltersStore;
@@ -13,11 +15,18 @@ export const useTokensFilterViewModel = () => {
       tokens,
       disabledTokens: [],
       min: 0,
-      max: 4
+      max: 4,
+      cancelButtonProps: {
+        children: t('common|cancel'),
+        theme: 'secondary'
+      },
+      confirmButtonProps: {
+        children: t('common|applyFilter')
+      }
     });
 
     liquidityListFiltersStore.setTokens(chosenTokens);
-  }, [chooseTokens, liquidityListFiltersStore, tokens]);
+  }, [chooseTokens, liquidityListFiltersStore, tokens, t]);
 
   return {
     tokens,
