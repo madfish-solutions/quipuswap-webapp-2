264d2e1745c8154f5fb0eb310334b1e9829c9c40
Inokentii Mazhara <keshan3262@gmail.com>
2023-02-24 11:46:24 +0200

    Add analytics for filters

--- src/modules/liquidity/pages/list/components/pool-type-filter/pool-type-filter.vm.ts
+++ src/modules/liquidity/pages/list/components/pool-type-filter/pool-type-filter.vm.ts
@@ -2,11 +2,13 @@ import { useMemo } from 'react';
 
 import { useLiquidityListFiltersStore } from '@modules/liquidity/hooks';
 import { PoolTypeOptionEnum } from '@modules/liquidity/interfaces';
+import { useAmplitudeService } from '@shared/hooks';
 import { useTranslation } from '@translation';
 
 export const usePoolTypeFilterViewModel = () => {
   const filtersStore = useLiquidityListFiltersStore();
   const { t } = useTranslation();
+  const { log } = useAmplitudeService();
 
   const options = useMemo(
     () => [
@@ -20,8 +22,11 @@ export const usePoolTypeFilterViewModel = () => {
   );
   const labels = useMemo(() => options.map(({ label }) => label), [options]);
 
-  const handlePoolTypeButtonClick = (poolTypeIndex: number) =>
-    filtersStore.setPoolTypeOption(options[poolTypeIndex].value);
+  const handlePoolTypeButtonClick = (poolTypeIndex: number) => {
+    const newPoolType = options[poolTypeIndex].value;
+    log('LIQUIDITY_POOL_TYPE_SELECTED', { poolType: newPoolType });
+    filtersStore.setPoolTypeOption(newPoolType);
+  };
   const activePoolTypeIndex = options.findIndex(({ value }) => value === filtersStore.poolTypeOption);
 
   return {
