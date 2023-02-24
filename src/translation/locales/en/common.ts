255169d001bfd73437fb3d25a767e0f614230571
Inokentii Mazhara <keshan3262@gmail.com>
2023-02-24 11:58:24 +0200

    Change tokens filter view for the case some tokens are selected

--- src/translation/locales/en/common.ts
+++ src/translation/locales/en/common.ts
@@ -166,6 +166,7 @@ export const common = {
     'Current RPC node {{currentRpcUrl}} is not responding. Click "Reconnect" to connect to {{nextRpcUrl}}.',
   Reconnect: 'Reconnect',
   tokensFilter: 'Filter by tokens',
+  filter: 'Filter',
   valueShouldBeNonNegative: 'The value should be non-negative.',
   valueShouldBePositive: 'The value should be greater than zero.',
   maxAvailableValueError: 'Max available value is {{value}}',
